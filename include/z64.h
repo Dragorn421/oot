8ecd1948ee8eaef3b34688b850223783d4529ab5
Random <28494085+Random06457@users.noreply.github.com>
2020-05-25 23:18:14 +0200

    Decompile a bunch of small files (#160)
    * Decompile a bunch of small files
    
    * Rename dacrate to dacRate
    
    * Run format.sh
    
    * Minor fixes in PR #160

--- include/z64.h
+++ include/z64.h
@@ -1732,4 +1732,9 @@ typedef struct {
     /* 0x06 */ u16 color;
 } SpeedMeterTimeEntry; // size = 0x08
 
+typedef struct {
+    /* 0x00 */ s16 intPart[4][4];
+    /* 0x20 */ u16 fracPart[4][4];
+} MatrixInternal; // size = 0x40
+
 #endif
