398b4456e8ba90be26ad3b200bbae62ccac02e5a
fig02 <fig02srl@gmail.com>
2020-06-24 21:20:31 -0400

    Rename culling related variables (#225)
    * rename some things
    
    * reword one comment
    
    * rename vars
    
    * fix bgjyalift

--- src/overlays/actors/ovl_Bg_Haka_Trap/z_bg_haka_trap.c
+++ src/overlays/actors/ovl_Bg_Haka_Trap/z_bg_haka_trap.c
@@ -138,7 +138,7 @@ void BgHakaTrap_Init(Actor* thisx, GlobalContext* globalCtx) {
     } else {
         this->unk168 = 0x28;
         this->actionFunc = &func_808809B0;
-        this->dyna.actor.unk_F8 = 500.0f;
+        this->dyna.actor.uncullZoneScale = 500.0f;
     }
     func_80061ED4(&this->dyna.actor.colChkInfo, NULL, &D_80881008);
 }
