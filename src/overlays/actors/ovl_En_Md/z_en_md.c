7a8ed0ec5ef02cb1b48d5f1a7f19623763b6b0ff
fig02 <fig02srl@gmail.com>
2020-10-28 13:02:35 -0400

    En_Elf Decompiled (#432)
    * init matched
    
    * everything but navi OK
    
    * progress
    
    * 4 left
    
    * remove asm
    
    * progress
    
    * progress
    
    * asm and spec
    
    * remove comment
    
    * suggestion
    
    * review1
    
    * use type enum
    
    * name func
    
    * overridelimbdraw
    
    * review 2
    
    * forgot one
    
    * merge master
    
    * format

--- src/overlays/actors/ovl_En_Md/z_en_md.c
+++ src/overlays/actors/ovl_En_Md/z_en_md.c
@@ -1,7 +1,6 @@
 #include "global.h"
-#include "z64collision_check.h"
 #include "z_en_md.h"
-#include "macros.h"
+#include "overlays/actors/ovl_En_Elf/z_en_elf.h"
 
 #define FLAGS 0x02000019
 
@@ -600,7 +599,7 @@ void EnMd_Init(Actor* thisx, GlobalContext* globalCtx) {
     this->actor.unk_1F = 6;
     this->unk210 = 255;
     Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_EN_ELF, this->actor.posRot.pos.x,
-                       this->actor.posRot.pos.y, this->actor.posRot.pos.z, 0, 0, 0, 3);
+                       this->actor.posRot.pos.y, this->actor.posRot.pos.z, 0, 0, 0, FAIRY_KOKIRI);
     if (((globalCtx->sceneNum == SCENE_SPOT04) && !(gSaveContext.eventChkInf[0] & 0x10)) ||
         ((globalCtx->sceneNum == SCENE_SPOT04) && (gSaveContext.eventChkInf[0] & 0x10) &&
          CHECK_QUEST_ITEM(QUEST_KOKIRI_EMERALD)) ||
