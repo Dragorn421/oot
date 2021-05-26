cfe73a9d5601c2725e7b198e9cd155ea4cbf7125
Vijfhoek <me@vijf.life>
2021-05-26 20:46:07 +0200

    Improve z_jpeg documentation (#825)
    * Improve z_jpeg documentation
    
    * JpegWork::data is probably better than address
    
    * Rename Jpeg_SendTask to Jpeg_ScheduleDecoderTask
    
    * Rename njpegUCode to gJpegUCode
    
    * Rename GetU16 to GetUnalignedU16 and improve docs

--- include/functions.h
+++ include/functions.h
@@ -850,7 +850,7 @@ void func_8006D074(GlobalContext* globalCtx);
 // ? func_8006D684(?);
 void func_8006DC68(GlobalContext* globalCtx, Player* player);
 void func_8006DD9C(Actor* actor, Vec3f* arg1, s16 arg2);
-u32 Jpeg_SendTask(JpegContext* ctx);
+u32 Jpeg_ScheduleDecoderTask(JpegContext* ctx);
 void Jpeg_CopyToZbuffer(u16* src, u16* zbuffer, s32 x, s32 y);
 u16 Jpeg_GetU16(u8* ptr);
 void Jpeg_ParseMarkers(u8* ptr, JpegContext* ctx);
