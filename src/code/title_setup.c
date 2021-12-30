323592fce708699f9532ffb008cb3caee59ce49b
Dragorn421 <Dragorn421@users.noreply.github.com>
2021-12-30 14:26:14 +0100

    Load to gameplay right after boot

--- src/code/title_setup.c
+++ src/code/title_setup.c
@@ -3,8 +3,34 @@
 void Setup_InitImpl(SetupState* this) {
     osSyncPrintf("ゼルダ共通データ初期化\n"); // "Zelda common data initalization"
     SaveContext_Init();
+
+    /*
+    // vanilla
     this->state.running = false;
     SET_NEXT_GAMESTATE(&this->state, ConsoleLogo_Init, ConsoleLogoState);
+    */
+
+    // Load debug save
+    Sram_InitDebugSave();
+
+    // Set age, time and entrance
+    gSaveContext.linkAge = LINK_AGE_ADULT;
+    gSaveContext.dayTime = CLOCK_TIME(12, 0);
+    gSaveContext.entranceIndex = ENTR_HYRULE_FIELD_0;
+
+    gSaveContext.respawnFlag = 0;
+    gSaveContext.respawn[RESPAWN_MODE_DOWN].entranceIndex = ENTR_LOAD_OPENING;
+
+    // Have the magic meter load correctly
+    gSaveContext.magicFillTarget = gSaveContext.magic;
+    gSaveContext.magicCapacity = 0;
+    gSaveContext.magic = 0;
+    gSaveContext.magicLevel = 0;
+
+    // Load Play state
+    gSaveContext.gameMode = GAMEMODE_NORMAL;
+    this->state.running = false;
+    SET_NEXT_GAMESTATE(&this->state, Play_Init, PlayState);
 }
 
 void Setup_Destroy(GameState* thisx) {
