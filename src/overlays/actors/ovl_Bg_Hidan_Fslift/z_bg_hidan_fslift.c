50249ab306c120d658642b8d1087503278cf7ff5
engineer124 <47598039+engineer124@users.noreply.github.com>
2022-11-26 18:15:33 -0500

    Document Camera for Elevator Platforms (Camera_Special7) (#1448)
    * document camera for vertical platforms
    
    * cleanup
    
    * another rename
    
    * small
    
    * another rename
    
    * add comment
    
    * PR Suggestions
    
    * more cleanup
    
    * oops
    
    * adjust comment

--- src/overlays/actors/ovl_Bg_Hidan_Fslift/z_bg_hidan_fslift.c
+++ src/overlays/actors/ovl_Bg_Hidan_Fslift/z_bg_hidan_fslift.c
@@ -137,7 +137,7 @@ void BgHidanFslift_Update(Actor* thisx, PlayState* play) {
         if (this->unk_16A == 0) {
             this->unk_16A = 3;
         }
-        Camera_ChangeSetting(play->cameraPtrs[CAM_ID_MAIN], CAM_SET_FIRE_PLATFORM);
+        Camera_ChangeSetting(play->cameraPtrs[CAM_ID_MAIN], CAM_SET_ELEVATOR_PLATFORM);
     } else if (DynaPolyActor_IsPlayerOnTop(thisx) == 0) {
         if (this->unk_16A != 0) {
             Camera_ChangeSetting(play->cameraPtrs[CAM_ID_MAIN], CAM_SET_DUNGEON0);
