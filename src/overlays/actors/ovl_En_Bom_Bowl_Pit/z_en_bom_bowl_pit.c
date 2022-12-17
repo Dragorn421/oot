05042491173b5345e5b0a7b547c29ba009e89a94
engineer124 <47598039+engineer124@users.noreply.github.com>
2022-12-17 16:07:31 -0500

    `z_play`: Camera Docs and Cleanup (#1460)
    * begin docs
    
    * More suggestions
    
    * more docs
    
    * timer to duration
    
    * partial PR review
    
    * init data with player
    
    * consistent comments
    
    * withPlayer to usingPlayer

--- src/overlays/actors/ovl_En_Bom_Bowl_Pit/z_en_bom_bowl_pit.c
+++ src/overlays/actors/ovl_En_Bom_Bowl_Pit/z_en_bom_bowl_pit.c
@@ -95,7 +95,7 @@ void func_809C4E8C(EnBomBowlPit* this, PlayState* play) {
                 this->subCamAtVel.x = fabsf(this->subCamAt.x - this->subCamAtNext.x) * 0.02f;
                 this->subCamAtVel.y = fabsf(this->subCamAt.y - this->subCamAtNext.y) * 0.02f;
                 this->subCamAtVel.z = fabsf(this->subCamAt.z - this->subCamAtNext.z) * 0.02f;
-                Play_CameraSetAtEye(play, this->subCamId, &this->subCamAt, &this->subCamEye);
+                Play_SetCameraAtEye(play, this->subCamId, &this->subCamAt, &this->subCamEye);
                 this->actor.textId = 0xF;
                 Message_StartTextbox(play, this->actor.textId, NULL);
                 this->unk154 = 5;
@@ -119,7 +119,7 @@ void func_809C5184(EnBomBowlPit* this, PlayState* play) {
         Math_ApproachF(&this->subCamEye.y, this->subCamEyeNext.y, this->subCamEyeMaxVelFrac.y, this->subCamEyeVel.y);
         Math_ApproachF(&this->subCamEye.z, this->subCamEyeNext.z, this->subCamEyeMaxVelFrac.z, this->subCamEyeVel.z);
     }
-    Play_CameraSetAtEye(play, this->subCamId, &this->subCamAt, &this->subCamEye);
+    Play_SetCameraAtEye(play, this->subCamId, &this->subCamAt, &this->subCamEye);
     if ((this->unk154 == Message_GetState(&play->msgCtx)) && Message_ShouldAdvance(play)) {
         Message_CloseTextbox(play);
     }
