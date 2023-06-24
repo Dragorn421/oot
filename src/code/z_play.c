e7cb797cd137273170d4058fe94f7509f9b04971
Dragorn421 <Dragorn421@users.noreply.github.com>
2023-06-24 18:46:17 +0200

    Fill VPC in crash handler for actor overlays, to facilitate debugging

--- src/code/z_play.c
+++ src/code/z_play.c
@@ -1,6 +1,7 @@
 #include "libc64/malloc.h"
 #include "libc64/qrand.h"
 #include "libu64/debug.h"
+#include "always_gc_fault.h"
 #include "array_count.h"
 #include "buffers.h"
 #include "color.h"
@@ -67,6 +68,10 @@ UNK_TYPE D_8012D1F4 = 0; // unused
 #endif
 
 Input* D_8012D1F8 = NULL;
+
+#if DEBUG_FEATURES && (!PLATFORM_N64 || ALWAYS_GC_FAULT)
+static FaultAddrConvClient sActorFaultAddrConvClient;
+#endif
 
 void Play_SpawnScene(PlayState* this, s32 sceneId, s32 spawn);
 
@@ -281,9 +286,56 @@ void Play_Destroy(GameState* thisx) {
 #endif
 
 #if DEBUG_FEATURES
+#if !PLATFORM_N64 || ALWAYS_GC_FAULT
+    Fault_RemoveAddrConvClient(&sActorFaultAddrConvClient);
+#endif
     Fault_RemoveClient(&D_801614B8);
 #endif
 }
+
+#if DEBUG_FEATURES && (!PLATFORM_N64 || ALWAYS_GC_FAULT)
+uintptr_t Actor_FaultAddrConv(uintptr_t addr, void* arg) {
+    int actor_id;
+    int n_matches = 0;
+    uintptr_t latest_match;
+
+    // Loop over the actor overlay table
+    for (actor_id = 0; actor_id < ACTOR_ID_MAX; actor_id++) {
+        ActorOverlay* ovlEntry = &gActorOverlayTable[actor_id];
+
+        // If the overlay is currently loaded in memory
+        if (ovlEntry->loadedRamAddr != NULL) {
+
+            uintptr_t loadedRamAddr = (uintptr_t)ovlEntry->loadedRamAddr;
+
+            uintptr_t vramStart = (uintptr_t)ovlEntry->vramStart;
+            uintptr_t vramEnd = (uintptr_t)ovlEntry->vramEnd;
+
+            uintptr_t ramStart = loadedRamAddr;
+            uintptr_t ramEnd = loadedRamAddr + (vramEnd - vramStart);
+
+            // If the input ram address `addr` falls within the ram range of the overlay
+            if (ramStart <= addr && addr < ramEnd) {
+                // Compute the vram equivalent of the input ram address
+                uintptr_t addr_vram = addr - loadedRamAddr + vramStart;
+
+                n_matches += 1;
+                latest_match = addr_vram;
+            }
+        }
+    }
+
+    // If exactly one overlay matches return the translated vram address
+    if (n_matches == 1) {
+        return latest_match;
+    }
+    // otherwise if 0 matches then nothing was found
+    // or if 2+ matches then something is wrong, don't report an address
+    else {
+        return 0;
+    }
+}
+#endif
 
 void Play_Init(GameState* thisx) {
     PlayState* this = (PlayState*)thisx;
@@ -499,6 +551,12 @@ void Play_Init(GameState* thisx) {
 #endif
 
     Actor_InitContext(this, &this->actorCtx, this->playerEntry);
+
+#if DEBUG_FEATURES && (!PLATFORM_N64 || ALWAYS_GC_FAULT)
+    // Allows filling the (VPC) column in the crash handler when the cpu faults inside actor overlays.
+    // That VPC address can then be looked up directly in the map file or with sym_info.py
+    Fault_AddAddrConvClient(&sActorFaultAddrConvClient, Actor_FaultAddrConv, NULL);
+#endif
 
     // Busyloop until the room loads
     while (!Room_ProcessRoomRequest(this, &this->roomCtx)) {
