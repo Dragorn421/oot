64ec41ff4acdb7f083c05acb2c6f0757aaf367dd
Billy <1184136+philtyl@users.noreply.github.com>
2023-09-20 01:41:42 +0000

    Dekunut Salesman Docs (#1493)
    * Dekunut Salesman Docs
    
    * Update src/overlays/actors/ovl_En_Dns/z_en_dns.c
    
    Remove extra param from debug logging
    
    Co-authored-by: Dragorn421 <Dragorn421@users.noreply.github.com>
    
    * * Full Deku/Potion name usage
    * Dialog -> CanBuy
    * Revert debug logging symbols
    * Proper declaration spacing
    * Fix en_shopnuts header doc
    * Biz -> Business
    
    * Fix missed deku prefix's
    
    * Synchronize CanBuy sticks and nuts
    
    * Fix enum naming
    
    * Clarified CanBuy success values
    
    * anon review
    
    * fig review
    
    * macro changes
    
    * fix macro usage
    
    ---------
    
    Co-authored-by: Dragorn421 <Dragorn421@users.noreply.github.com>
    Co-authored-by: fig02 <fig02srl@gmail.com>

--- src/overlays/actors/ovl_En_Dns/z_en_dns.h
+++ src/overlays/actors/ovl_En_Dns/z_en_dns.h
@@ -3,36 +3,61 @@
 
 #include "ultra64.h"
 #include "global.h"
+#include "assets/objects/object_shopnuts/object_shopnuts.h"
+
+#define DNS_GET_TYPE(thisx) ((thisx)->params)
 
 typedef enum EnDnsType {
-    /*  0 */ EN_DNS_TYPE_DEKU_NUTS,
-    /*  1 */ EN_DNS_TYPE_DEKU_STICKS,
-    /*  2 */ EN_DNS_TYPE_HEART_PIECE,
-    /*  3 */ EN_DNS_TYPE_DEKU_SEEDS,
-    /*  4 */ EN_DNS_TYPE_DEKU_SHIELD,
-    /*  5 */ EN_DNS_TYPE_BOMBS,
-    /*  6 */ EN_DNS_TYPE_ARROWS,
-    /*  7 */ EN_DNS_TYPE_RED_POTION,
-    /*  8 */ EN_DNS_TYPE_GREEN_POTION,
-    /*  9 */ EN_DNS_TYPE_DEKU_STICKS_CAPACITY,
-    /* 10 */ EN_DNS_TYPE_DEKU_NUTS_CAPACITY
+    /*  0 */ DNS_TYPE_DEKU_NUTS_5,
+    /*  1 */ DNS_TYPE_DEKU_STICKS_1,
+    /*  2 */ DNS_TYPE_HEART_PIECE,
+    /*  3 */ DNS_TYPE_DEKU_SEEDS_30,
+    /*  4 */ DNS_TYPE_DEKU_SHIELD,
+    /*  5 */ DNS_TYPE_BOMBS_5,
+    /*  6 */ DNS_TYPE_ARROWS_30,
+    /*  7 */ DNS_TYPE_RED_POTION,
+    /*  8 */ DNS_TYPE_GREEN_POTION,
+    /*  9 */ DNS_TYPE_DEKU_STICK_UPGRADE,
+    /* 10 */ DNS_TYPE_DEKU_NUT_UPGRADE
 } EnDnsType;
 
+typedef enum {
+    /*  0 */ DNS_CANBUY_RESULT_NEED_RUPEES,
+    /*  1 */ DNS_CANBUY_RESULT_CAPACITY_FULL,
+    /*  2 */ DNS_CANBUY_RESULT_SUCCESS_NEW_ITEM,
+    /*  3 */ DNS_CANBUY_RESULT_CANT_GET_NOW,
+    /*  4 */ DNS_CANBUY_RESULT_SUCCESS
+} EnDnsCanBuyResult;
+
+typedef enum {
+    /*  0 */ DNS_ANIM_IDLE,
+    /*  1 */ DNS_ANIM_BURROW,
+    /*  2 */ DNS_ANIM_IDLE_TRANSITION
+} EnDnsAnimation;
+
 struct EnDns;
+
+typedef struct EnDnsPurchaseInfo {
+    /* 0x00 */ s16 price;
+    /* 0x02 */ s16 unk_2; // unused
+    /* 0x04 */ s32 gid;
+    /* 0x08 */ u32 (*canBuy)(struct EnDns*);
+    /* 0x0C */ void (*payment)(struct EnDns*);
+} EnDnsPurchaseInfo;
 
 typedef struct EnDns {
     /* 0x0000 */ Actor actor;
     /* 0x14C */ SkelAnime unk14C;
-    /* 0x190 */ Vec3s unk190[0x12];
-    /* 0x1FC */ Vec3s unk1FC[0x12];
+    /* 0x190 */ Vec3s unk190[BUSINESS_SCRUB_LIMB_MAX];
+    /* 0x1FC */ Vec3s unk1FC[BUSINESS_SCRUB_LIMB_MAX];
     /* 0x268 */ void (*unk268)(struct EnDns*, PlayState*);
     /* 0x26C */ ColliderCylinder unk26C;
     /* 0x2B8 */ s16 unk2B8;
-    /* 0x2BA */ u8 unk2BA;
-    /* 0x2BB */ u8 unk2BB;
+    /* 0x2BA */ u8 animIndex;
+    /* 0x2BB */ u8 bumpOn;
     /* 0x2BC */ u8 unk2BC;
     /* 0x2BD */ u8 unk2BD;
-    /* 0x2C0 */ struct EnDnsPurchaseInfo* unk2C0;
+    /* 0x2C0 */ EnDnsPurchaseInfo* unk2C0;
     /* 0x2C4 */ f32 unk2C4;
 } EnDns; // size = 0x02C8
 
