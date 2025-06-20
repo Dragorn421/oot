40397fc34ed0f3c82605a85873e59e17d0d64305
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-06-20 10:12:45 +0200

    Rename AT `effect` to `hitSpecialEffect` (#2637)
    * Rename AT `effect` to `hitSpecialEffect`
    
    * bss

--- src/overlays/actors/ovl_En_Firefly/z_en_firefly.c
+++ src/overlays/actors/ovl_En_Firefly/z_en_firefly.c
@@ -74,7 +74,7 @@ static Vec3f D_80A14FC8 = { 0.0f, 0.0f, 0.0f };
 
 void func_80A13070_Unignite_(EnFirefly* this) {
     this->actor.params += 2;
-    this->collider.elements->base.atDmgInfo.effect = 0;
+    this->collider.elements->base.atDmgInfo.hitSpecialEffect = HIT_SPECIAL_EFFECT_0;
     this->unk1B8 = 0;
     this->unk1B9 = 0;
     this->actor.naviEnemyId = NAVI_ENEMY_KEESE;
@@ -86,7 +86,7 @@ void func_80A13098_Ignite_(EnFirefly* this) {
     } else {
         this->actor.params -= 2;
     }
-    this->collider.elements->base.atDmgInfo.effect = 1;
+    this->collider.elements->base.atDmgInfo.hitSpecialEffect = HIT_SPECIAL_EFFECT_1;
     this->unk1B8 = 1;
     this->unk1B9 = 1;
     this->actor.naviEnemyId = NAVI_ENEMY_FIRE_KEESE;
@@ -125,10 +125,10 @@ void EnFirefly_Init(Actor* thisx, PlayState* play) {
             this->actionFunc = func_80A13A08_Action;
         }
         if (this->actor.params == 4) {
-            this->collider.elements->base.atDmgInfo.effect = 2;
+            this->collider.elements->base.atDmgInfo.hitSpecialEffect = HIT_SPECIAL_EFFECT_2;
             this->actor.naviEnemyId = NAVI_ENEMY_ICE_KEESE;
         } else {
-            this->collider.elements->base.atDmgInfo.effect = 0;
+            this->collider.elements->base.atDmgInfo.hitSpecialEffect = HIT_SPECIAL_EFFECT_0;
             this->actor.naviEnemyId = NAVI_ENEMY_KEESE;
         }
         this->unk310_homeY = this->actor.home.pos.y + 100.0f;
