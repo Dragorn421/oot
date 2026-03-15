HackerN64/HackerOoT

Actor Cleanups (#177)
* rename init chains, colchkinfo, colider inits, damage tables

* actor cleanups

* fix collider init script names

* small fixes

* ichain arg name

* change dynapoly types and names

* revert enru1 data name

* and the type

* pr suggestions

src/overlays/actors/ovl_Bg_Toki_Swd/z_bg_toki_swd.c
@@ -19,7 +19,7 @@ void func_808BAF40(BgTokiSwd* this, GlobalContext* globalCtx);
 void func_808BB0AC(BgTokiSwd* this, GlobalContext* globalCtx);
 void func_808BB128(BgTokiSwd* this, GlobalContext* globalCtx);
 
-extern u32 D_06001BD0;
+extern Gfx D_06001BD0[];
 
 #include "z_bg_toki_swd_cutscene_data.c" EARLY
 
@@ -35,20 +35,20 @@ const ActorInit Bg_Toki_Swd_InitVars = {
     (ActorFunc)BgTokiSwd_Draw,
 };
 
-static ColliderCylinderInit colliderInit = {
+static ColliderCylinderInit sCylinderInit = {
     { COLTYPE_UNK10, 0x00, 0x00, 0x39, 0x12, COLSHAPE_CYLINDER },
     { 0x00, { 0xFFCFFFFF, 0x00, 0x00 }, { 0xFFCFFFFF, 0x00, 0x00 }, 0x00, 0x00, 0x01 },
     { 10, 70, 0, { 0 } }
 };
 
-static CollisionCheckInfoInit colChkInfoInit = {
+static CollisionCheckInfoInit sColChkInfoInit = {
     0x0A,
     0x0023,
     0x0064,
     0xFF,
 };
 
-static InitChainEntry initChain[] = {
+static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale, 0x19, ICHAIN_STOP),
 };
 
@@ -60,7 +60,7 @@ void BgTokiSwd_Init(Actor* thisx, GlobalContext* globalCtx) {
     BgTokiSwd* this = THIS;
     s32 pad;
 
-    Actor_ProcessInitChain(thisx, initChain);
+    Actor_ProcessInitChain(thisx, sInitChain);
     this->actor.shape.unk_08 = 800.0f;
     BgTokiSwd_SetupAction(thisx, func_808BAF40);
 
@@ -73,9 +73,9 @@ void BgTokiSwd_Init(Actor* thisx, GlobalContext* globalCtx) {
     }
 
     Collider_InitCylinder(globalCtx, &this->collider);
-    Collider_SetCylinder(globalCtx, &this->collider, thisx, &colliderInit);
+    Collider_SetCylinder(globalCtx, &this->collider, thisx, &sCylinderInit);
     Collider_CylinderUpdate(thisx, &this->collider);
-    func_80061ED4(&thisx->colChkInfo, 0, &colChkInfoInit);
+    func_80061ED4(&thisx->colChkInfo, 0, &sColChkInfoInit);
 }
 
 void BgTokiSwd_Destroy(Actor* thisx, GlobalContext* globalCtx) {
@@ -91,7 +91,7 @@ void func_808BAF40(BgTokiSwd* this, GlobalContext* globalCtx) {
         globalCtx->csCtx.segment = D_808BBD90;
         gSaveContext.cutsceneTrigger = 1;
     }
-    if (LINK_IS_CHILD || ((gSaveContext.eventChkInf[5] & 0x20) != 0)) {
+    if (LINK_IS_CHILD || ((gSaveContext.eventChkInf[5] & 0x20))) {
         if (func_8002F410(&this->actor, globalCtx) != 0) {
             if (LINK_IS_CHILD) {
                 Item_Give(globalCtx, ITEM_SWORD_MASTER);
@@ -102,7 +102,7 @@ void func_808BAF40(BgTokiSwd* this, GlobalContext* globalCtx) {
             Audio_SetBGM(NA_BGM_STOP);
             Audio_SetBGM(NA_SE_PL_BOUND_DIRT);
             gSaveContext.cutsceneTrigger = 1;
-            this->actor.attachedA = 0;
+            this->actor.attachedA = NULL;
             BgTokiSwd_SetupAction(this, func_808BB0AC);
         } else {
             if (func_8002E084(&this->actor, 0x2000) != 0) {
@@ -169,7 +169,7 @@ void BgTokiSwd_Draw(Actor* thisx, GlobalContext* globalCtx) {
                Gfx_TexScroll(globalCtx->state.gfxCtx, 0, -(globalCtx->gameplayFrames % 0x80), 32, 32));
     gSPMatrix(gfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_toki_swd.c", 742),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-    gSPDisplayList(gfxCtx->polyOpa.p++, &D_06001BD0);
+    gSPDisplayList(gfxCtx->polyOpa.p++, D_06001BD0);
 
     Graph_CloseDisps(dispRefs, globalCtx->state.gfxCtx, "../z_bg_toki_swd.c", 776);
 }

src/overlays/actors/ovl_En_Heishi2/z_en_heishi2.c
@@ -54,7 +54,7 @@ extern AnimationHeader D_06005C30;
 extern AnimationHeader D_06005500;
 extern SkeletonHeader D_0600BAC8;
 extern Gfx D_0602B060[]; // Keaton Mask
-extern Gfx D_06002C10[];
+extern Gfx D_06002C10[]; // 2D Guard in Window
 
 const ActorInit En_Heishi2_InitVars = {
     ACTOR_EN_HEISHI2,
@@ -68,7 +68,7 @@ const ActorInit En_Heishi2_InitVars = {
     (ActorFunc)EnHeishi2_Draw,
 };
 
-static ColliderCylinderInit cylinderInit = {
+static ColliderCylinderInit sCylinderInit = {
     { COLTYPE_UNK10, 0x00, 0x00, 0x39, 0x20, COLSHAPE_CYLINDER },
     { 0x00, { 0x00000000, 0x00, 0x00 }, { 0x00000000, 0x00, 0x00 }, 0x00, 0x00, 0x01 },
     { 33, 40, 0, { 0, 0, 0 } },
@@ -80,7 +80,7 @@ void EnHeishi2_Init(Actor* thisx, GlobalContext* globalCtx) {
 
     Actor_SetScale(&this->actor, 0.01f);
     this->initParams = this->actor.params & 0xFF;
-    this->actor.colChkInfo.mass = 0xFF;
+    this->actor.colChkInfo.mass = 255;
 
     if ((this->initParams == 6) || (this->initParams == 9)) {
         this->actor.draw = func_80A54C6C;
@@ -114,7 +114,7 @@ void EnHeishi2_Init(Actor* thisx, GlobalContext* globalCtx) {
                        this->transitionDrawTable, 17);
         collider = &this->collider;
         Collider_InitCylinder(globalCtx, collider);
-        Collider_SetCylinder(globalCtx, collider, &this->actor, &cylinderInit);
+        Collider_SetCylinder(globalCtx, collider, &this->actor, &sCylinderInit);
         this->collider.dim.yShift = 0;
         this->collider.dim.radius = 0xF;
         this->collider.dim.height = 0x46;
@@ -855,7 +855,7 @@ void func_80A54C6C(Actor* thisx, GlobalContext* globalCtx) {
     Graph_OpenDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_heishi2.c", 1772);
     gSPMatrix(gfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_en_heishi2.c", 1774),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-    gSPDisplayList(gfxCtx->polyOpa.p++, &D_06002C10); // 2D Guard in Window
+    gSPDisplayList(gfxCtx->polyOpa.p++, &D_06002C10);
     Graph_CloseDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_heishi2.c", 1777);
 }
 

src/overlays/actors/ovl_En_It/z_en_it.c
@@ -14,13 +14,13 @@ void EnIt_Init(Actor* thisx, GlobalContext* globalCtx);
 void EnIt_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnIt_Update(Actor* thisx, GlobalContext* globalCtx);
 
-static ColliderCylinderInit cylinderInitData = {
+static ColliderCylinderInit sCylinderInit = {
     { COLTYPE_UNK10, 0x00, 0x00, 0x05, 0x10, COLSHAPE_CYLINDER },
     { 0x00, { 0x00000000, 0x00, 0x00 }, { 0x00000000, 0x00, 0x00 }, 0x00, 0x00, 0x01 },
     { 40, 10, 0, { 0 } },
 };
 
-static CollisionCheckInfoInit2 colChkInfoInit = {
+static CollisionCheckInfoInit2 sColChkInfoInit = {
     0x00, 0x0000, 0x0000, 0x0000, 0xFF,
 };
 
@@ -41,8 +41,8 @@ void EnIt_Init(Actor* thisx, GlobalContext* globalCtx) {
 
     this->actor.params = 0x0D05;
     Collider_InitCylinder(globalCtx, &this->collider);
-    Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &cylinderInitData);
-    func_80061EFC(&this->actor.colChkInfo, 0, &colChkInfoInit); // Init Damage Chart
+    Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
+    func_80061EFC(&this->actor.colChkInfo, 0, &sColChkInfoInit);
 }
 
 void EnIt_Destroy(Actor* thisx, GlobalContext* globalCtx) {

src/overlays/actors/ovl_En_M_Fire1/z_en_m_fire1.c
@@ -1,7 +1,7 @@
 /*
  * File: z_en_m_fire1.c
  * Overlay: ovl_En_M_Fire1
- * Description: The attack from a Deku Nut. Spawned by ovl_En_Arrow.
+ * Description: Deku Nut Hitbox
  */
 
 #include "z_en_m_fire1.h"
@@ -26,7 +26,7 @@ const ActorInit En_M_Fire1_InitVars = {
     NULL,
 };
 
-static ColliderCylinderInit cylinderInitData = {
+static ColliderCylinderInit sCylinderInit = {
     { COLTYPE_UNK10, 0x09, 0x00, 0x00, 0x08, COLSHAPE_CYLINDER },
     { 0x02, { 0x00000001, 0x00, 0x00 }, { 0xFFCFFFFF, 0x00, 0x00 }, 0x19, 0x00, 0x00 },
     { 200, 200, 0, { 0 } }
@@ -41,7 +41,7 @@ void EnMFire1_Init(Actor* thisx, GlobalContext* globalCtx) {
     }
 
     Collider_InitCylinder(globalCtx, &this->collider);
-    Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &cylinderInitData);
+    Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
 }
 
 void EnMFire1_Destroy(Actor* thisx, GlobalContext* globalCtx) {

src/overlays/actors/ovl_En_Nutsball/z_en_nutsball.c
@@ -1,7 +1,7 @@
 /*
  * File: z_en_nutsball.c
  * Overlay: ovl_En_Nutsball
- * Description: The projectile fired by deku scrubs and octoroks.
+ * Description: Projectile fired by deku scrubs
  */
 
 #include "z_en_nutsball.h"
@@ -30,14 +30,15 @@ const ActorInit En_Nutsball_InitVars = {
     (ActorFunc)NULL,
 };
 
-static ColliderCylinderInit cylinderInitData = {
+static ColliderCylinderInit sCylinderInit = {
     { COLTYPE_UNK10, 0x11, 0x09, 0x39, 0x20, COLSHAPE_CYLINDER },
     { 0x00, { 0xFFCFFFFF, 0x00, 0x08 }, { 0xFFCFFFFF, 0x00, 0x00 }, 0x11, 0x01, 0x01 },
     { 13, 13, 0, { 0 } },
 };
 
-static s16 objectTbl[] = { OBJECT_DEKUNUTS, OBJECT_HINTNUTS, OBJECT_SHOPNUTS, OBJECT_DNS, OBJECT_DNK };
-static u32 dListTbl[] = { 0x06002028, 0x060012F0, 0x06004008, 0x06002410, 0x06001890 };
+static s16 sObjectIDs[] = { OBJECT_DEKUNUTS, OBJECT_HINTNUTS, OBJECT_SHOPNUTS, OBJECT_DNS, OBJECT_DNK };
+
+static Gfx* sDLists[] = { 0x06002028, 0x060012F0, 0x06004008, 0x06002410, 0x06001890 };
 
 void EnNutsball_Init(Actor* thisx, GlobalContext* globalCtx) {
     EnNutsball* this = THIS;
@@ -45,8 +46,8 @@ void EnNutsball_Init(Actor* thisx, GlobalContext* globalCtx) {
 
     ActorShape_Init(&this->actor.shape, 400.0f, ActorShadow_DrawFunc_Circle, 13.0f);
     Collider_InitCylinder(globalCtx, &this->collider);
-    Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &cylinderInitData);
-    this->objBankIndex = Object_GetIndex(&globalCtx->objectCtx, objectTbl[this->actor.params]);
+    Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
+    this->objBankIndex = Object_GetIndex(&globalCtx->objectCtx, sObjectIDs[this->actor.params]);
 
     if (this->objBankIndex < 0) {
         Actor_Kill(&this->actor);
@@ -80,7 +81,7 @@ void func_80ABBBA8(EnNutsball* this, GlobalContext* globalCtx) {
     this->timer--;
 
     if (this->timer == 0) {
-        this->actor.gravity = -1;
+        this->actor.gravity = -1.0f;
     }
 
     this->actor.initPosRot.rot.z += 0x2AA8;
@@ -126,7 +127,7 @@ void EnNutsball_Update(Actor* thisx, GlobalContext* globalCtx) {
         this->actionFunc(this, globalCtx);
 
         Actor_MoveForward(&this->actor);
-        func_8002E4B4(globalCtx, &this->actor, 10, cylinderInitData.dim.radius, cylinderInitData.dim.height, 5);
+        func_8002E4B4(globalCtx, &this->actor, 10, sCylinderInit.dim.radius, sCylinderInit.dim.height, 5);
         Collider_CylinderUpdate(&this->actor, &this->collider);
 
         this->actor.flags |= 0x1000000;
@@ -148,7 +149,7 @@ void EnNutsball_Draw(Actor* thisx, GlobalContext* globalCtx) {
     Matrix_RotateZ(thisx->initPosRot.rot.z * 9.58738e-05f, MTXMODE_APPLY);
     gSPMatrix(gfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_en_nutsball.c", 333),
               G_MTX_MODELVIEW | G_MTX_LOAD);
-    gSPDisplayList(gfxCtx->polyOpa.p++, dListTbl[thisx->params]);
+    gSPDisplayList(gfxCtx->polyOpa.p++, sDLists[thisx->params]);
 
     Graph_CloseDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_nutsball.c", 337);
 }

Dragorn421/oot

src/overlays/actors/ovl_Bg_Toki_Swd/z_bg_toki_swd.c
@@ -19,7 +19,7 @@ void func_808BAF40(BgTokiSwd* this, GlobalContext* globalCtx);
 void func_808BB0AC(BgTokiSwd* this, GlobalContext* globalCtx);
 void func_808BB128(BgTokiSwd* this, GlobalContext* globalCtx);
 
-extern u32 D_06001BD0;
+extern Gfx D_06001BD0[];
 
 #include "z_bg_toki_swd_cutscene_data.c" EARLY
 
@@ -35,7 +35,7 @@ const ActorInit Bg_Toki_Swd_InitVars = {
     (ActorFunc)BgTokiSwd_Draw,
 };
 
-static ColliderCylinderSrc colliderInit = {
+static ColliderCylinderSrc sCylinderInit = {
     {
         COL_MATERIAL_NONE,
         AT_NONE,
@@ -45,7 +45,7 @@ static ColliderCylinderSrc colliderInit = {
         COLTYPE_CYLINDER,
     },
     {
-        0,
+        ELEM_MATERIAL_UNK0,
         {
             0xFFCFFFFF,
             HIT_SPECIAL_EFFECT_NONE,
@@ -56,9 +56,9 @@ static ColliderCylinderSrc colliderInit = {
             HIT_BACKLASH_NONE,
             0,
         },
-        0,
-        0,
-        1,
+        ATELEM_NONE,
+        ACELEM_NONE,
+        OCELEM_ON,
     },
     {
         10,
@@ -72,14 +72,14 @@ static ColliderCylinderSrc colliderInit = {
     },
 };
 
-static CollideDataInit collideDataInit = {
+static CollideDataInit sCollideDataInit = {
     0x0A,
     0x0023,
     0x0064,
     0xFF,
 };
 
-static InitChainEntry initChain[] = {
+static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale, 0x19, ICHAIN_STOP),
 };
 
@@ -91,7 +91,7 @@ void BgTokiSwd_Init(Actor* thisx, GlobalContext* globalCtx) {
     BgTokiSwd* this = THIS;
     s32 pad;
 
-    Actor_ProcessInitChain(thisx, initChain);
+    Actor_ProcessInitChain(thisx, sInitChain);
     this->actor.shape.unk_08 = 800.0f;
     BgTokiSwd_SetupAction(thisx, func_808BAF40);
 
@@ -104,9 +104,9 @@ void BgTokiSwd_Init(Actor* thisx, GlobalContext* globalCtx) {
     }
 
     Collider_InitCylinder(globalCtx, &this->collider);
-    Collider_LoadCylinder(globalCtx, &this->collider, thisx, &colliderInit);
+    Collider_LoadCylinder(globalCtx, &this->collider, thisx, &sCylinderInit);
     Collider_UpdateCylinderShape(thisx, &this->collider);
-    func_80061ED4(&thisx->collideData, 0, &collideDataInit);
+    func_80061ED4(&thisx->collideData, 0, &sCollideDataInit);
 }
 
 void BgTokiSwd_Destroy(Actor* thisx, GlobalContext* globalCtx) {
@@ -122,7 +122,7 @@ void func_808BAF40(BgTokiSwd* this, GlobalContext* globalCtx) {
         globalCtx->csCtx.segment = D_808BBD90;
         gSaveContext.cutsceneTrigger = 1;
     }
-    if (LINK_IS_CHILD || ((gSaveContext.eventChkInf[5] & 0x20) != 0)) {
+    if (LINK_IS_CHILD || ((gSaveContext.eventChkInf[5] & 0x20))) {
         if (func_8002F410(&this->actor, globalCtx) != 0) {
             if (LINK_IS_CHILD) {
                 Item_Give(globalCtx, ITEM_SWORD_MASTER);
@@ -133,7 +133,7 @@ void func_808BAF40(BgTokiSwd* this, GlobalContext* globalCtx) {
             Audio_SetBGM(NA_BGM_STOP);
             Audio_SetBGM(NA_SE_PL_BOUND_DIRT);
             gSaveContext.cutsceneTrigger = 1;
-            this->actor.attachedA = 0;
+            this->actor.attachedA = NULL;
             BgTokiSwd_SetupAction(this, func_808BB0AC);
         } else {
             if (func_8002E084(&this->actor, 0x2000) != 0) {
@@ -200,7 +200,7 @@ void BgTokiSwd_Draw(Actor* thisx, GlobalContext* globalCtx) {
                Gfx_TexScroll(globalCtx->state.gfxCtx, 0, -(globalCtx->gameplayFrames % 0x80), 32, 32));
     gSPMatrix(gfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_toki_swd.c", 742),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-    gSPDisplayList(gfxCtx->polyOpa.p++, &D_06001BD0);
+    gSPDisplayList(gfxCtx->polyOpa.p++, D_06001BD0);
 
     Graph_CloseDisps(dispRefs, globalCtx->state.gfxCtx, "../z_bg_toki_swd.c", 776);
 }

src/overlays/actors/ovl_En_Heishi2/z_en_heishi2.c
@@ -54,7 +54,7 @@ extern AnimationHeader D_06005C30;
 extern AnimationHeader D_06005500;
 extern SkeletonHeader D_0600BAC8;
 extern Gfx D_0602B060[]; // Keaton Mask
-extern Gfx D_06002C10[];
+extern Gfx D_06002C10[]; // 2D Guard in Window
 
 const ActorInit En_Heishi2_InitVars = {
     ACTOR_EN_HEISHI2,
@@ -68,7 +68,7 @@ const ActorInit En_Heishi2_InitVars = {
     (ActorFunc)EnHeishi2_Draw,
 };
 
-ColliderCylinderSrc cylinderInit_EnHeishi2 = {
+ColliderCylinderSrc sCylinderInit_EnHeishi2 = {
     {
         COL_MATERIAL_NONE,
         AT_NONE,
@@ -111,7 +111,7 @@ void EnHeishi2_Init(Actor* thisx, GlobalContext* globalCtx) {
 
     Actor_SetScale(&this->actor, 0.01f);
     this->initParams = this->actor.params & 0xFF;
-    this->actor.collideData.mass = 0xFF;
+    this->actor.collideData.mass = 255;
 
     if ((this->initParams == 6) || (this->initParams == 9)) {
         this->actor.draw = func_80A54C6C;
@@ -145,7 +145,7 @@ void EnHeishi2_Init(Actor* thisx, GlobalContext* globalCtx) {
                        this->transitionDrawTable, 17);
         collider = &this->collider;
         Collider_InitCylinder(globalCtx, collider);
-        Collider_LoadCylinder(globalCtx, collider, &this->actor, &cylinderInit_EnHeishi2);
+        Collider_LoadCylinder(globalCtx, collider, &this->actor, &sCylinderInit_EnHeishi2);
         this->collider.shape.yShift = 0;
         this->collider.shape.radius = 0xF;
         this->collider.shape.height = 0x46;
@@ -886,7 +886,7 @@ void func_80A54C6C(Actor* thisx, GlobalContext* globalCtx) {
     Graph_OpenDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_heishi2.c", 1772);
     gSPMatrix(gfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_en_heishi2.c", 1774),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-    gSPDisplayList(gfxCtx->polyOpa.p++, &D_06002C10); // 2D Guard in Window
+    gSPDisplayList(gfxCtx->polyOpa.p++, &D_06002C10);
     Graph_CloseDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_heishi2.c", 1777);
 }
 

src/overlays/actors/ovl_En_It/z_en_it.c
@@ -14,7 +14,7 @@ void EnIt_Init(Actor* thisx, GlobalContext* globalCtx);
 void EnIt_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnIt_Update(Actor* thisx, GlobalContext* globalCtx);
 
-static ColliderCylinderSrc cylinderInitData = {
+static ColliderCylinderSrc sCylinderInit = {
     {
         COL_MATERIAL_NONE,
         AT_NONE,
@@ -51,8 +51,8 @@ static ColliderCylinderSrc cylinderInitData = {
     },
 };
 
-static u8 collideDataInit[] = {
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,
+static CollideDataInitAlt sCollideDataInit = {
+    0x00, 0x0000, 0x0000, 0x0000, 0xFF,
 };
 
 const ActorInit En_It_InitVars = {
@@ -72,8 +72,8 @@ void EnIt_Init(Actor* thisx, GlobalContext* globalCtx) {
 
     this->actor.params = 0x0D05;
     Collider_InitCylinder(globalCtx, &this->collider);
-    Collider_LoadCylinder(globalCtx, &this->collider, &this->actor, &cylinderInitData);
-    func_80061EFC(&this->actor.collideData, 0, &collideDataInit); // Init Damage Chart
+    Collider_LoadCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
+    func_80061EFC(&this->actor.collideData, 0, &sCollideDataInit);
 }
 
 void EnIt_Destroy(Actor* thisx, GlobalContext* globalCtx) {

src/overlays/actors/ovl_En_M_Fire1/z_en_m_fire1.c
@@ -1,7 +1,7 @@
 /*
  * File: z_en_m_fire1.c
  * Overlay: ovl_En_M_Fire1
- * Description: The attack from a Deku Nut. Spawned by ovl_En_Arrow.
+ * Description: Deku Nut Hitbox
  */
 
 #include "z_en_m_fire1.h"
@@ -26,7 +26,7 @@ const ActorInit En_M_Fire1_InitVars = {
     NULL,
 };
 
-static ColliderCylinderSrc cylinderInitData = {
+static ColliderCylinderSrc sCylinderInit = {
     {
         COL_MATERIAL_NONE,
         AT_ON | AT_TYPE_PLAYER,
@@ -72,7 +72,7 @@ void EnMFire1_Init(Actor* thisx, GlobalContext* globalCtx) {
     }
 
     Collider_InitCylinder(globalCtx, &this->collider);
-    Collider_LoadCylinder(globalCtx, &this->collider, &this->actor, &cylinderInitData);
+    Collider_LoadCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
 }
 
 void EnMFire1_Destroy(Actor* thisx, GlobalContext* globalCtx) {

src/overlays/actors/ovl_En_Nutsball/z_en_nutsball.c
@@ -1,7 +1,7 @@
 /*
  * File: z_en_nutsball.c
  * Overlay: ovl_En_Nutsball
- * Description: The projectile fired by deku scrubs and octoroks.
+ * Description: Projectile fired by deku scrubs
  */
 
 #include "z_en_nutsball.h"
@@ -30,7 +30,7 @@ const ActorInit En_Nutsball_InitVars = {
     (ActorFunc)NULL,
 };
 
-static ColliderCylinderSrc cylinderInitData = {
+static ColliderCylinderSrc sCylinderInit = {
     {
         COL_MATERIAL_NONE,
         AT_ON | AT_TYPE_ENEMY,
@@ -67,8 +67,9 @@ static ColliderCylinderSrc cylinderInitData = {
     },
 };
 
-static s16 objectTbl[] = { OBJECT_DEKUNUTS, OBJECT_HINTNUTS, OBJECT_SHOPNUTS, OBJECT_DNS, OBJECT_DNK };
-static u32 dListTbl[] = { 0x06002028, 0x060012F0, 0x06004008, 0x06002410, 0x06001890 };
+static s16 sObjectIDs[] = { OBJECT_DEKUNUTS, OBJECT_HINTNUTS, OBJECT_SHOPNUTS, OBJECT_DNS, OBJECT_DNK };
+
+static Gfx* sDLists[] = { 0x06002028, 0x060012F0, 0x06004008, 0x06002410, 0x06001890 };
 
 void EnNutsball_Init(Actor* thisx, GlobalContext* globalCtx) {
     EnNutsball* this = THIS;
@@ -76,8 +77,8 @@ void EnNutsball_Init(Actor* thisx, GlobalContext* globalCtx) {
 
     ActorShape_Init(&this->actor.shape, 400.0f, ActorShadow_DrawFunc_Circle, 13.0f);
     Collider_InitCylinder(globalCtx, &this->collider);
-    Collider_LoadCylinder(globalCtx, &this->collider, &this->actor, &cylinderInitData);
-    this->objBankIndex = Object_GetIndex(&globalCtx->objectCtx, objectTbl[this->actor.params]);
+    Collider_LoadCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
+    this->objBankIndex = Object_GetIndex(&globalCtx->objectCtx, sObjectIDs[this->actor.params]);
 
     if (this->objBankIndex < 0) {
         Actor_Kill(&this->actor);
@@ -111,7 +112,7 @@ void func_80ABBBA8(EnNutsball* this, GlobalContext* globalCtx) {
     this->timer--;
 
     if (this->timer == 0) {
-        this->actor.gravity = -1;
+        this->actor.gravity = -1.0f;
     }
 
     this->actor.initPosRot.rot.z += 0x2AA8;
@@ -157,7 +158,7 @@ void EnNutsball_Update(Actor* thisx, GlobalContext* globalCtx) {
         this->actionFunc(this, globalCtx);
 
         Actor_MoveForward(&this->actor);
-        func_8002E4B4(globalCtx, &this->actor, 10, cylinderInitData.shape.radius, cylinderInitData.shape.height, 5);
+        func_8002E4B4(globalCtx, &this->actor, 10, sCylinderInit.shape.radius, sCylinderInit.shape.height, 5);
         Collider_UpdateCylinderShape(&this->actor, &this->collider);
 
         this->actor.flags |= 0x1000000;
@@ -179,7 +180,7 @@ void EnNutsball_Draw(Actor* thisx, GlobalContext* globalCtx) {
     Matrix_RotateZ(thisx->initPosRot.rot.z * 9.58738e-05f, MTXMODE_APPLY);
     gSPMatrix(gfxCtx->polyOpa.p++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_en_nutsball.c", 333),
               G_MTX_MODELVIEW | G_MTX_LOAD);
-    gSPDisplayList(gfxCtx->polyOpa.p++, dListTbl[thisx->params]);
+    gSPDisplayList(gfxCtx->polyOpa.p++, sDLists[thisx->params]);
 
     Graph_CloseDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_nutsball.c", 337);
 }