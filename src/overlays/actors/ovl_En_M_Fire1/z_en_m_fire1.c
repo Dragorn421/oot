HackerN64/HackerOoT

More actor cleanup Part 1 (#107)
* Fix naming and structs in z_en_item00.c and z_en_a_keep.c

* Decompile init vars in z_en_item00.c and z_en_a_keep.c

* Create missing .h files for the last few actors

* Fix some collider member names in actor structs

* Fix old actors not properly using "actionFunc" / "SetupAction"

* Remove some local temporary actor structs

* Fix some actor header includes to be absolute instead of relative

src/overlays/actors/ovl_En_M_Fire1/z_en_m_fire1.c
@@ -40,23 +40,23 @@ void EnMFire1_Init(EnMFire1* this, GlobalContext* globalCtx) {
         Actor_ChangeType(globalCtx, &globalCtx->actorCtx, &thisLocal->actor, ACTORTYPE_ITEMACTION);
     }
 
-    Collider_InitCylinder(globalCtx, &thisLocal->capsule);
-    Collider_SetCylinder(globalCtx, &thisLocal->capsule, &thisLocal->actor, &cylinderInitData);
+    Collider_InitCylinder(globalCtx, &thisLocal->collider);
+    Collider_SetCylinder(globalCtx, &thisLocal->collider, &thisLocal->actor, &cylinderInitData);
 }
 
 void EnMFire1_Destroy(EnMFire1* this, GlobalContext* globalCtx) {
-    ColliderCylinder* capsule = &this->capsule;
-    Collider_DestroyCylinder(globalCtx, capsule);
+    ColliderCylinder* collider = &this->collider;
+    Collider_DestroyCylinder(globalCtx, collider);
 }
 
 void EnMFire1_Update(EnMFire1* this, GlobalContext* globalCtx) {
     s32 pad;
     EnMFire1* thisLocal = this;
 
-    if (Math_ApproxF(&thisLocal->unk_0198, 1.0, 0.2)) {
+    if (Math_ApproxF(&thisLocal->unk_198, 1.0f, 0.2f)) {
         Actor_Kill(&this->actor);
     } else {
-        Collider_CylinderUpdate(&thisLocal->actor, &thisLocal->capsule);
-        CollisionCheck_SetAT(globalCtx, &globalCtx->colChkCtx, &thisLocal->capsule);
+        Collider_CylinderUpdate(&thisLocal->actor, &thisLocal->collider);
+        CollisionCheck_SetAT(globalCtx, &globalCtx->colChkCtx, &thisLocal->collider);
     }
 }

Dragorn421/oot

src/overlays/actors/ovl_En_M_Fire1/z_en_m_fire1.c
@@ -71,23 +71,23 @@ void EnMFire1_Init(EnMFire1* this, GlobalContext* globalCtx) {
         Actor_ChangeType(globalCtx, &globalCtx->actorCtx, &thisLocal->actor, ACTORTYPE_ITEMACTION);
     }
 
-    Collider_InitCylinder(globalCtx, &thisLocal->capsule);
-    Collider_LoadCylinder(globalCtx, &thisLocal->capsule, &thisLocal->actor, &cylinderInitData);
+    Collider_InitCylinder(globalCtx, &thisLocal->collider);
+    Collider_LoadCylinder(globalCtx, &thisLocal->collider, &thisLocal->actor, &cylinderInitData);
 }
 
 void EnMFire1_Destroy(EnMFire1* this, GlobalContext* globalCtx) {
-    ColliderCylinder* capsule = &this->capsule;
-    Collider_DestroyCylinder(globalCtx, capsule);
+    ColliderCylinder* collider = &this->collider;
+    Collider_DestroyCylinder(globalCtx, collider);
 }
 
 void EnMFire1_Update(EnMFire1* this, GlobalContext* globalCtx) {
     s32 pad;
     EnMFire1* thisLocal = this;
 
-    if (Math_ApproxF(&thisLocal->unk_0198, 1.0, 0.2)) {
+    if (Math_ApproxF(&thisLocal->unk_198, 1.0f, 0.2f)) {
         Actor_Kill(&this->actor);
     } else {
-        Collider_UpdateCylinderShape(&thisLocal->actor, &thisLocal->capsule);
-        Collider_AddAT(globalCtx, &globalCtx->colliderCtx, &thisLocal->capsule);
+        Collider_UpdateCylinderShape(&thisLocal->actor, &thisLocal->collider);
+        Collider_AddAT(globalCtx, &globalCtx->colliderCtx, &thisLocal->collider);
     }
 }