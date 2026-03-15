HackerN64/HackerOoT

Decomp game.c (#129)
* decomp most of game.c

* improve GameState_DrawInputDisplay matching

* fix merge conflicts

* pr suggestions

* remove builtin math functions

include/functions.h
@@ -80,16 +80,6 @@ s32 osRecvMesg(OSMesgQueue* mq, OSMesg* msg, s32 flag);
 void __createSpeedParam(void);
 void __osInitialize_common(void);
 void __osInitialize_autodetect();
-// ? __ull_rshift(?);
-// ? __ull_rem(?);
-// ? __ull_div(?);
-// ? __ll_lshift(?);
-// ? __ll_rem(?);
-// ? __ll_div(?);
-// ? __ll_mul(?);
-// ? __ull_divremi(?);
-// ? __ll_mod(?);
-// ? __ll_rshift(?);
 void __osExceptionPreamble();
 // ? __osException(?);
 void __osEnqueueAndYield(OSThread**);
@@ -801,9 +791,9 @@ void SaveContext_Init(void);
 void func_8006375C(s32 arg0, s32 arg1, float* d_80855320);
 // ? func_8006376C(?);
 // ? func_80063828(?);
-// ? func_8006390C(?);
+void func_8006390C(Input* input);
 // ? func_80063C04(?);
-// ? func_80063D7C(?);
+void func_80063D7C(GraphicsContext* gfxCtx);
 void DebugDisplay_Init(void);
 DebugDispObject* DebugDisplay_AddObject(f32 posX, f32 posY, f32 posZ, s16 rotX, s16 rotY, s16 rotZ, f32 scaleX,
                                         f32 scaleY, f32 scaleZ, u8 red, u8 green, u8 blue, u8 alpha, s16 type,
@@ -1316,15 +1306,15 @@ s32 func_800ABE74(f32 eyeX, f32 eyeY, f32 eyeZ);
 // ? func_800ACAF8(?);
 // ? func_800ACE70(?);
 // ? func_800ACE90(?);
-// ? func_800ACE98(?);
+void func_800ACE98(struct_801664F0*, Gfx**);
 // ? func_800AD000(?);
 // ? func_800AD054(?);
 // ? func_800AD080(?);
 // ? func_800AD394(?);
-void func_800AD5C0(UNK_PTR, Gfx**);
+void func_800AD5C0(VisMonoStruct*, Gfx**);
 // ? func_800AD920(?);
 // ? func_800AD950(?);
-// ? func_800AD958(?);
+void func_800AD958(struct_801664F0*, Gfx**);
 // ? func_800ADBB0(?);
 // ? func_800AE2C0(?);
 // ? func_800AEFC8(?);
@@ -1524,22 +1514,22 @@ void AudioMgr_Start(AudioMgr* audioMgr, void* stack, OSPri pri, OSId id, SchedCo
 void TitleSetup_InitImpl(GameState* gameState);
 void TitleSetup_Destroy(GameState* gameState);
 void TitleSetup_Init(GameState* gameState);
-// ? func_800C4130(?);
-// ? func_800C41F0(?);
+void GameState_FaultPrint(void);
+void GameState_SetFBFilter(Gfx** gfx);
 // ? func_800C4344(?);
-// ? func_800C4558(?);
-// ? func_800C46EC(?);
-// ? func_800C48CC(?);
+void GameState_DrawInputDisplay(u16 input, Gfx** gfx);
+void GameState_Draw(GameState* gameState, GraphicsContext* gfxCtx);
+void GameState_SetFrameBuffer(GraphicsContext* gfxCtx);
 // ? func_800C49F4(?);
-// ? func_800C4A98(?);
-// ? func_800C4AC8(?);
-// ? func_800C4E80(?);
-// ? func_800C4F20(?);
-// ? func_800C5080(?);
-// ? func_800C5360(?);
-// ? func_800C546C(?);
-// ? func_800C547C(?);
-void* Game_Alloc(GameState* gameState, size_t size, char* file, s32 line);
+void GameState_ReqPadData(GameState* gameState);
+void GameState_Update(GameState* gameState);
+void GameState_InitArena(GameState* gameState, size_t size);
+void GameState_Realloc(GameState* gameState, size_t size);
+void GameState_Init(GameState* gameState, GameStateFunc init, GraphicsContext* gfxCtx);
+void GameState_Destroy(GameState* gameState);
+GameStateFunc GameState_GetInit(GameState* gameState);
+u32 GameState_IsRunning(GameState* gameState);
+void* GameState_AllocEnd(GameState* gameState, size_t size, char* file, s32 line);
 void func_800C55D0(GameAlloc* this);
 void* GameAlloc_MallocDebug(GameAlloc* this, u32 size, const char* file, s32 line);
 void* GameAlloc_Malloc(GameAlloc* this, u32 size);
@@ -1550,7 +1540,7 @@ void Graph_FaultClient();
 void Graph_DisassembleUCode(void* arg0);
 void Graph_UCodeFaultClient(void* arg0);
 void* Graph_InitTHGA(GraphicsContext* gfxCtx);
-GameStateOverlay* Graph_GetNextGameState();
+GameStateOverlay* Graph_GetNextGameState(GameState* gameState);
 void Graph_Init(GraphicsContext* gfxCtx);
 void Graph_Destroy(GraphicsContext* gfxCtx);
 void Graph_TaskSet00(GraphicsContext* gfxCtx);

Dragorn421/oot

include/functions.h
@@ -80,16 +80,6 @@ s32 osRecvMesg(OSMesgQueue* mq, OSMesg* msg, s32 flag);
 void __createSpeedParam(void);
 void __osInitialize_common(void);
 void __osInitialize_autodetect();
-// ? __ull_rshift(?);
-// ? __ull_rem(?);
-// ? __ull_div(?);
-// ? __ll_lshift(?);
-// ? __ll_rem(?);
-// ? __ll_div(?);
-// ? __ll_mul(?);
-// ? __ull_divremi(?);
-// ? __ll_mod(?);
-// ? __ll_rshift(?);
 void __osExceptionPreamble();
 // ? __osException(?);
 void __osEnqueueAndYield(OSThread**);
@@ -764,9 +754,9 @@ void func_800636C0_cc0(void);
 void func_8006375C_cc0(UNK_TYPE, UNK_TYPE, UNK_TYPE);
 // ? func_8006376C_cc0(?);
 // ? func_80063828_cc0(?);
-void func_8006390C_cc0(struct_8006390C_arg0* arg0);
+void func_8006390C_cc0(Input* input);
 // ? func_80063C04_cc0(?);
-void func_80063D7C_cc0(GraphicsContext* arg0);
+void func_80063D7C_cc0(GraphicsContext* gfxCtx);
 void DebugDisplay_Init(void);
 DebugDispObject* DebugDisplay_AddObject(f32 posX, f32 posY, f32 posZ, s16 rotX, s16 rotY, s16 rotZ, f32 scaleX,
                                         f32 scaleY, f32 scaleZ, u8 red, u8 green, u8 blue, u8 alpha, s16 type,
@@ -1279,15 +1269,15 @@ s32 func_800ABE74(f32 eyeX, f32 eyeY, f32 eyeZ);
 // ? func_800ACAF8(?);
 // ? func_800ACE70(?);
 // ? func_800ACE90(?);
-// ? func_800ACE98(?);
+void func_800ACE98(struct_801664F0*, Gfx**);
 // ? func_800AD000(?);
 // ? func_800AD054(?);
 // ? func_800AD080(?);
 // ? func_800AD394(?);
-void func_800AD5C0(UNK_PTR, Gfx**);
+void func_800AD5C0(VisMonoStruct*, Gfx**);
 // ? func_800AD920(?);
 // ? func_800AD950(?);
-// ? func_800AD958(?);
+void func_800AD958(struct_801664F0*, Gfx**);
 // ? func_800ADBB0(?);
 // ? func_800AE2C0(?);
 // ? func_800AEFC8(?);
@@ -1487,22 +1477,22 @@ void AudioMgr_Start(AudioMgr* audioMgr, void* stack, OSPri pri, OSId id, SchedCo
 void TitleSetup_InitImpl(GameState* gameState);
 void TitleSetup_Destroy(GameState* gameState);
 void TitleSetup_Init(GameState* gameState);
-// ? func_800C4130(?);
-// ? func_800C41F0(?);
+void GameState_FaultPrint(void);
+void GameState_SetFBFilter(Gfx** gfx);
 // ? func_800C4344(?);
-// ? func_800C4558(?);
-// ? func_800C46EC(?);
-// ? func_800C48CC(?);
+void GameState_DrawInputDisplay(u16 input, Gfx** gfx);
+void GameState_Draw(GameState* gameState, GraphicsContext* gfxCtx);
+void GameState_SetFrameBuffer(GraphicsContext* gfxCtx);
 // ? func_800C49F4(?);
-// ? func_800C4A98(?);
-// ? func_800C4AC8(?);
-// ? func_800C4E80(?);
-// ? func_800C4F20(?);
-// ? func_800C5080(?);
-// ? func_800C5360(?);
-// ? func_800C546C(?);
-// ? func_800C547C(?);
-void* Game_Alloc(GameState* gameState, size_t size, char* file, s32 line);
+void GameState_ReqPadData(GameState* gameState);
+void GameState_Update(GameState* gameState);
+void GameState_InitArena(GameState* gameState, size_t size);
+void GameState_Realloc(GameState* gameState, size_t size);
+void GameState_Init(GameState* gameState, GameStateFunc init, GraphicsContext* gfxCtx);
+void GameState_Destroy(GameState* gameState);
+GameStateFunc GameState_GetInit(GameState* gameState);
+u32 GameState_IsRunning(GameState* gameState);
+void* GameState_AllocEnd(GameState* gameState, size_t size, char* file, s32 line);
 void func_800C55D0(GameAlloc* this);
 void* GameAlloc_MallocDebug(GameAlloc* this, u32 size, const char* file, s32 line);
 void* GameAlloc_Malloc(GameAlloc* this, u32 size);
@@ -1513,7 +1503,7 @@ void Graph_FaultClient();
 void Graph_DisassembleUCode(void* arg0);
 void Graph_UCodeFaultClient(void* arg0);
 void* Graph_InitTHGA(GraphicsContext* gfxCtx);
-GameStateOverlay* Graph_GetNextGameState();
+GameStateOverlay* Graph_GetNextGameState(GameState* gameState);
 void Graph_Init(GraphicsContext* gfxCtx);
 void Graph_Destroy(GraphicsContext* gfxCtx);
 void Graph_TaskSet00(GraphicsContext* gfxCtx);