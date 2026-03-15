HackerN64/HackerOoT

More actor cleanup Part 2 (#116)
* Change all main actor functions to take a 'Actor* thisx' argument

* Change all actor callbacks to also take a 'Actor* thisx' argument

src/overlays/actors/ovl_Bg_Toki_Swd/z_bg_toki_swd.c
@@ -8,10 +8,13 @@
 
 #define FLAGS 0x00000010
 
-void BgTokiSwd_Init(BgTokiSwd* this, GlobalContext* globalCtx);
-void BgTokiSwd_Destroy(BgTokiSwd* this, GlobalContext* globalCtx);
-void BgTokiSwd_Update(BgTokiSwd* this, GlobalContext* globalCtx);
-void BgTokiSwd_Draw(BgTokiSwd* this, GlobalContext* globalCtx);
+#define THIS ((BgTokiSwd*)thisx)
+
+void BgTokiSwd_Init(Actor* thisx, GlobalContext* globalCtx);
+void BgTokiSwd_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void BgTokiSwd_Update(Actor* thisx, GlobalContext* globalCtx);
+void BgTokiSwd_Draw(Actor* thisx, GlobalContext* globalCtx);
+
 void BgTokiSwd_SetupAction(BgTokiSwd* this, ActorFunc actionFunc);
 void func_808BAF40(BgTokiSwd* this, GlobalContext* globalCtx);
 void func_808BB0AC(BgTokiSwd* this, GlobalContext* globalCtx);
@@ -182,14 +185,14 @@ void BgTokiSwd_SetupAction(BgTokiSwd* this, ActorFunc actionFunc) {
     this->actionFunc = actionFunc;
 }
 
-void BgTokiSwd_Init(BgTokiSwd* this, GlobalContext* globalCtx) {
-    ColliderCylinder* collision;
-    Actor* thisx = &this->actor;
+void BgTokiSwd_Init(Actor* thisx, GlobalContext* globalCtx) {
+    BgTokiSwd* this = THIS;
+    s32 pad;
 
     Actor_ProcessInitChain(thisx, initChain);
     this->actor.shape.unk_08 = 800.0f;
     BgTokiSwd_SetupAction(thisx, func_808BAF40);
-    collision = &this->collider;
+
     if (LINK_IS_ADULT) {
         thisx->draw = NULL;
     }
@@ -198,15 +201,16 @@ void BgTokiSwd_Init(BgTokiSwd* this, GlobalContext* globalCtx) {
         globalCtx->unk_11D30[0] = 0xFF;
     }
 
-    Collider_InitCylinder(globalCtx, collision);
-    Collider_SetCylinder(globalCtx, collision, thisx, &colliderInit);
-    Collider_CylinderUpdate(thisx, collision);
+    Collider_InitCylinder(globalCtx, &this->collider);
+    Collider_SetCylinder(globalCtx, &this->collider, thisx, &colliderInit);
+    Collider_CylinderUpdate(thisx, &this->collider);
     func_80061ED4(&thisx->colChkInfo, 0, &colChkInfoInit);
 }
 
-void BgTokiSwd_Destroy(BgTokiSwd* this, GlobalContext* globalCtx) {
-    ColliderCylinder* collider = &this->collider;
-    Collider_DestroyCylinder(globalCtx, collider);
+void BgTokiSwd_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    BgTokiSwd* this = THIS;
+
+    Collider_DestroyCylinder(globalCtx, &this->collider);
 }
 
 void func_808BAF40(BgTokiSwd* this, GlobalContext* globalCtx) {
@@ -268,16 +272,20 @@ void func_808BB128(BgTokiSwd* this, GlobalContext* globalCtx) {
     }
 }
 
-void BgTokiSwd_Update(BgTokiSwd* this, GlobalContext* globalCtx) {
+void BgTokiSwd_Update(Actor* thisx, GlobalContext* globalCtx) {
+    BgTokiSwd* this = THIS;
+
     this->actionFunc(this, globalCtx);
     CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->collider);
 }
 
-void BgTokiSwd_Draw(BgTokiSwd* this, GlobalContext* globalCtx) {
-    s32 pad[4];
+void BgTokiSwd_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    BgTokiSwd* this = THIS;
+    s32 pad[3];
     GameState* state;
     GraphicsContext* gfxCtx;
     Gfx* dispRefs[4];
+
     state = &globalCtx->state;
     gfxCtx = globalCtx->state.gfxCtx;
 

src/overlays/actors/ovl_Door_Ana/z_door_ana.c
@@ -8,10 +8,12 @@
 
 #define FLAGS 0x02000000
 
-void DoorAna_Init(DoorAna* this, GlobalContext* globalCtx);
-void DoorAna_Destroy(DoorAna* this, GlobalContext* globalCtx);
-void DoorAna_Update(DoorAna* this, GlobalContext* globalCtx);
-void DoorAna_Draw(DoorAna* this, GlobalContext* globalCtx);
+#define THIS ((DoorAna*)thisx)
+
+void DoorAna_Init(Actor* thisx, GlobalContext* globalCtx);
+void DoorAna_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void DoorAna_Update(Actor* thisx, GlobalContext* globalCtx);
+void DoorAna_Draw(Actor* thisx, GlobalContext* globalCtx);
 
 void DoorAna_SetupAction(DoorAna* this, ActorFunc func);
 
@@ -52,8 +54,8 @@ void DoorAna_SetupAction(DoorAna* this, ActorFunc func) {
     this->actionFunc = func;
 }
 
-void DoorAna_Init(DoorAna* this, GlobalContext* globalCtx) {
-    ColliderCylinder* collider;
+void DoorAna_Init(Actor* thisx, GlobalContext* globalCtx) {
+    DoorAna* this = THIS;
 
     this->actor.shape.rot.z = 0;
     this->actor.shape.rot.y = this->actor.shape.rot.z;
@@ -61,9 +63,8 @@ void DoorAna_Init(DoorAna* this, GlobalContext* globalCtx) {
     if ((this->actor.params & 0x300) != 0) {
         // only allocate collider for grottos that need bombing/hammering open
         if ((this->actor.params & 0x200) != 0) {
-            collider = &this->collider;
-            Collider_InitCylinder(globalCtx, collider);
-            Collider_SetCylinder(globalCtx, collider, &this->actor, &colliderInit);
+            Collider_InitCylinder(globalCtx, &this->collider);
+            Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &colliderInit);
         } else {
             this->actor.flags |= 0x10;
         }
@@ -75,7 +76,9 @@ void DoorAna_Init(DoorAna* this, GlobalContext* globalCtx) {
     this->actor.unk_1F = 0;
 }
 
-void DoorAna_Destroy(DoorAna* this, GlobalContext* globalCtx) {
+void DoorAna_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    DoorAna* this = THIS;
+
     // free collider if it has one
     if ((this->actor.params & 0x200) != 0) {
         Collider_DestroyCylinder(globalCtx, &this->collider);
@@ -84,7 +87,7 @@ void DoorAna_Destroy(DoorAna* this, GlobalContext* globalCtx) {
 
 // update routine for grottos that are currently "hidden"/unopened
 void DoorAna_Update_Hidden(DoorAna* this, GlobalContext* globalCtx) {
-    bool openGrotto = false;
+    u32 openGrotto = false;
     if ((this->actor.params & 0x200) == 0) {
         // opening with song of storms
         if (this->actor.waterSurfaceDist < 40000.0f && Flags_GetEnv(globalCtx, 5)) {
@@ -154,13 +157,15 @@ void DoorAna_Update_Entering(DoorAna* this, GlobalContext* globalCtx) {
     }
 }
 
-void DoorAna_Update(DoorAna* this, GlobalContext* globalCtx) {
+void DoorAna_Update(Actor* thisx, GlobalContext* globalCtx) {
+    DoorAna* this = THIS;
+
     this->actionFunc(this, globalCtx);
     // changes the grottos facing angle based on camera angle
     this->actor.shape.rot.y = func_8005A9F4(globalCtx->cameraPtrs[globalCtx->activeCamera]) + 0x8000;
 }
 
-void DoorAna_Draw(DoorAna* this, GlobalContext* globalCtx) {
+void DoorAna_Draw(Actor* thisx, GlobalContext* globalCtx) {
     GraphicsContext* gfxCtx = globalCtx->state.gfxCtx;
     Gfx* dispRefs[4];
 

src/overlays/actors/ovl_En_Ani/z_en_ani.c
@@ -8,9 +8,14 @@
 
 #define FLAGS 0x00000009
 
+#define THIS ((EnAni*)thisx)
+
+void EnAni_Init(Actor* thisx, GlobalContext* globalCtx);
+void EnAni_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void EnAni_Update(Actor* thisx, GlobalContext* globalCtx);
+void EnAni_Draw(Actor* thisx, GlobalContext* globalCtx);
+
 void EnAni_SetupAction(EnAni* this, ActorFunc actionFunc);
-void EnAni_Init(EnAni* this, GlobalContext* globalCtx);
-void EnAni_Destroy(EnAni* this, GlobalContext* globalCtx);
 s32 EnAni_SetText(EnAni* this, GlobalContext* globalCtx, u16 textId);
 void func_809B04F0(EnAni* this, GlobalContext* globalCtx);
 void func_809B0524(EnAni* this, GlobalContext* globalCtx);
@@ -22,10 +27,6 @@ void func_809B0988(EnAni* this, GlobalContext* globalCtx);
 void func_809B0994(EnAni* this, GlobalContext* globalCtx);
 void func_809B0A28(EnAni* this, GlobalContext* globalCtx);
 void func_809B0A6C(EnAni* this, GlobalContext* globalCtx);
-void EnAni_Update(EnAni* this, GlobalContext* globalCtx);
-s32 EnAni_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, EnAni* enAni);
-void EnAni_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, EnAni* enAni);
-void EnAni_Draw(EnAni* this, GlobalContext* globalCtx);
 
 extern SkeletonHeader D_060000F0;
 extern AnimationHeader D_060067B8;
@@ -67,16 +68,15 @@ void EnAni_SetupAction(EnAni* this, ActorFunc actionFunc) {
     this->actionFunc = actionFunc;
 }
 
-void EnAni_Init(EnAni* this, GlobalContext* globalCtx) {
-    AnimationHeader* anim;
-    u32 pad;
+void EnAni_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnAni* this = THIS;
+    s32 pad;
 
-    anim = &D_060076EC;
     Actor_ProcessInitChain(&this->actor, initChain);
     ActorShape_Init(&this->actor.shape, -2800.0f, ActorShadow_DrawFunc_Circle, 36.0f);
-    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_060000F0, anim, this->limbDrawTable, this->transitionDrawTable,
-                     0x10);
-    SkelAnime_ChangeAnimDefaultStop(&this->skelAnime, anim);
+    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_060000F0, &D_060076EC, this->limbDrawTable,
+                     this->transitionDrawTable, 0x10);
+    SkelAnime_ChangeAnimDefaultStop(&this->skelAnime, &D_060076EC);
     Collider_InitCylinder(globalCtx, &this->collider);
     Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &cylinderInitData);
     this->actor.colChkInfo.mass = 0xFF;
@@ -91,9 +91,9 @@ void EnAni_Init(EnAni* this, GlobalContext* globalCtx) {
     this->actor.velocity.y = -1.0f;
 }
 
-void EnAni_Destroy(EnAni* this, GlobalContext* globalCtx) {
-    ColliderCylinder* collider;
-    collider = &this->collider;
+void EnAni_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    EnAni* this = THIS;
+
     Collider_DestroyCylinder(globalCtx, &this->collider);
 }
 
@@ -238,14 +238,12 @@ void func_809B0A6C(EnAni* this, GlobalContext* globalCtx) {
     }
 }
 
-void EnAni_Update(EnAni* this, GlobalContext* globalCtx) {
-    ColliderCylinder* collider;
-    u32 pad;
-    u32 pad2;
+void EnAni_Update(Actor* thisx, GlobalContext* globalCtx) {
+    EnAni* this = THIS;
+    s32 pad[2];
 
-    collider = &this->collider;
-    Collider_CylinderUpdate(&this->actor, collider);
-    CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, collider);
+    Collider_CylinderUpdate(&this->actor, &this->collider);
+    CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->collider);
     Actor_MoveForward(&this->actor);
     func_8002E4B4(globalCtx, &this->actor, 0.0f, 0.0f, 0.0f, 4);
     if ((globalCtx->csCtx.state != 0) && (globalCtx->csCtx.actorActions[0] != NULL)) {
@@ -298,36 +296,34 @@ void EnAni_Update(EnAni* this, GlobalContext* globalCtx) {
     }
 }
 
-s32 EnAni_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, EnAni* enAni) {
-    EnAni* temp;
+s32 EnAni_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx) {
+    EnAni* this = THIS;
 
-    temp = enAni;
     if (limbIndex == 15) {
-        rot->x += temp->unk_29C.y;
-        rot->z += temp->unk_29C.x;
+        rot->x += this->unk_29C.y;
+        rot->z += this->unk_29C.x;
     }
     return 0;
 }
 
-void EnAni_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, EnAni* enAni) {
+void EnAni_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx) {
     if (limbIndex == 15) {
-        Matrix_MultVec3f(&EnAniVec, &enAni->actor.posRot2.pos);
+        Matrix_MultVec3f(&EnAniVec, &thisx->posRot2.pos);
     }
 }
 
-void EnAni_Draw(EnAni* this, GlobalContext* globalCtx) {
-    UNK_PTR* temp;
-    u32 pad;
+void EnAni_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    EnAni* this = THIS;
+    s32 pad;
     GraphicsContext* gfxCtx;
     Gfx* dispRefs[4];
 
-    temp = D_809B0F80;
     gfxCtx = globalCtx->state.gfxCtx;
 
     Graph_OpenDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_ani.c", 719);
     func_800943C8(globalCtx->state.gfxCtx);
 
-    gSPSegment(gfxCtx->polyOpa.p++, 0x08, SEGMENTED_TO_VIRTUAL(temp[this->unk_2AC]));
+    gSPSegment(gfxCtx->polyOpa.p++, 0x08, SEGMENTED_TO_VIRTUAL(D_809B0F80[this->unk_2AC]));
 
     SkelAnime_DrawSV(globalCtx, this->skelAnime.skeleton, this->skelAnime.limbDrawTbl, this->skelAnime.dListCount,
                      EnAni_OverrideLimbDraw, EnAni_PostLimbDraw, &this->actor);

Dragorn421/oot

src/overlays/actors/ovl_Bg_Toki_Swd/z_bg_toki_swd.c
@@ -8,10 +8,13 @@
 
 #define FLAGS 0x00000010
 
-void BgTokiSwd_Init(BgTokiSwd* this, GlobalContext* globalCtx);
-void BgTokiSwd_Destroy(BgTokiSwd* this, GlobalContext* globalCtx);
-void BgTokiSwd_Update(BgTokiSwd* this, GlobalContext* globalCtx);
-void BgTokiSwd_Draw(BgTokiSwd* this, GlobalContext* globalCtx);
+#define THIS ((BgTokiSwd*)thisx)
+
+void BgTokiSwd_Init(Actor* thisx, GlobalContext* globalCtx);
+void BgTokiSwd_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void BgTokiSwd_Update(Actor* thisx, GlobalContext* globalCtx);
+void BgTokiSwd_Draw(Actor* thisx, GlobalContext* globalCtx);
+
 void BgTokiSwd_SetupAction(BgTokiSwd* this, ActorFunc actionFunc);
 void func_808BAF40(BgTokiSwd* this, GlobalContext* globalCtx);
 void func_808BB0AC(BgTokiSwd* this, GlobalContext* globalCtx);
@@ -198,7 +201,7 @@ static ColliderCylinderSrc colliderInit = {
     },
 };
 
-static CollideDataInit sub98Init = {
+static CollideDataInit collideDataInit = {
     0x0A,
     0x0023,
     0x0064,
@@ -213,14 +216,14 @@ void BgTokiSwd_SetupAction(BgTokiSwd* this, ActorFunc actionFunc) {
     this->actionFunc = actionFunc;
 }
 
-void BgTokiSwd_Init(BgTokiSwd* this, GlobalContext* globalCtx) {
-    ColliderCylinder* collision;
-    Actor* thisx = &this->actor;
+void BgTokiSwd_Init(Actor* thisx, GlobalContext* globalCtx) {
+    BgTokiSwd* this = THIS;
+    s32 pad;
 
     Actor_ProcessInitChain(thisx, initChain);
     this->actor.shape.unk_08 = 800.0f;
     BgTokiSwd_SetupAction(thisx, func_808BAF40);
-    collision = &this->collider;
+
     if (LINK_IS_ADULT) {
         thisx->draw = NULL;
     }
@@ -229,15 +232,16 @@ void BgTokiSwd_Init(BgTokiSwd* this, GlobalContext* globalCtx) {
         globalCtx->unk_11D30[0] = 0xFF;
     }
 
-    Collider_InitCylinder(globalCtx, collision);
-    Collider_LoadCylinder(globalCtx, collision, thisx, &colliderInit);
-    Collider_UpdateCylinderShape(thisx, collision);
-    func_80061ED4(&thisx->collideData, 0, &sub98Init);
+    Collider_InitCylinder(globalCtx, &this->collider);
+    Collider_LoadCylinder(globalCtx, &this->collider, thisx, &colliderInit);
+    Collider_UpdateCylinderShape(thisx, &this->collider);
+    func_80061ED4(&thisx->collideData, 0, &collideDataInit);
 }
 
-void BgTokiSwd_Destroy(BgTokiSwd* this, GlobalContext* globalCtx) {
-    ColliderCylinder* collider = &this->collider;
-    Collider_DestroyCylinder(globalCtx, collider);
+void BgTokiSwd_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    BgTokiSwd* this = THIS;
+
+    Collider_DestroyCylinder(globalCtx, &this->collider);
 }
 
 void func_808BAF40(BgTokiSwd* this, GlobalContext* globalCtx) {
@@ -299,16 +303,20 @@ void func_808BB128(BgTokiSwd* this, GlobalContext* globalCtx) {
     }
 }
 
-void BgTokiSwd_Update(BgTokiSwd* this, GlobalContext* globalCtx) {
+void BgTokiSwd_Update(Actor* thisx, GlobalContext* globalCtx) {
+    BgTokiSwd* this = THIS;
+
     this->actionFunc(this, globalCtx);
     Collider_AddOC(globalCtx, &globalCtx->colliderCtx, &this->collider);
 }
 
-void BgTokiSwd_Draw(BgTokiSwd* this, GlobalContext* globalCtx) {
-    s32 pad[4];
+void BgTokiSwd_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    BgTokiSwd* this = THIS;
+    s32 pad[3];
     GameState* state;
     GraphicsContext* gfxCtx;
     Gfx* dispRefs[4];
+
     state = &globalCtx->state;
     gfxCtx = globalCtx->state.gfxCtx;
 

src/overlays/actors/ovl_Door_Ana/z_door_ana.c
@@ -8,10 +8,12 @@
 
 #define FLAGS 0x02000000
 
-void DoorAna_Init(DoorAna* this, GlobalContext* globalCtx);
-void DoorAna_Destroy(DoorAna* this, GlobalContext* globalCtx);
-void DoorAna_Update(DoorAna* this, GlobalContext* globalCtx);
-void DoorAna_Draw(DoorAna* this, GlobalContext* globalCtx);
+#define THIS ((DoorAna*)thisx)
+
+void DoorAna_Init(Actor* thisx, GlobalContext* globalCtx);
+void DoorAna_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void DoorAna_Update(Actor* thisx, GlobalContext* globalCtx);
+void DoorAna_Draw(Actor* thisx, GlobalContext* globalCtx);
 
 void DoorAna_SetupAction(DoorAna* this, ActorFunc func);
 
@@ -83,8 +85,8 @@ void DoorAna_SetupAction(DoorAna* this, ActorFunc func) {
     this->actionFunc = func;
 }
 
-void DoorAna_Init(DoorAna* this, GlobalContext* globalCtx) {
-    ColliderCylinder* collider;
+void DoorAna_Init(Actor* thisx, GlobalContext* globalCtx) {
+    DoorAna* this = THIS;
 
     this->actor.shape.rot.z = 0;
     this->actor.shape.rot.y = this->actor.shape.rot.z;
@@ -92,9 +94,8 @@ void DoorAna_Init(DoorAna* this, GlobalContext* globalCtx) {
     if ((this->actor.params & 0x300) != 0) {
         // only allocate collider for grottos that need bombing/hammering open
         if ((this->actor.params & 0x200) != 0) {
-            collider = &this->collider;
-            Collider_InitCylinder(globalCtx, collider);
-            Collider_LoadCylinder(globalCtx, collider, &this->actor, &colliderInit);
+            Collider_InitCylinder(globalCtx, &this->collider);
+            Collider_LoadCylinder(globalCtx, &this->collider, &this->actor, &colliderInit);
         } else {
             this->actor.flags |= 0x10;
         }
@@ -106,7 +107,9 @@ void DoorAna_Init(DoorAna* this, GlobalContext* globalCtx) {
     this->actor.unk_1F = 0;
 }
 
-void DoorAna_Destroy(DoorAna* this, GlobalContext* globalCtx) {
+void DoorAna_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    DoorAna* this = THIS;
+
     // free collider if it has one
     if ((this->actor.params & 0x200) != 0) {
         Collider_DestroyCylinder(globalCtx, &this->collider);
@@ -115,7 +118,7 @@ void DoorAna_Destroy(DoorAna* this, GlobalContext* globalCtx) {
 
 // update routine for grottos that are currently "hidden"/unopened
 void DoorAna_Update_Hidden(DoorAna* this, GlobalContext* globalCtx) {
-    bool openGrotto = false;
+    u32 openGrotto = false;
     if ((this->actor.params & 0x200) == 0) {
         // opening with song of storms
         if (this->actor.waterSurfaceDist < 40000.0f && Flags_GetEnv(globalCtx, 5)) {
@@ -185,13 +188,15 @@ void DoorAna_Update_Entering(DoorAna* this, GlobalContext* globalCtx) {
     }
 }
 
-void DoorAna_Update(DoorAna* this, GlobalContext* globalCtx) {
+void DoorAna_Update(Actor* thisx, GlobalContext* globalCtx) {
+    DoorAna* this = THIS;
+
     this->actionFunc(this, globalCtx);
     // changes the grottos facing angle based on camera angle
     this->actor.shape.rot.y = func_8005A9F4(globalCtx->cameraPtrs[globalCtx->activeCamera]) + 0x8000;
 }
 
-void DoorAna_Draw(DoorAna* this, GlobalContext* globalCtx) {
+void DoorAna_Draw(Actor* thisx, GlobalContext* globalCtx) {
     GraphicsContext* gfxCtx = globalCtx->state.gfxCtx;
     Gfx* dispRefs[4];
 

src/overlays/actors/ovl_En_Ani/z_en_ani.c
@@ -8,9 +8,14 @@
 
 #define FLAGS 0x00000009
 
+#define THIS ((EnAni*)thisx)
+
+void EnAni_Init(Actor* thisx, GlobalContext* globalCtx);
+void EnAni_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void EnAni_Update(Actor* thisx, GlobalContext* globalCtx);
+void EnAni_Draw(Actor* thisx, GlobalContext* globalCtx);
+
 void EnAni_SetupAction(EnAni* this, ActorFunc actionFunc);
-void EnAni_Init(EnAni* this, GlobalContext* globalCtx);
-void EnAni_Destroy(EnAni* this, GlobalContext* globalCtx);
 s32 EnAni_SetText(EnAni* this, GlobalContext* globalCtx, u16 textId);
 void func_809B04F0(EnAni* this, GlobalContext* globalCtx);
 void func_809B0524(EnAni* this, GlobalContext* globalCtx);
@@ -22,10 +27,6 @@ void func_809B0988(EnAni* this, GlobalContext* globalCtx);
 void func_809B0994(EnAni* this, GlobalContext* globalCtx);
 void func_809B0A28(EnAni* this, GlobalContext* globalCtx);
 void func_809B0A6C(EnAni* this, GlobalContext* globalCtx);
-void EnAni_Update(EnAni* this, GlobalContext* globalCtx);
-s32 EnAni_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, EnAni* enAni);
-void EnAni_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, EnAni* enAni);
-void EnAni_Draw(EnAni* this, GlobalContext* globalCtx);
 
 extern SkeletonHeader D_060000F0;
 extern AnimationHeader D_060067B8;
@@ -98,16 +99,15 @@ void EnAni_SetupAction(EnAni* this, ActorFunc actionFunc) {
     this->actionFunc = actionFunc;
 }
 
-void EnAni_Init(EnAni* this, GlobalContext* globalCtx) {
-    AnimationHeader* anim;
-    u32 pad;
+void EnAni_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnAni* this = THIS;
+    s32 pad;
 
-    anim = &D_060076EC;
     Actor_ProcessInitChain(&this->actor, initChain);
     ActorShape_Init(&this->actor.shape, -2800.0f, ActorShadow_DrawFunc_Circle, 36.0f);
-    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_060000F0, anim, this->limbDrawTable, this->transitionDrawTable,
-                     0x10);
-    SkelAnime_ChangeAnimDefaultStop(&this->skelAnime, anim);
+    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_060000F0, &D_060076EC, this->limbDrawTable,
+                     this->transitionDrawTable, 0x10);
+    SkelAnime_ChangeAnimDefaultStop(&this->skelAnime, &D_060076EC);
     Collider_InitCylinder(globalCtx, &this->collider);
     Collider_LoadCylinder(globalCtx, &this->collider, &this->actor, &cylinderInitData);
     this->actor.collideData.mass = 0xFF;
@@ -122,9 +122,9 @@ void EnAni_Init(EnAni* this, GlobalContext* globalCtx) {
     this->actor.velocity.y = -1.0f;
 }
 
-void EnAni_Destroy(EnAni* this, GlobalContext* globalCtx) {
-    ColliderCylinder* collider;
-    collider = &this->collider;
+void EnAni_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    EnAni* this = THIS;
+
     Collider_DestroyCylinder(globalCtx, &this->collider);
 }
 
@@ -269,14 +269,12 @@ void func_809B0A6C(EnAni* this, GlobalContext* globalCtx) {
     }
 }
 
-void EnAni_Update(EnAni* this, GlobalContext* globalCtx) {
-    ColliderCylinder* collider;
-    u32 pad;
-    u32 pad2;
+void EnAni_Update(Actor* thisx, GlobalContext* globalCtx) {
+    EnAni* this = THIS;
+    s32 pad[2];
 
-    collider = &this->collider;
-    Collider_UpdateCylinderShape(&this->actor, collider);
-    Collider_AddOC(globalCtx, &globalCtx->colliderCtx, collider);
+    Collider_UpdateCylinderShape(&this->actor, &this->collider);
+    Collider_AddOC(globalCtx, &globalCtx->colliderCtx, &this->collider);
     Actor_MoveForward(&this->actor);
     func_8002E4B4(globalCtx, &this->actor, 0.0f, 0.0f, 0.0f, 4);
     if ((globalCtx->csCtx.state != 0) && (globalCtx->csCtx.actorActions[0] != NULL)) {
@@ -329,36 +327,34 @@ void EnAni_Update(EnAni* this, GlobalContext* globalCtx) {
     }
 }
 
-s32 EnAni_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, EnAni* enAni) {
-    EnAni* temp;
+s32 EnAni_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx) {
+    EnAni* this = THIS;
 
-    temp = enAni;
     if (limbIndex == 15) {
-        rot->x += temp->unk_29C.y;
-        rot->z += temp->unk_29C.x;
+        rot->x += this->unk_29C.y;
+        rot->z += this->unk_29C.x;
     }
     return 0;
 }
 
-void EnAni_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, EnAni* enAni) {
+void EnAni_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx) {
     if (limbIndex == 15) {
-        Matrix_MultVec3f(&EnAniVec, &enAni->actor.posRot2.pos);
+        Matrix_MultVec3f(&EnAniVec, &thisx->posRot2.pos);
     }
 }
 
-void EnAni_Draw(EnAni* this, GlobalContext* globalCtx) {
-    UNK_PTR* temp;
-    u32 pad;
+void EnAni_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    EnAni* this = THIS;
+    s32 pad;
     GraphicsContext* gfxCtx;
     Gfx* dispRefs[4];
 
-    temp = D_809B0F80;
     gfxCtx = globalCtx->state.gfxCtx;
 
     Graph_OpenDisps(dispRefs, globalCtx->state.gfxCtx, "../z_en_ani.c", 719);
     func_800943C8(globalCtx->state.gfxCtx);
 
-    gSPSegment(gfxCtx->polyOpa.p++, 0x08, SEGMENTED_TO_VIRTUAL(temp[this->unk_2AC]));
+    gSPSegment(gfxCtx->polyOpa.p++, 0x08, SEGMENTED_TO_VIRTUAL(D_809B0F80[this->unk_2AC]));
 
     SkelAnime_DrawSV(globalCtx, this->skelAnime.skeleton, this->skelAnime.limbDrawTbl, this->skelAnime.dListCount,
                      EnAni_OverrideLimbDraw, EnAni_PostLimbDraw, &this->actor);