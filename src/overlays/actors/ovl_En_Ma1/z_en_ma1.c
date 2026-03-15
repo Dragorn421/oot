HackerN64/HackerOoT

Decompiled ovl_En_Ma2 (#93)
* first steps

* some more

* fixing merge

* more progress

* progs

* Decompiled ovl_En_Ma1

* newline

* Pr suggestion

* 2 to go

* Makefile machinery work

* Updating Jenkinsfile

* Initial setup for OSX repo support

* Adding ido7 to repo

* Makefile reversion

* Iniital Dockerfile

* iconv fix

* Docker stuff

* Dun

* Fixing stuff

* Updating Dockerfile

* Update README-OSX.md

* Restore functions.h

* Formatting

* Defining some funcs

* Renaming funcs

* Removing unnecessary README-OSX

* Addressing PR comments

src/overlays/actors/ovl_En_Ma1/z_en_ma1.c
@@ -37,7 +37,7 @@ const ActorInit En_Ma1_InitVars = {
     (ActorFunc)EnMa1_Draw,
 };
 
-ColliderCylinderInit cylinderInit = {
+static ColliderCylinderInit cylinderInit = {
     { COLTYPE_UNK10, 0x00, 0x00, 0x39, 0x20, COLSHAPE_CYLINDER },
     { 0x00, { 0x00000000, 0x00, 0x00 }, { 0x00000000, 0x00, 0x00 }, 0x00, 0x00, 0x01 },
     { 18, 46, 0, { 0, 0, 0 } },

Dragorn421/oot

src/overlays/actors/ovl_En_Ma1/z_en_ma1.c
@@ -37,7 +37,7 @@ const ActorInit En_Ma1_InitVars = {
     (ActorFunc)EnMa1_Draw,
 };
 
-ColliderCylinderSrc cylinderInit = {
+static ColliderCylinderSrc cylinderInit = {
     {
         COL_MATERIAL_NONE,
         AT_NONE,