HackerN64/HackerOoT

z_select decompiled, 1 non matching left (#202)
* First pass z_select

* lots of progress. 2 non matchings and data stuff needed

* match Select_LoadGame

* z_select almost ok, 1 func left

* remove comments

* remove comments and add description

* remove unused flg_set asm

* set_next_gamestate macro

* pr suggestions 1

* pr suggestions 2

* map -> scene

* pr suggestions 3

Co-authored-by: Kelebek1 <eeeedddccc@hotmail.co.uk>

src/code/z_debug.c
@@ -224,7 +224,7 @@ void func_80063D7C(GraphicsContext* gfxCtx) {
     Gfx* dispRefs[4]; // stores state of GfxCtx next ptrs
 
     Graph_OpenDisps(dispRefs, gfxCtx, "../z_debug.c", 628);
-    GfxPrint_Ctor(&gfxPrint);
+    GfxPrint_Init(&gfxPrint);
     sp78 = gfxCtx->polyOpa.p;
     tempRet = Graph_GfxPlusOne(gfxCtx->polyOpa.p);
     gSPDisplayList(gfxCtx->overlay.p++, tempRet);
@@ -245,5 +245,5 @@ void func_80063D7C(GraphicsContext* gfxCtx) {
     gfxCtx->polyOpa.p = sp7C;
     if (0) {}
     Graph_CloseDisps(dispRefs, gfxCtx, "../z_debug.c", 664);
-    GfxPrint_Dtor(&gfxPrint);
+    GfxPrint_Destroy(&gfxPrint);
 }

Dragorn421/oot

src/code/z_debug.c
@@ -1,3 +1,4 @@
+#include "functions.h"
 #include "global.h"
 
 GameInfo* gGameInfo;
@@ -218,7 +219,7 @@ void func_80063D7C_cc0(GraphicsContext* arg0) {
     Gfx* sp30[4];
 
     Graph_OpenDisps(sp30, arg0, "../z_debug.c", 0x274);
-    GfxPrint_Ctor(&sp48);
+    GfxPrint_Init(&sp48);
     sp78 = arg0->polyOpa.p;
     temp_v0 = Graph_GfxPlusOne(sp78);
     gSPDisplayList(arg0->overlay.p++, temp_v0);
@@ -236,5 +237,5 @@ void func_80063D7C_cc0(GraphicsContext* arg0) {
     arg0->polyOpa.p = temp_v0;
     if (1) {}
     Graph_CloseDisps(sp30, arg0, "../z_debug.c", 0x298);
-    GfxPrint_Dtor(&sp48);
+    GfxPrint_Destroy(&sp48);
 }