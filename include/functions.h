HackerN64/HackerOoT

ovl_En_Horse_Normal (#403)
* merge upstream

* Merge upstream

* Match EnHorseNormal_Init

* Work on EnHorseNormal_Draw (probably finish depending functions first)

* func_80A6CC88

* Work on EnHorseNormal_Draw

* Clang format

* Properly use OPEN_DISPS

* Stop using thisx lol

* Use the funny angle number

* Use &D_04049AD0 because it's not a display list?

* Stop using fun macros to fix stack

* EnHorseNormal_Draw

* EnHorseNormal_Destroy

* EnHorseNormal_Update

* Use disp macros EnHorseNormal_Draw

* Correctly type D_04049AD0

* func_80A6C570

* Name some draw stuff

* Fix names

* Make if condition more readable

* func_80A6B30C and fix incorrect  pointer types

* Function prototypes

* func_80A6B250

* Add header comment

* Move skin function to functions.h

* Change to stable

* Cleanup a little

* Match some more

* Work on func_80A6BE6C (won't compile yet)

* Work on func_80A6BE6C again

* Progress

* func_80A6CAFC

* Match all but one :)

* Work on func_80A6BE6C

* Work more on func_80A6BE6C

* Multiply floating point by integer lol

* Reorder assignment to almost match

* func_80A6BE6C

* Fix warning and remove unnecessary forward declarations

* Data section

* Cleanup data a bit

* More cleanup and clang format

* Clang format is a little confused

* More cleanup

* Formatting

* Revert formatting on z_bg_spot01_objects2.c

* Name unk_14C

* Add clone comment

* Name clonePos

* Name and cleanup

* Name some animation related stuff

* Remove unnecessary assignment

* Name cloneRotY

* Use ARRAY_COUNT for sAnimations

* Add more to unknown struct layout

* Use u8 for last struct member

* Use s32 for length

* Add padding disclaimer

* Update src/overlays/actors/ovl_En_Horse_Normal/z_en_horse_normal.c

Co-authored-by: Dragorn421 <Dragorn421@users.noreply.github.com>

* Update src/overlays/actors/ovl_En_Horse_Normal/z_en_horse_normal.c

include/functions.h
@@ -1379,6 +1379,8 @@ void SkelAnime_Free(SkelAnime* skelAnime, GlobalContext* globalCtx);
 // ? func_800A5F60(?);
 // ? func_800A60D8(?);
 void func_800A6330(Actor* this, GlobalContext* globalCtx, PSkinAwb*, void* callback, s32);
+void func_800A63CC(Actor* this, GlobalContext* globalCtx, PSkinAwb*, s32, s32, s32, s32, s32);
+void func_800A6408(ColliderJntSphItem* collider, u8 joint, Vec3f*, Vec3f*);
 // ? func_800A6460(?);
 void func_800A663C(GlobalContext* globalCtx, PSkinAwb*, SkeletonHeader*, AnimationHeader*);
 void func_800A6888(GlobalContext*, PSkinAwb*);

Dragorn421/oot

include/functions.h
@@ -1351,6 +1351,8 @@ void SkelAnime_Free(SkelAnime* skelAnime, GlobalContext* globalCtx);
 // ? func_800A5F60(?);
 // ? func_800A60D8(?);
 void func_800A6330(Actor* this, GlobalContext* globalCtx, PSkinAwb*, void* callback, s32);
+void func_800A63CC(Actor* this, GlobalContext* globalCtx, PSkinAwb*, s32, s32, s32, s32, s32);
+void func_800A6408(ColliderSpheresElement* collider, u8 joint, Vec3f*, Vec3f*);
 // ? func_800A6460(?);
 void func_800A663C(GlobalContext* globalCtx, PSkinAwb*, SkeletonHeader*, AnimationHeader*);
 void func_800A6888(GlobalContext*, PSkinAwb*);