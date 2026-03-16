HackerN64/HackerOoT

Oshihiki (#441)
* Darkmeiro decompilation

Bg_Gnd_Darkmeiro decompiled, matched, and documented.

* give this a shot

* fix conflict

* one more try

* could be useful

* whoops

* matching

* format

* document and format

* .s files

* cleanup and macros in block-related actors

* fixed a few more things, also an error I cso an error I caught in another file.

src/overlays/actors/ovl_En_Zl1/z_en_zl1.c
@@ -38,7 +38,7 @@ const ActorInit En_Zl1_InitVars = {
     (ActorFunc)EnZl1_Draw,
 };
 
-ColliderCylinderInit sCylinderInit = {
+static ColliderCylinderInit sCylinderInit = {
     { COLTYPE_UNK0, 0x00, 0x00, 0x39, 0x20, COLSHAPE_CYLINDER },
     { 0x01, { 0x00000000, 0x00, 0x00 }, { 0x00000000, 0x00, 0x00 }, 0x00, 0x00, 0x01 },
     { 20, 46, 0, { 0, 0, 0 } },

src/overlays/actors/ovl_Obj_Lightswitch/z_obj_lightswitch.c
@@ -6,6 +6,7 @@
 
 #include "z_obj_lightswitch.h"
 #include "vt.h"
+#include "overlays/actors/ovl_Obj_Oshihiki/z_obj_oshihiki.h"
 
 #define FLAGS 0x00000010
 
@@ -176,10 +177,11 @@ void ObjLightswitch_Init(Actor* thisx, GlobalContext* globalCtx) {
         this->actor.posRot.rot.x = this->actor.initPosRot.rot.x = this->actor.shape.rot.x;
         this->actor.posRot.rot.z = this->actor.initPosRot.rot.z = this->actor.shape.rot.z;
         this->actor.flags |= 0x20;
-        if (!Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_OBJ_OSHIHIKI,
-                                this->actor.initPosRot.pos.x, this->actor.initPosRot.pos.y,
-                                this->actor.initPosRot.pos.z, 0, this->actor.initPosRot.rot.y, 0, 0xFF00)) {
+        if (Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_OBJ_OSHIHIKI,
+                               this->actor.initPosRot.pos.x, this->actor.initPosRot.pos.y, this->actor.initPosRot.pos.z,
+                               0, this->actor.initPosRot.rot.y, 0, (0xFF << 8) | PUSHBLOCK_SMALL_START_ON) == NULL) {
             osSyncPrintf(VT_COL(RED, WHITE));
+            // Push-pull block occurrence failure
             osSyncPrintf("押引ブロック発生失敗(%s %d)(arg_data 0x%04x)\n", "../z_obj_lightswitch.c", 452,
                          this->actor.params);
             osSyncPrintf(VT_RST);
@@ -191,11 +193,15 @@ void ObjLightswitch_Init(Actor* thisx, GlobalContext* globalCtx) {
     if (removeSelf) {
         Actor_Kill(&this->actor);
     }
+    // Light switch
     osSyncPrintf("(光スイッチ)(arg_data 0x%04x)\n", this->actor.params);
 }
 
 void ObjLightswitch_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    Collider_DestroyJntSph(globalCtx, &THIS->collider);
+    GlobalContext* globalCtx2 = globalCtx;
+    ObjLightswitch* this = THIS;
+
+    Collider_DestroyJntSph(globalCtx2, &this->collider);
 }
 
 void ObjLightswitch_SetupOff(ObjLightswitch* this) {

Dragorn421/oot

src/overlays/actors/ovl_En_Zl1/z_en_zl1.c
@@ -38,7 +38,7 @@ const ActorInit En_Zl1_InitVars = {
     (ActorFunc)EnZl1_Draw,
 };
 
-ColliderCylinderSrc sCylinderInit = {
+static ColliderCylinderSrc sCylinderInit = {
     {
         COL_MATERIAL_HIT0,
         AT_NONE,

src/overlays/actors/ovl_Obj_Lightswitch/z_obj_lightswitch.c
@@ -6,6 +6,7 @@
 
 #include "z_obj_lightswitch.h"
 #include "vt.h"
+#include "overlays/actors/ovl_Obj_Oshihiki/z_obj_oshihiki.h"
 
 #define FLAGS 0x00000010
 
@@ -209,10 +210,11 @@ void ObjLightswitch_Init(Actor* thisx, GlobalContext* globalCtx) {
         this->actor.posRot.rot.x = this->actor.initPosRot.rot.x = this->actor.shape.rot.x;
         this->actor.posRot.rot.z = this->actor.initPosRot.rot.z = this->actor.shape.rot.z;
         this->actor.flags |= 0x20;
-        if (!Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_OBJ_OSHIHIKI,
-                                this->actor.initPosRot.pos.x, this->actor.initPosRot.pos.y,
-                                this->actor.initPosRot.pos.z, 0, this->actor.initPosRot.rot.y, 0, 0xFF00)) {
+        if (Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_OBJ_OSHIHIKI,
+                               this->actor.initPosRot.pos.x, this->actor.initPosRot.pos.y, this->actor.initPosRot.pos.z,
+                               0, this->actor.initPosRot.rot.y, 0, (0xFF << 8) | PUSHBLOCK_SMALL_START_ON) == NULL) {
             osSyncPrintf(VT_COL(RED, WHITE));
+            // Push-pull block occurrence failure
             osSyncPrintf("押引ブロック発生失敗(%s %d)(arg_data 0x%04x)\n", "../z_obj_lightswitch.c", 452,
                          this->actor.params);
             osSyncPrintf(VT_RST);
@@ -224,11 +226,15 @@ void ObjLightswitch_Init(Actor* thisx, GlobalContext* globalCtx) {
     if (removeSelf) {
         Actor_Kill(&this->actor);
     }
+    // Light switch
     osSyncPrintf("(光スイッチ)(arg_data 0x%04x)\n", this->actor.params);
 }
 
 void ObjLightswitch_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    Collider_DestroySpheres(globalCtx, &THIS->collider);
+    GlobalContext* globalCtx2 = globalCtx;
+    ObjLightswitch* this = THIS;
+
+    Collider_DestroySpheres(globalCtx2, &this->collider);
 }
 
 void ObjLightswitch_SetupOff(ObjLightswitch* this) {