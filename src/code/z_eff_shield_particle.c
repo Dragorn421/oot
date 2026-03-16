HackerN64/HackerOoT

z_skin_matrix.c decompiled (1 non matching, 1 non equivalent) (#243)
* func_800A7BE4 and func_800A7C20 done

- func_800A7BE4 copy of "Math_Vec3f_ToVec3s" in z_lib.c
- func_800A7C20 copy of "Math_Vec3s_ToVec3f" in z_lib.c

* done func_800A7A24

* func_800A76A4 done

* func_800A6E10 done

* func_800A6EF4 done

* func_800A6FA0 done

* func_800A72FC done

* Deleted z_skin_matrix.data.s and updated spec

* func_800A730C done

* func_800A735C done

* func_800A7E70 done, func_800A7C60 matching but I'm not happy with it (weird types in function params)

* Corrections. func_800A7C60 in skin_matrix done, Matrix_MtxFToMtx in sys_matrix done.

* func_800A7EC0 nonmatching

* func_800A8030 nonmatching but VERY close (two registers swapped)

* func_800A8030 done

* updating comments

* func_800A7704 done

* func_800A7894 done

* halfway through deciphering mips_to_c for func_800A73E0

* func_800A73E0 functional but non-matching (saved register differences and probably regalloc)

* Renaming some arguments and rewriting comments

* Renamed files across whole project and deleted asm nonmatchings

* ran format.sh

* fixed function name

* fixing multiplcation order in SkinMatrix_MtxFMtxFMult

* Corrections in SkinMatrix_MtxFMtxFMult

* Formatting changes after review

* Changes as per code review

* fixing rename error

* fixing rename error

* rename fixes

* fixing function rename error

* ran ./format.sh

* last couple of changes as per code review

* renamed SetScaling -> SetScale

* Skin_Matrix_Invert -> SkinMatrix_Invert

* Renaming and fixing debug message print

* Renamed argument "mf" to more specific "clear" in SkinMatrix_GetClear

* renamed again to "mfp"

* snake case to camel case changes

src/code/z_eff_shield_particle.c
@@ -203,17 +203,17 @@ void EffectShieldParticle_Draw(void* thisx, GraphicsContext* gfxCtx) {
                 temp3 = 1.0f;
             }
 
-            func_800A7A24(&spC4, this->position.x, this->position.y, this->position.z);
-            func_800A7704(&sp104, 0, elem->yaw, 0);
-            func_800A6FA0(&spC4, &sp104, &sp84);
-            func_800A7704(&sp104, 0, 0, elem->pitch);
-            func_800A6FA0(&sp84, &sp104, &spC4);
-            func_800A7A24(&sp104, temp1, 0.0f, 0.0f);
-            func_800A6FA0(&spC4, &sp104, &sp84);
-            func_800A76A4(&sp104, temp3 * 0.02f, 0.02f, 0.02f);
-            func_800A6FA0(&sp84, &sp104, &spC4);
+            SkinMatrix_SetTranslate(&spC4, this->position.x, this->position.y, this->position.z);
+            SkinMatrix_SetRotateRPY(&sp104, 0, elem->yaw, 0);
+            SkinMatrix_MtxFMtxFMult(&spC4, &sp104, &sp84);
+            SkinMatrix_SetRotateRPY(&sp104, 0, 0, elem->pitch);
+            SkinMatrix_MtxFMtxFMult(&sp84, &sp104, &spC4);
+            SkinMatrix_SetTranslate(&sp104, temp1, 0.0f, 0.0f);
+            SkinMatrix_MtxFMtxFMult(&spC4, &sp104, &sp84);
+            SkinMatrix_SetScale(&sp104, temp3 * 0.02f, 0.02f, 0.02f);
+            SkinMatrix_MtxFMtxFMult(&sp84, &sp104, &spC4);
 
-            mtx = func_800A7E70(gfxCtx, &spC4);
+            mtx = SkinMatrix_MtxFToNewMtx(gfxCtx, &spC4);
             if (mtx == NULL) {
                 break;
             }

src/code/z_eff_spark.c
@@ -215,11 +215,11 @@ void EffectSpark_Draw(void* thisx, GraphicsContext* gfxCtx) {
 
             elem = &this->elements[i];
 
-            func_800A7A24(&spEC, elem->position.x, elem->position.y, elem->position.z);
+            SkinMatrix_SetTranslate(&spEC, elem->position.x, elem->position.y, elem->position.z);
             temp = ((Math_Rand_ZeroOne() * 2.5f) + 1.5f) * 0.015625f;
-            func_800A76A4(&spAC, temp, temp, 1.0f);
-            func_800A6FA0(&spEC, &globalCtx->mf_11DA0, &sp6C);
-            func_800A6FA0(&sp6C, &spAC, &sp12C);
+            SkinMatrix_SetScale(&spAC, temp, temp, 1.0f);
+            SkinMatrix_MtxFMtxFMult(&spEC, &globalCtx->mf_11DA0, &sp6C);
+            SkinMatrix_MtxFMtxFMult(&sp6C, &spAC, &sp12C);
 
             vertices[j].v.ob[0] = -32;
             vertices[j].v.ob[1] = -32;
@@ -269,7 +269,7 @@ void EffectSpark_Draw(void* thisx, GraphicsContext* gfxCtx) {
 
             if (this) {}
 
-            mtx = func_800A7E70(gfxCtx, &sp12C);
+            mtx = SkinMatrix_MtxFToNewMtx(gfxCtx, &sp12C);
             if (mtx == NULL) {
                 break;
             }

src/overlays/actors/ovl_Bg_Haka_Trap/z_bg_haka_trap.c
@@ -511,7 +511,7 @@ void BgHakaTrap_Draw(Actor* thisx, GlobalContext* globalCtx) {
         sp2C.z = thisx->posRot.pos.z;
         sp2C.y = thisx->posRot.pos.y + 110.0f;
 
-        func_800A6EF4(&globalCtx->mf_11D60, &sp2C, &this->unk_16C);
+        SkinMatrix_Vec3fMtxFMultXYZ(&globalCtx->mf_11D60, &sp2C, &this->unk_16C);
         func_80078914(&this->unk_16C, NA_SE_EV_BRIDGE_CLOSE - SFX_FLAG);
     }
 }

Dragorn421/oot

src/code/z_eff_shield_particle.c
@@ -202,17 +202,17 @@ void EffectShieldParticle_Draw(void* thisx, GraphicsContext* gfxCtx) {
                 temp3 = 1.0f;
             }
 
-            func_800A7A24(&spC4, this->pos.x, this->pos.y, this->pos.z);
-            func_800A7704(&sp104, 0, elem->yaw, 0);
-            func_800A6FA0(&spC4, &sp104, &sp84);
-            func_800A7704(&sp104, 0, 0, elem->pitch);
-            func_800A6FA0(&sp84, &sp104, &spC4);
-            func_800A7A24(&sp104, temp1, 0.0f, 0.0f);
-            func_800A6FA0(&spC4, &sp104, &sp84);
-            func_800A76A4(&sp104, temp3 * 0.02f, 0.02f, 0.02f);
-            func_800A6FA0(&sp84, &sp104, &spC4);
+            SkinMatrix_SetTranslate(&spC4, this->pos.x, this->pos.y, this->pos.z);
+            SkinMatrix_SetRotateRPY(&sp104, 0, elem->yaw, 0);
+            SkinMatrix_MtxFMtxFMult(&spC4, &sp104, &sp84);
+            SkinMatrix_SetRotateRPY(&sp104, 0, 0, elem->pitch);
+            SkinMatrix_MtxFMtxFMult(&sp84, &sp104, &spC4);
+            SkinMatrix_SetTranslate(&sp104, temp1, 0.0f, 0.0f);
+            SkinMatrix_MtxFMtxFMult(&spC4, &sp104, &sp84);
+            SkinMatrix_SetScale(&sp104, temp3 * 0.02f, 0.02f, 0.02f);
+            SkinMatrix_MtxFMtxFMult(&sp84, &sp104, &spC4);
 
-            mtx = func_800A7E70(gfxCtx, &spC4);
+            mtx = SkinMatrix_MtxFToNewMtx(gfxCtx, &spC4);
             if (mtx == NULL) {
                 break;
             }

src/code/z_eff_spark.c
@@ -215,11 +215,11 @@ void EffectSpark_Draw(void* thisx, GraphicsContext* gfxCtx) {
 
             elem = &this->elements[i];
 
-            func_800A7A24(&spEC, elem->pos.x, elem->pos.y, elem->pos.z);
+            SkinMatrix_SetTranslate(&spEC, elem->pos.x, elem->pos.y, elem->pos.z);
             temp = ((Math_Rand_ZeroOne() * 2.5f) + 1.5f) * 0.015625f;
-            func_800A76A4(&spAC, temp, temp, 1.0f);
-            func_800A6FA0(&spEC, &globalCtx->mf_11DA0, &sp6C);
-            func_800A6FA0(&sp6C, &spAC, &sp12C);
+            SkinMatrix_SetScale(&spAC, temp, temp, 1.0f);
+            SkinMatrix_MtxFMtxFMult(&spEC, &globalCtx->mf_11DA0, &sp6C);
+            SkinMatrix_MtxFMtxFMult(&sp6C, &spAC, &sp12C);
 
             vertices[j].v.ob[0] = -32;
             vertices[j].v.ob[1] = -32;
@@ -269,7 +269,7 @@ void EffectSpark_Draw(void* thisx, GraphicsContext* gfxCtx) {
 
             if (this) {}
 
-            mtx = func_800A7E70(gfxCtx, &sp12C);
+            mtx = SkinMatrix_MtxFToNewMtx(gfxCtx, &sp12C);
             if (mtx == NULL) {
                 break;
             }

src/overlays/actors/ovl_Bg_Haka_Trap/z_bg_haka_trap.c
@@ -470,7 +470,7 @@ void BgHakaTrap_Draw(Actor* thisx, GlobalContext* globalCtx) {
         sp2C.x = this->dyna.actor.posRot.pos.x;
         sp2C.z = this->dyna.actor.posRot.pos.z;
         sp2C.y = this->dyna.actor.posRot.pos.y + 110.0f;
-        func_800A6EF4(&globalCtx->mf_11D60, &sp2C, &this->unk16C);
+        SkinMatrix_Vec3fMtxFMultXYZ(&globalCtx->mf_11D60, &sp2C, &this->unk16C);
         func_80078914(&this->unk16C, NA_SE_EV_BRIDGE_CLOSE - SFX_FLAG);
     }
 }