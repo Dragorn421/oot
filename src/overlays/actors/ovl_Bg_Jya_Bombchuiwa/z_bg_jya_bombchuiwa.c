HackerN64/HackerOoT

Remove inline THIS-> from actors  (#538)
* Darkmeiro decompilation

Bg_Gnd_Darkmeiro decompiled, matched, and documented.

* give this a shot

* fix conflict

* one more try

* could be useful

* whoops

* ZAP2 stuff

* ZAP why

* ZAP again

* THIS

* PLAYER

src/overlays/actors/ovl_Bg_Jya_Bombchuiwa/z_bg_jya_bombchuiwa.c
@@ -80,8 +80,11 @@ void BgJyaBombchuiwa_Init(Actor* thisx, GlobalContext* globalCtx) {
     Actor_SetHeight(thisx, 0.0f);
 }
 
-void BgJyaBombchuiwa_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    Collider_DestroyJntSph(globalCtx, &THIS->collider);
+void BgJyaBombchuiwa_Destroy(Actor* thisx, GlobalContext* globalCtx2) {
+    GlobalContext* globalCtx = globalCtx2;
+    BgJyaBombchuiwa* this = THIS;
+
+    Collider_DestroyJntSph(globalCtx, &this->collider);
 }
 
 void BgJyaBombchuiwa_Break(BgJyaBombchuiwa* this, GlobalContext* globalCtx) {

src/overlays/actors/ovl_En_Butte/z_en_butte.c
@@ -163,8 +163,11 @@ void EnButte_Init(Actor* thisx, GlobalContext* globalCtx) {
     osSyncPrintf("(field keep 蝶)(%x)(arg_data 0x%04x)\n", this, this->actor.params);
 }
 
-void EnButte_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    Collider_DestroyJntSph(globalCtx, &THIS->collider);
+void EnButte_Destroy(Actor* thisx, GlobalContext* globalCtx2) {
+    GlobalContext* globalCtx = globalCtx2;
+    EnButte* this = THIS;
+
+    Collider_DestroyJntSph(globalCtx, &this->collider);
 }
 
 void func_809CD56C(EnButte* this) {

src/overlays/actors/ovl_En_Goroiwa/z_en_goroiwa.c
@@ -538,8 +538,11 @@ void EnGoroiwa_Init(Actor* thisx, GlobalContext* globalCtx) {
                  this->actor.initPosRot.rot.z & 1);
 }
 
-void EnGoroiwa_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    Collider_DestroyJntSph(globalCtx, &THIS->collider);
+void EnGoroiwa_Destroy(Actor* thisx, GlobalContext* globalCtx2) {
+    GlobalContext* globalCtx = globalCtx2;
+    EnGoroiwa* this = THIS;
+
+    Collider_DestroyJntSph(globalCtx, &this->collider);
 }
 
 void func_80A4D5E0(EnGoroiwa* this) {

src/overlays/actors/ovl_Obj_Comb/z_obj_comb.c
@@ -145,8 +145,11 @@ void ObjComb_Init(Actor* thisx, GlobalContext* globalCtx) {
     ObjComb_SetupWait(this);
 }
 
-void ObjComb_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    Collider_DestroyJntSph(globalCtx, &THIS->collider);
+void ObjComb_Destroy(Actor* thisx, GlobalContext* globalCtx2) {
+    GlobalContext* globalCtx = globalCtx2;
+    ObjComb* this = THIS;
+
+    Collider_DestroyJntSph(globalCtx, &this->collider);
 }
 
 void ObjComb_SetupWait(ObjComb* this) {

Dragorn421/oot

src/overlays/actors/ovl_Bg_Jya_Bombchuiwa/z_bg_jya_bombchuiwa.c
@@ -113,8 +113,11 @@ void BgJyaBombchuiwa_Init(Actor* thisx, GlobalContext* globalCtx) {
     Actor_SetHeight(thisx, 0.0f);
 }
 
-void BgJyaBombchuiwa_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    Collider_DestroySpheres(globalCtx, &THIS->collider);
+void BgJyaBombchuiwa_Destroy(Actor* thisx, GlobalContext* globalCtx2) {
+    GlobalContext* globalCtx = globalCtx2;
+    BgJyaBombchuiwa* this = THIS;
+
+    Collider_DestroySpheres(globalCtx, &this->collider);
 }
 
 void BgJyaBombchuiwa_Break(BgJyaBombchuiwa* this, GlobalContext* globalCtx) {

src/overlays/actors/ovl_En_Butte/z_en_butte.c
@@ -198,8 +198,11 @@ void EnButte_Init(Actor* thisx, GlobalContext* globalCtx) {
     osSyncPrintf("(field keep 蝶)(%x)(arg_data 0x%04x)\n", this, this->actor.params);
 }
 
-void EnButte_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    Collider_DestroySpheres(globalCtx, &THIS->collider);
+void EnButte_Destroy(Actor* thisx, GlobalContext* globalCtx2) {
+    GlobalContext* globalCtx = globalCtx2;
+    EnButte* this = THIS;
+
+    Collider_DestroySpheres(globalCtx, &this->collider);
 }
 
 void func_809CD56C(EnButte* this) {

src/overlays/actors/ovl_En_Goroiwa/z_en_goroiwa.c
@@ -571,8 +571,11 @@ void EnGoroiwa_Init(Actor* thisx, GlobalContext* globalCtx) {
                  this->actor.initPosRot.rot.z & 1);
 }
 
-void EnGoroiwa_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    Collider_DestroySpheres(globalCtx, &THIS->collider);
+void EnGoroiwa_Destroy(Actor* thisx, GlobalContext* globalCtx2) {
+    GlobalContext* globalCtx = globalCtx2;
+    EnGoroiwa* this = THIS;
+
+    Collider_DestroySpheres(globalCtx, &this->collider);
 }
 
 void func_80A4D5E0(EnGoroiwa* this) {

src/overlays/actors/ovl_Obj_Comb/z_obj_comb.c
@@ -178,8 +178,11 @@ void ObjComb_Init(Actor* thisx, GlobalContext* globalCtx) {
     ObjComb_SetupWait(this);
 }
 
-void ObjComb_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    Collider_DestroySpheres(globalCtx, &THIS->collider);
+void ObjComb_Destroy(Actor* thisx, GlobalContext* globalCtx2) {
+    GlobalContext* globalCtx = globalCtx2;
+    ObjComb* this = THIS;
+
+    Collider_DestroySpheres(globalCtx, &this->collider);
 }
 
 void ObjComb_SetupWait(ObjComb* this) {