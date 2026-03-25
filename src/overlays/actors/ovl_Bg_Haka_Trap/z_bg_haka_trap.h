fd20e6abb3ed6d832aa0a0b82cf70907618ecec4
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-25 23:51:22 +0100

    BgHakaTrap (based on oot-splat @ 9981c8b50a4ddaa2e71e8d4d6f6bdfa6b16ef3ae)

--- src/overlays/actors/ovl_Bg_Haka_Trap/z_bg_haka_trap.h
+++ src/overlays/actors/ovl_Bg_Haka_Trap/z_bg_haka_trap.h
@@ -1,19 +1,31 @@
-#ifndef _Z_BG_HAKA_TRAP_H_
-#define _Z_BG_HAKA_TRAP_H_
+#ifndef BG_HAKA_TRAP_H
+#define BG_HAKA_TRAP_H
 
-#include <ultra64.h>
-#include <global.h>
+#include "global.h"
+
+typedef enum BgHakaTrapType {
+    /* 0 */ BG_HAKA_TRAP_GUILLOTINE,
+    /* 1 */ BG_HAKA_TRAP_SPIKED_CRUSHER,
+    /* 2 */ BG_HAKA_TRAP_SPIKED_WOODEN_WALL_1,
+    /* 3 */ BG_HAKA_TRAP_SPIKED_WOODEN_WALL_2,
+    /* 4 */ BG_HAKA_TRAP_PROPELLER,
+    /* 5 */ BG_HAKA_TRAP_GUILLOTINE_ALT
+} BgHakaTrapType;
 
 struct BgHakaTrap;
 
 typedef void (*BgHakaTrapActionFunc)(struct BgHakaTrap*, GlobalContext*);
 
 typedef struct BgHakaTrap {
-    /* 0x0000 */ DynaPolyActor dyna;
-    /* 0x0164 */ BgHakaTrapActionFunc actionFunc;
-    /* 0x0168 */ char unk_168[0x134];
-} BgHakaTrap; // size = 0x029C
-
-extern const ActorInit Bg_Haka_Trap_InitVars;
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x164 */ BgHakaTrapActionFunc actionFunc;
+    /* 0x168 */ u8 unk168;
+    /* 0x169 */ s8 unk169;
+    /* 0x16A */ s16 unk16A;
+    /* 0x16C */ Vec3f unk16C;
+    /* 0x178 */ ColliderCylinder unk178;
+    /* 0x1C4 */ ColliderTris unk1C4;
+    /* 0x1E4 */ ColliderTrisItem unk1E4[2];
+} BgHakaTrap; // size = 0x29C
 
 #endif
