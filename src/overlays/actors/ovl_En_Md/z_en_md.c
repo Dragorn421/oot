HackerN64/HackerOoT

SkelAnime "Skeleton" Documentation Pass (#497)
* Darkmeiro decompilation

Bg_Gnd_Darkmeiro decompiled, matched, and documented.

* give this a shot

* fix conflict

* one more try

* could be useful

* whoops

* ZAP2 stuff

* ZAP why

* ZAP again

* spooky scary skeletons

* there's a skeleton inside of you

* formatting

* types

* a matched function even

* undo change

* lol anime

* ok how did I forget the .s

* array fix

* would you like to ride the bone train?

* stuff

* more renames

* renames and cleanup

* names suck

* idea

* using void** for now

* stuff

* look more type fixes

Co-authored-by: fig <fig02srl@gmail.com>

src/overlays/actors/ovl_En_Md/z_en_md.c
@@ -55,7 +55,7 @@ static struct_80034EC0_Entry sAnimations[] = {
 };
 
 extern AnimationHeader D_060002C8;
-extern SkeletonHeader D_06007FB8;
+extern FlexSkeletonHeader D_06007FB8;
 
 void func_80AAA250(EnMd* this) {
     f32 initialFrame;
@@ -307,7 +307,7 @@ void func_80AAAA24(EnMd* this) {
                 }
                 break;
         }
-    } else if (this->skelAnime.animCurrentSeg != &D_060002C8) {
+    } else if (this->skelAnime.animation != &D_060002C8) {
         func_80034EC0(&this->skelAnime, sAnimations, 10);
         func_80AAA92C(this, 0);
     }
@@ -593,8 +593,8 @@ void EnMd_Init(Actor* thisx, GlobalContext* globalCtx) {
     s32 pad;
 
     ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawFunc_Circle, 24.0f);
-    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_06007FB8, NULL, this->limbDrawTable, this->transitionDrawTable,
-                     17);
+    SkelAnime_InitFlex(globalCtx, &this->skelAnime, &D_06007FB8, NULL, this->limbDrawTable, this->transitionDrawTable,
+                       17);
 
     Collider_InitCylinder(globalCtx, &this->collider);
     Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
@@ -633,7 +633,7 @@ void EnMd_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 }
 
 void func_80AAB874(EnMd* this, GlobalContext* globalCtx) {
-    if (this->skelAnime.animCurrentSeg == &D_060002C8) {
+    if (this->skelAnime.animation == &D_060002C8) {
         func_80034F54(globalCtx, &this->unk_214, &this->unk_236, 17);
     } else if ((this->unk_1E0.unk_00 == 0) && (this->unk_20B != 7)) {
         func_80AAA92C(this, 7);
@@ -643,7 +643,7 @@ void func_80AAB874(EnMd* this, GlobalContext* globalCtx) {
 }
 
 void func_80AAB8F8(EnMd* this, GlobalContext* globalCtx) {
-    if (this->skelAnime.animCurrentSeg == &D_060002C8) {
+    if (this->skelAnime.animation == &D_060002C8) {
         func_80034F54(globalCtx, &this->unk_214, &this->unk_236, 17);
     }
     func_80AAA93C(this);
@@ -695,7 +695,7 @@ void func_80AAB948(EnMd* this, GlobalContext* globalCtx) {
         return;
     }
 
-    if (this->skelAnime.animCurrentSeg == &D_060002C8) {
+    if (this->skelAnime.animation == &D_060002C8) {
         func_80034F54(globalCtx, &this->unk_214, &this->unk_236, 17);
     }
 
@@ -772,7 +772,7 @@ void EnMd_Update(Actor* thisx, GlobalContext* globalCtx) {
     this->actionFunc(this, globalCtx);
 }
 
-s32 EnMd_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx,
+s32 EnMd_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, void* thisx,
                           Gfx** gfx) {
     EnMd* this = THIS;
     Vec3s vec;
@@ -798,12 +798,12 @@ s32 EnMd_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, 
     return 0;
 }
 
-void EnMd_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx, Gfx** gfx) {
-    s32 pad;
+void EnMd_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, void* thisx, Gfx** gfx) {
+    EnMd* this = THIS;
     Vec3f vec = { 400.0f, 0.0f, 0.0f };
 
     if (limbIndex == 16) {
-        Matrix_MultVec3f(&vec, &thisx->posRot2.pos);
+        Matrix_MultVec3f(&vec, &this->actor.posRot2.pos);
     }
 }
 

Dragorn421/oot

src/overlays/actors/ovl_En_Md/z_en_md.c
@@ -313,7 +313,7 @@ void func_80AAAA24(EnMd* this) {
                 }
                 break;
         }
-    } else if (this->unk14C.animCurrentSeg != &D_60002C8) {
+    } else if (this->unk14C.animation != &D_60002C8) {
         func_80034EC0(&this->unk14C, D_80AAC348, 0xA);
         func_80AAA92C(this, 0);
     }
@@ -586,7 +586,7 @@ void EnMd_Init(Actor* thisx, GlobalContext* globalCtx) {
     s32 pad;
 
     ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawFunc_Circle, 24.0f);
-    SkelAnime_InitSV(globalCtx, &this->unk14C, &D_6007FB8, NULL, this->unk258, this->unk2BE, 0x11);
+    SkelAnime_InitFlex(globalCtx, &this->unk14C, &D_6007FB8, NULL, this->unk258, this->unk2BE, 0x11);
     Collider_InitCylinder(globalCtx, &this->unk194);
     Collider_SetCylinder(globalCtx, &this->unk194, &this->actor, &D_80AAC310);
     func_80061EFC(&this->actor.colChkInfo, NULL, &D_80AAC33C);
@@ -621,7 +621,7 @@ void EnMd_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 }
 
 void func_80AAB874(EnMd* this, GlobalContext* globalCtx) {
-    if (this->unk14C.animCurrentSeg == &D_60002C8) {
+    if (this->unk14C.animation == &D_60002C8) {
         func_80034F54(globalCtx, this->unk214, this->unk236, 0x11);
     } else if ((this->unk1E0.unk_00 == 0) && (this->unk20B != 7)) {
         func_80AAA92C(this, 7);
@@ -630,7 +630,7 @@ void func_80AAB874(EnMd* this, GlobalContext* globalCtx) {
 }
 
 void func_80AAB8F8(EnMd* this, GlobalContext* globalCtx) {
-    if (this->unk14C.animCurrentSeg == &D_60002C8) {
+    if (this->unk14C.animation == &D_60002C8) {
         func_80034F54(globalCtx, this->unk214, this->unk236, 0x11);
     }
     func_80AAA93C(this);
@@ -674,7 +674,7 @@ void func_80AAB948(EnMd* this, GlobalContext* globalCtx) {
         this->unk190 = func_80AABD0C;
         this->actor.speedXZ = 1.5f;
     } else {
-        if (this->unk14C.animCurrentSeg == &D_60002C8) {
+        if (this->unk14C.animation == &D_60002C8) {
             func_80034F54(globalCtx, this->unk214, this->unk236, 0x11);
         }
         if ((this->unk1E0.unk_00 == 0) && (globalCtx->sceneNum == SCENE_SPOT10)) {
@@ -742,9 +742,9 @@ void EnMd_Update(Actor* thisx, GlobalContext* globalCtx) {
     this->unk190(this, globalCtx);
 }
 
-s32 func_80AABEF0(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* actor,
+s32 func_80AABEF0(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, void* thisx,
                   Gfx** gfx) {
-    EnMd* this = (EnMd*)actor;
+    EnMd* this = (EnMd*)thisx;
     Vec3s sp2C;
 
     if (limbIndex == 0x10) {
@@ -766,13 +766,13 @@ s32 func_80AABEF0(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* p
     return 0;
 }
 
-void func_80AAC104(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* actor, Gfx** gfx) {
-    s32 pad;
+void func_80AAC104(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, void* thisx, Gfx** gfx) {
+    EnMd* this = (EnMd*)thisx;
     Vec3f sp18;
 
     sp18 = D_80AAC498;
     if (limbIndex == 0x10) {
-        Matrix_MultVec3f(&sp18, &actor->posRot2.pos);
+        Matrix_MultVec3f(&sp18, &this->actor.posRot2.pos);
     }
 }
 