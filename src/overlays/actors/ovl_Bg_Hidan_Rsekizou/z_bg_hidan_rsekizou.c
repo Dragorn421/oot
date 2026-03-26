HackerN64/HackerOoT

z_bgcheck.c, 800430A0.c, 80043480.c (#256)
* beginning of migrating changes

* got matching

* changed order a bit

* clean up bgcheck

* fix conflict

* fix conflict again

* first stab at identifying types, some oks

* Clean up most bad structs/pointer math, move relevant structs to z64bgcheck.h, get some OKs

* more OKs, z_bgcheck.bss migration, update some sys_math3d.c args

* couple more OKs

* pushing some OKs

* fix compilation issues

* code_800430A0.c OK, more files decomp'd

* 8003A3E0 big OK :)

* Decomp most of func_8003C614, decomp helper funcs

* Decomp SurfaceType, CamData, and WaterBox property related functions

* more OKs, big OK in 8003C078

* more OKs, more progress, move a function definition in z_collision_check to functions.h

* more clean-ups, more OKs, dyn_vtx is now defined as u8*

* 8003A5B8, 8003A7D8, 8003C614, 8003DD6C OK, document function args better

* data migrated, more OKs

* 80041240 OK, func_8003B3C8 and func_8003BB18 disassembled

* func_80040284, 800409A8 non_matching, add IS_ZERO macro

* All asm files have C representations, some big OKs, lots of minor tweaks

* More OKs, non-matching code cleanup

* 8003FBF4 and 80040BE4 OK, improve codegen for most functions

* format z_bgcheck.c

* fix warnings, compile errors on NON_MATCHING

* func_8003EE80 is now NON_MATCHING

* begin documenting some functions

* formatting

* more documentation, func_8003A95C OK

* fix PHYSICAL_TO_VIRTUAL changes

* fix var rename

* More documentation, functions 80040E40, 80041648 OK, change types to not be compatible with ZAP

* func_8004239C ok, more NON_MATCHING improvements, more documentation

* Implement most suggested changes

* Convert comments to slower comments

* /**

* Implement ZAP2 changes

* my anti-virus ate my format.sh results

* Rename a couple hundred functions, fix minor stuff

* rename var so that clang formats correctly

* run format.sh

* i

src/overlays/actors/ovl_Bg_Hidan_Rsekizou/z_bg_hidan_rsekizou.c
@@ -63,21 +63,21 @@ static InitChainEntry sInitChain[] = {
 static UNK_PTR D_8088CD74[] = { 0x06015D20, 0x06016120, 0x06016520, 0x06016920,
                                 0x06016D20, 0x06017120, 0x06017520, 0x06017920 };
 
-extern UNK_TYPE D_0600D5C0; // Dynapoly Data in Object
-extern Gfx D_0600AD00[];    // Display List
-extern Gfx D_0600DC30[];    // Display List
+extern CollisionHeader D_0600D5C0;
+extern Gfx D_0600AD00[]; // Display List
+extern Gfx D_0600DC30[]; // Display List
 
 void BgHidanRsekizou_Init(Actor* thisx, GlobalContext* globalCtx) {
     BgHidanRsekizou* this = THIS;
     s32 i;
     s32 pad;
-    s32 polyID;
+    CollisionHeader* colHeader;
 
-    polyID = 0;
+    colHeader = NULL;
     Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
-    DynaPolyInfo_SetActorMove(&this->dyna, 0);
-    DynaPolyInfo_Alloc(&D_0600D5C0, &polyID);
-    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, polyID);
+    DynaPolyActor_Init(&this->dyna, DPM_UNK);
+    CollisionHeader_GetVirtual(&D_0600D5C0, &colHeader);
+    this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, colHeader);
     Collider_InitJntSph(globalCtx, &this->collider);
     Collider_SetJntSph(globalCtx, &this->collider, &this->dyna.actor, &sJntSphInit, this->colliderItems);
     for (i = 0; i < ARRAY_COUNT(this->colliderItems); i++) {
@@ -90,7 +90,7 @@ void BgHidanRsekizou_Init(Actor* thisx, GlobalContext* globalCtx) {
 void BgHidanRsekizou_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     BgHidanRsekizou* this = THIS;
 
-    DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
     Collider_DestroyJntSph(globalCtx, &this->collider);
 }
 

src/overlays/actors/ovl_Bg_Ice_Turara/z_bg_ice_turara.c
@@ -28,7 +28,7 @@ static ColliderCylinderInit sCylinderInit = {
 };
 
 extern Gfx D_060023D0[];
-extern ColHeader D_06002594;
+extern CollisionHeader D_06002594;
 
 const ActorInit Bg_Ice_Turara_InitVars = {
     ACTOR_BG_ICE_TURARA,
@@ -52,16 +52,15 @@ static InitChainEntry sInitChain[] = {
 void BgIceTurara_Init(Actor* thisx, GlobalContext* globalCtx) {
     BgIceTurara* this = THIS;
     s32 pad;
-    ColHeader* colHeader = NULL;
+    CollisionHeader* colHeader = NULL;
 
     Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
-    DynaPolyInfo_SetActorMove(&this->dyna, 0);
-    DynaPolyInfo_Alloc(&D_06002594, &colHeader);
+    DynaPolyActor_Init(&this->dyna, DPM_UNK);
+    CollisionHeader_GetVirtual(&D_06002594, &colHeader);
     Collider_InitCylinder(globalCtx, &this->collider);
     Collider_SetCylinder(globalCtx, &this->collider, &this->dyna.actor, &sCylinderInit);
     Collider_CylinderUpdate(&this->dyna.actor, &this->collider);
-    this->dyna.dynaPolyId =
-        DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, colHeader);
+    this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, colHeader);
     if (this->dyna.actor.params == TURARA_STALAGMITE) {
         this->actionFunc = BgIceTurara_Stalagmite;
     } else {
@@ -74,7 +73,7 @@ void BgIceTurara_Init(Actor* thisx, GlobalContext* globalCtx) {
 void BgIceTurara_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     BgIceTurara* this = THIS;
 
-    DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
     Collider_DestroyCylinder(globalCtx, &this->collider);
 }
 
@@ -136,7 +135,7 @@ void BgIceTurara_Shiver(BgIceTurara* this, GlobalContext* globalCtx) {
         this->dyna.actor.posRot.pos.z = this->dyna.actor.initPosRot.pos.z;
         Collider_CylinderUpdate(&this->dyna.actor, &this->collider);
         CollisionCheck_SetAT(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
-        func_8003EBF8(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+        func_8003EBF8(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
         this->actionFunc = BgIceTurara_Fall;
     } else {
         sp28 = Rand_ZeroOne();
@@ -158,7 +157,7 @@ void BgIceTurara_Fall(BgIceTurara* this, GlobalContext* globalCtx) {
         BgIceTurara_Break(this, globalCtx, 40.0f);
         if (this->dyna.actor.params == TURARA_STALACTITE_REGROW) {
             this->dyna.actor.posRot.pos.y = this->dyna.actor.initPosRot.pos.y + 120.0f;
-            func_8003EC50(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+            func_8003EC50(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
             this->actionFunc = BgIceTurara_Regrow;
         } else {
             Actor_Kill(&this->dyna.actor);

src/overlays/actors/ovl_Obj_Timeblock/z_obj_timeblock.c
@@ -101,15 +101,14 @@ void ObjTimeblock_ToggleSwitchFlag(GlobalContext* globalCtx, s32 flag) {
 void ObjTimeblock_Init(Actor* thisx, GlobalContext* globalCtx) {
     ObjTimeblock* this = THIS;
     s32 pad;
-    UNK_PTR colHeader = NULL;
+    CollisionHeader* colHeader = NULL;
 
-    DynaPolyInfo_SetActorMove(&this->dyna, DPM_UNK);
+    DynaPolyActor_Init(&this->dyna, DPM_UNK);
     this->dyna.actor.posRot.rot.z = this->dyna.actor.shape.rot.z = 0;
 
-    DynaPolyInfo_Alloc(&D_06000B30, &colHeader);
+    CollisionHeader_GetVirtual(&D_06000B30, &colHeader);
 
-    this->dyna.dynaPolyId =
-        DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, colHeader);
+    this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, colHeader);
 
     Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
     Actor_SetScale(&this->dyna.actor, sSizeOptions[(this->dyna.actor.params >> 8) & 1].scale);
@@ -146,7 +145,7 @@ void ObjTimeblock_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     s32 pad;
     ObjTimeblock* this = THIS;
 
-    DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
 }
 
 u8 ObjTimeblock_PlayerIsInRange(ObjTimeblock* this, GlobalContext* globalCtx) {
@@ -326,9 +325,9 @@ void ObjTimeblock_Update(Actor* thisx, GlobalContext* globalCtx) {
     }
 
     if (this->isVisible) {
-        func_8003EC50(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+        func_8003EC50(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
     } else {
-        func_8003EBF8(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+        func_8003EBF8(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
     }
 }
 

Dragorn421/oot

src/overlays/actors/ovl_Bg_Hidan_Rsekizou/z_bg_hidan_rsekizou.c
@@ -24,7 +24,7 @@ const ActorInit Bg_Hidan_Rsekizou_InitVars = {
 };
 
 extern Gfx D_600AD00[];
-extern UNK_TYPE D_600D5C0;
+extern CollisionHeader D_600D5C0;
 extern Gfx D_600DC30[];
 
 static ColliderJntSphItemInit D_8088CC80[6] = {
@@ -67,13 +67,13 @@ void BgHidanRsekizou_Init(Actor* thisx, GlobalContext* globalCtx) {
     BgHidanRsekizou* this = (BgHidanRsekizou*)thisx;
     s32 i;
     s32 pad;
-    s32 sp30;
+    CollisionHeader* sp30;
 
-    sp30 = 0;
+    sp30 = NULL;
     Actor_ProcessInitChain(&this->dyna.actor, D_8088CD68);
-    DynaPolyInfo_SetActorMove(&this->dyna, DPM_UNK);
-    DynaPolyInfo_Alloc(&D_600D5C0, &sp30);
-    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, sp30);
+    DynaPolyActor_Init(&this->dyna, DPM_UNK);
+    CollisionHeader_GetVirtual(&D_600D5C0, &sp30);
+    this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, sp30);
     Collider_InitJntSph(globalCtx, &this->unk168);
     Collider_SetJntSph(globalCtx, &this->unk168, &this->dyna.actor, &D_8088CD58, this->unk188);
     for (i = 0; i < 6; i++) {
@@ -86,7 +86,7 @@ void BgHidanRsekizou_Init(Actor* thisx, GlobalContext* globalCtx) {
 void BgHidanRsekizou_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     BgHidanRsekizou* this = (BgHidanRsekizou*)thisx;
 
-    DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
     Collider_DestroyJntSph(globalCtx, &this->unk168);
 }
 

src/overlays/actors/ovl_Bg_Ice_Turara/z_bg_ice_turara.c
@@ -23,7 +23,7 @@ void func_80892424(BgIceTurara*, GlobalContext*);
 void func_80892574(BgIceTurara*, GlobalContext*);
 
 extern Gfx D_60023D0[];
-extern UNK_TYPE D_6002594;
+extern CollisionHeader D_6002594;
 
 static ColliderCylinderInit D_80892620 = {
     { 0xA, 0x11, 9, 0, 0x20, 1 },
@@ -54,16 +54,16 @@ static Color_RGBA8 D_8089268C = { 0, 0x32, 0x64, 0xFF };
 void BgIceTurara_Init(Actor* thisx, GlobalContext* globalCtx) {
     BgIceTurara* this = (BgIceTurara*)thisx;
     s32 pad;
-    s32 sp24;
+    CollisionHeader* sp24;
 
-    sp24 = 0;
+    sp24 = NULL;
     Actor_ProcessInitChain(&this->dyna.actor, D_8089266C);
-    DynaPolyInfo_SetActorMove(&this->dyna, DPM_UNK);
-    DynaPolyInfo_Alloc(&D_6002594, &sp24);
+    DynaPolyActor_Init(&this->dyna, DPM_UNK);
+    CollisionHeader_GetVirtual(&D_6002594, &sp24);
     Collider_InitCylinder(globalCtx, &this->unk16C);
     Collider_SetCylinder(globalCtx, &this->unk16C, &this->dyna.actor, &D_80892620);
     Collider_CylinderUpdate(&this->dyna.actor, &this->unk16C);
-    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, sp24);
+    this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, sp24);
     if (this->dyna.actor.params == 0) {
         this->unk164 = func_80892220;
     } else {
@@ -76,7 +76,7 @@ void BgIceTurara_Init(Actor* thisx, GlobalContext* globalCtx) {
 void BgIceTurara_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     BgIceTurara* this = (BgIceTurara*)thisx;
 
-    DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
     Collider_DestroyCylinder(globalCtx, &this->unk16C);
 }
 
@@ -134,7 +134,7 @@ void func_808922B8(BgIceTurara* this, GlobalContext* globalCtx) {
         this->dyna.actor.posRot.pos.z = this->dyna.actor.initPosRot.pos.z;
         Collider_CylinderUpdate(&this->dyna.actor, &this->unk16C);
         CollisionCheck_SetAT(globalCtx, &globalCtx->colChkCtx, &this->unk16C.base);
-        func_8003EBF8(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+        func_8003EBF8(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
         this->unk164 = func_80892424;
     } else {
         sp28 = Rand_ZeroOne();
@@ -164,7 +164,7 @@ void func_80892424(BgIceTurara* this, GlobalContext* globalCtx) {
         func_80892040(this, globalCtx, 40.0f);
         if (this->dyna.actor.params == 2) {
             this->dyna.actor.posRot.pos.y = this->dyna.actor.initPosRot.pos.y + 120.0f;
-            func_8003EC50(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+            func_8003EC50(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
             this->unk164 = func_80892574;
         } else {
             Actor_Kill(&this->dyna.actor);

src/overlays/actors/ovl_Obj_Timeblock/z_obj_timeblock.c
@@ -101,13 +101,13 @@ void func_80BA00CC(GlobalContext* globalCtx, s32 arg1) {
 void ObjTimeblock_Init(Actor* thisx, GlobalContext* globalCtx) {
     ObjTimeblock* this = (ObjTimeblock*)thisx;
     s32 pad;
-    s32 sp2C;
+    CollisionHeader* sp2C;
 
-    sp2C = 0;
-    DynaPolyInfo_SetActorMove(&this->dyna, DPM_UNK);
+    sp2C = NULL;
+    DynaPolyActor_Init(&this->dyna, DPM_UNK);
     this->dyna.actor.posRot.rot.z = this->dyna.actor.shape.rot.z = 0;
-    DynaPolyInfo_Alloc(&D_6000B30, &sp2C);
-    this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, sp2C);
+    CollisionHeader_GetVirtual(&D_6000B30, &sp2C);
+    this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, sp2C);
     Actor_ProcessInitChain(&this->dyna.actor, D_80BA0B28);
     Actor_SetScale(&this->dyna.actor, D_80BA0AF0[(this->dyna.actor.params >> 8) & 1].unk0);
     if ((this->dyna.actor.params >> 6) & 1) {
@@ -145,7 +145,7 @@ void ObjTimeblock_Init(Actor* thisx, GlobalContext* globalCtx) {
 void ObjTimeblock_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     ObjTimeblock* this = (ObjTimeblock*)thisx;
 
-    DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
 }
 
 s32 func_80BA032C(ObjTimeblock* this, GlobalContext* globalCtx) {
@@ -335,10 +335,10 @@ void ObjTimeblock_Update(Actor* thisx, GlobalContext* globalCtx) {
         this->unk16C = temp_v0 - 1;
     }
     if (this->unk178 != 0) {
-        func_8003EC50(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+        func_8003EC50(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
         return;
     }
-    func_8003EBF8(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+    func_8003EBF8(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
 }
 
 void ObjTimeblock_Draw(Actor* thisx, GlobalContext* globalCtx) {