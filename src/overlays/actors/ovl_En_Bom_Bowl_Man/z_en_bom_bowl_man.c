HackerN64/HackerOoT

En_Tite OK (#555)
* EnTite_Update matching

* Fixes to update

* made good progress, func_80B1A2A0 is in a good place to be matching soon

* Merge stuff

* func_80B19E94 done, func_80B18CC4 done

* EnTite_Destroy and EnTite_Init done

* Commenting out sDamageTable and EnTite_Init until all the data matches

* Some more small functions done

* more progress

* func_80B19918 done

* func_80B18E7C done

* func_80B19524 done

* more progress

* EnTite_Draw equiv but nonmatching, file otherwise done

* Found some unstaged changed on old laptop

* Draw matching: File fully matching!

* Documentation

* finished documentation

* deleted data files

* update spec

* fixed waterY -> yDistToWater rename after merge

* ran format.sh

* fixing accidental renames in camera

* Fixed some obvious number/comment formatting issues

* Removed unecessary prototypes

* merge

* running format.sh

* suggestions

* more pr comment changes

* format to push

* one more change

* more renames (see pr comments)

* merge

* renames

* format.sh

* fix renames

* function comment formatting

* merge + format

* endless merge fixes

* merge fixes until  die

* yet again a merge fix

* pr suggestions

src/overlays/actors/ovl_En_Bom_Bowl_Man/z_en_bom_bowl_man.c
@@ -107,7 +107,7 @@ void EnBomBowMan_WaitAsleep(EnBomBowlMan* this, GlobalContext* globalCtx) {
     } else {
         yawDiff = ABS((s16)(this->actor.yawTowardsLink - this->actor.shape.rot.y));
 
-        if (!(this->actor.xzDistFromLink > 120.0f) && (yawDiff < 0x4300)) {
+        if (!(this->actor.xzDistToLink > 120.0f) && (yawDiff < 0x4300)) {
             func_8002F2CC(&this->actor, globalCtx, 120.0f);
         }
     }
@@ -269,7 +269,7 @@ void EnBomBowMan_RunGame(EnBomBowlMan* this, GlobalContext* globalCtx) {
             }
         } else {
             yawDiff = ABS((s16)(this->actor.yawTowardsLink - this->actor.shape.rot.y));
-            if (!(this->actor.xzDistFromLink > 120.0f) && (yawDiff < 0x4300)) {
+            if (!(this->actor.xzDistToLink > 120.0f) && (yawDiff < 0x4300)) {
                 func_8002F2CC(&this->actor, globalCtx, 120.0f);
             }
         }

src/overlays/actors/ovl_En_Kusa/z_en_kusa.c
@@ -306,17 +306,17 @@ void func_80A9B8D8(EnKusa* this, GlobalContext* globalCtx) {
         func_80A9BEAC(this);
         this->actor.flags |= 0x800;
     } else {
-        if (!(this->collider.base.maskA & 8) && (this->actor.xzDistFromLink > 12.0f)) {
+        if (!(this->collider.base.maskA & 8) && (this->actor.xzDistToLink > 12.0f)) {
             this->collider.base.maskA |= 8;
         }
 
-        if (this->actor.xzDistFromLink < 600.0f) {
+        if (this->actor.xzDistToLink < 600.0f) {
             Collider_CylinderUpdate(&this->actor, &this->collider);
             CollisionCheck_SetAC(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
 
-            if (this->actor.xzDistFromLink < 400.0f) {
+            if (this->actor.xzDistToLink < 400.0f) {
                 CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
-                if (this->actor.xzDistFromLink < 100.0f) {
+                if (this->actor.xzDistToLink < 100.0f) {
                     func_8002F580(&this->actor, globalCtx);
                 }
             }
@@ -377,7 +377,7 @@ void func_80A9BC1C(EnKusa* this, GlobalContext* globalCtx) {
     } else {
         if (this->actor.bgCheckFlags & 0x40) {
             contactPos.x = this->actor.posRot.pos.x;
-            contactPos.y = this->actor.posRot.pos.y + this->actor.waterY;
+            contactPos.y = this->actor.posRot.pos.y + this->actor.yDistToWater;
             contactPos.z = this->actor.posRot.pos.z;
             EffectSsGSplash_Spawn(globalCtx, &contactPos, NULL, NULL, 0, 400);
             EffectSsGRipple_Spawn(globalCtx, &contactPos, 150, 650, 0);

Dragorn421/oot

src/overlays/actors/ovl_En_Bom_Bowl_Man/z_en_bom_bowl_man.c
@@ -108,7 +108,7 @@ void func_809C38A8_WaitTalk(EnBomBowlMan* this, GlobalContext* globalCtx) {
     } else {
         relYawTowardsPlayer = this->actor.yawTowardsLink - this->actor.shape.rot.y;
         relYawTowardsPlayerAbs = ABS(relYawTowardsPlayer);
-        if (!(this->actor.xzDistFromLink > 120.0f) && (relYawTowardsPlayerAbs < 0x4300)) {
+        if (!(this->actor.xzDistToLink > 120.0f) && (relYawTowardsPlayerAbs < 0x4300)) {
             func_8002F2CC(&this->actor, globalCtx, 120.0f);
         }
     }
@@ -253,7 +253,7 @@ void func_809C3DC4(EnBomBowlMan* this, GlobalContext* globalCtx) {
     } else {
         relYawTowardsPlayer = this->actor.yawTowardsLink - this->actor.shape.rot.y;
         relYawTowardsPlayerAbs = ABS(relYawTowardsPlayer);
-        if (!(this->actor.xzDistFromLink > 120.0f) && (relYawTowardsPlayerAbs < 0x4300)) {
+        if (!(this->actor.xzDistToLink > 120.0f) && (relYawTowardsPlayerAbs < 0x4300)) {
             func_8002F2CC(&this->actor, globalCtx, 120.0f);
         }
     }

src/overlays/actors/ovl_En_Kusa/z_en_kusa.c
@@ -275,15 +275,15 @@ void func_80A9B8D8(EnKusa* this, GlobalContext* globalCtx) {
         func_80A9BEAC(this);
         this->actor.flags |= 0x800;
     } else {
-        if (!(this->unk150.base.maskA & 8) && (this->actor.xzDistFromLink > 12.0f)) {
+        if (!(this->unk150.base.maskA & 8) && (this->actor.xzDistToLink > 12.0f)) {
             this->unk150.base.maskA |= 8;
         }
-        if (this->actor.xzDistFromLink < 600.0f) {
+        if (this->actor.xzDistToLink < 600.0f) {
             Collider_CylinderUpdate(&this->actor, &this->unk150);
             CollisionCheck_SetAC(globalCtx, &globalCtx->colChkCtx, &this->unk150.base);
-            if (this->actor.xzDistFromLink < 400.0f) {
+            if (this->actor.xzDistToLink < 400.0f) {
                 CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->unk150.base);
-                if (this->actor.xzDistFromLink < 100.0f) {
+                if (this->actor.xzDistToLink < 100.0f) {
                     func_8002F580(&this->actor, globalCtx);
                 }
             }
@@ -343,7 +343,7 @@ void func_80A9BC1C(EnKusa* this, GlobalContext* globalCtx) {
     } else {
         if (this->actor.bgCheckFlags & 0x40) {
             sp30.x = this->actor.posRot.pos.x;
-            sp30.y = this->actor.posRot.pos.y + this->actor.waterY;
+            sp30.y = this->actor.posRot.pos.y + this->actor.yDistToWater;
             sp30.z = this->actor.posRot.pos.z;
             EffectSsGSplash_Spawn(globalCtx, &sp30, NULL, NULL, 0, 400);
             EffectSsGRipple_Spawn(globalCtx, &sp30, 150, 650, 0);