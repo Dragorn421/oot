cc2fbb238e5ca5788f073eca3e49772081ff570c
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-26 11:35:36 +0100

    EnFirefly (based on oot-splat @ 9908528484d92d69f2162b28b3a4abe83287362a)

--- src/overlays/actors/ovl_En_Firefly/z_en_firefly.h
+++ src/overlays/actors/ovl_En_Firefly/z_en_firefly.h
@@ -6,9 +6,29 @@
 
 struct EnFirefly;
 
+typedef enum EnFireflyBodyPart {
+    /* 0 */ EN_FIREFLY_BODY_PART_LIMB_15,
+    /* 1 */ EN_FIREFLY_BODY_PART_LIMB_21,
+    /* 2 */ EN_FIREFLY_BODY_PART_LIMB_10,
+    /* 3 */ EN_FIREFLY_BODY_PART_MAX
+} EnFireflyBodyPart;
+
+typedef void (*EnFireflyActionFunc)(struct EnFirefly*, GlobalContext*);
+
 typedef struct EnFirefly {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x228];
+    /* 0x000 */ Actor actor;
+    /* 0x14C */ Vec3f bodyPartsPos[EN_FIREFLY_BODY_PART_MAX];
+    /* 0x170 */ SkelAnime skelAnime;
+    /* 0x1B4 */ EnFireflyActionFunc actionFunc;
+    /* 0x1B8 */ u8 unk1B8;
+    /* 0x1B9 */ u8 unk1B9;
+    /* 0x1BA */ s16 unk1BA_timer;
+    /* 0x1BC */ s16 unk1BC_targetRotX;
+    /* 0x1BE */ Vec3s unk1BE[0x1C];
+    /* 0x266 */ Vec3s unk266[0x1C];
+    /* 0x310 */ f32 unk310_homeY;
+    /* 0x314 */ ColliderJntSph collider;
+    /* 0x334 */ ColliderJntSphItem colliderElements[1];
 } EnFirefly; // size = 0x0374
 
 extern const ActorInit En_Firefly_InitVars;
