6a8f4d75def8f87de62ac3eaf10792c46dc8370c
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-26 10:25:46 +0100

    BgHidanRsekizou (based on oot-splat @ 1c8a913344363ec47047d1c86a93cc398157d199)

--- src/overlays/actors/ovl_Bg_Hidan_Rsekizou/z_bg_hidan_rsekizou.h
+++ src/overlays/actors/ovl_Bg_Hidan_Rsekizou/z_bg_hidan_rsekizou.h
@@ -1,14 +1,18 @@
 #ifndef _Z_BG_HIDAN_RSEKIZOU_H_
 #define _Z_BG_HIDAN_RSEKIZOU_H_
 
+#include "z64collision_check.h"
 #include <ultra64.h>
 #include <global.h>
 
 struct BgHidanRsekizou;
 
 typedef struct BgHidanRsekizou {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x1BC];
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x164 */ s16 unk164;
+    /* 0x166 */ s16 unk166;
+    /* 0x168 */ ColliderJntSph unk168;
+    /* 0x188 */ ColliderJntSphItem unk188[6];
 } BgHidanRsekizou; // size = 0x0308
 
 extern const ActorInit Bg_Hidan_Rsekizou_InitVars;
