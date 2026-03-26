HackerN64/HackerOoT

decompile z_camera (#398)
* cleanup

* name camera action functions

* decompile a few small functions, name a few Camera struct members

* decompile camera data, decompile a few camera functions

* Split ASM for code_800BB0A0

* removing code_800BB0A0.s

* PR Requests, Camera WIP

* remove #define NON_MATCHING from db_camera

* rename code_8007BF90.c to z_olib.c, rename functions in z_olib.c

* camera wip

* rename some struct memebers, some decomp wip

* pr updates

* camera wip

* name some fields in Camera Struct, being making sense of Camera_Update

* Camera WIP

* wip

* wip

* add z64camera.h header, begin creating CameraSetting macros

* wip

* wip

* wip

* wip

* migrate camera bss to c

* match a couple functions in db_camera

* match some small db_camera functions

* wip

* migrate db_camera rodata, match a few functions

* remote db_camera.rodata.s

* match some of db_camera

* identify types of some unknown data pieces

* some small wip

* Match Camera_Init, some function changes, some struct name changes.  Change unk_C0 and unk_CC to floats from Vec3fs

* add naming for a few more Camera struct members

* wip

* match func_80043F94

* Match Camera_Jump1

* document some of Camera_Jump1

* wip

* match Camera_Jump3

* Match Camera_Update, FeelsAmazing

* wip

* wip

* match Camera_SetParam

* minor cleanup

* wip

* wip

* match Camera_KeepOn0

* some documentation, modify some matching functions to match style of others.

* match Camera_Demo1

* match camera_demo9

* document Camera_Demo1 and Camera_Demo9

* wip

* Match camera_battle4

* match camera_unique2

* Match Camera_Unique3

* match camera_special6

* match Camera_Special5

* wip

* document camera_special6

* naming updates

* match camera_Unique1

* match Camera_Unique0

* wip

* Match Camera_CalcUpFromPitchYawRoll

* match func_80045508

* document Camera_Battle

src/overlays/actors/ovl_En_Bom_Bowl_Pit/z_en_bom_bowl_pit.c
@@ -102,7 +102,7 @@ void EnBomBowlPit_DetectHit(EnBomBowlPit* this, GlobalContext* globalCtx) {
                 this->unk_1D4.y = fabsf(this->unk_180.y - this->unk_1BC.y) * 0.02f;
                 this->unk_1D4.z = fabsf(this->unk_180.z - this->unk_1BC.z) * 0.02f;
 
-                func_800C04D8(globalCtx, this->camId, &this->unk_180, &this->unk_18C);
+                Gameplay_CameraSetAtEye(globalCtx, this->camId, &this->unk_180, &this->unk_18C);
                 this->actor.textId = 0xF;
                 func_8010B680(globalCtx, this->actor.textId, NULL);
                 this->unk_154 = 5;
@@ -128,7 +128,7 @@ void EnBomBowlPit_CameraDollyIn(EnBomBowlPit* this, GlobalContext* globalCtx) {
         Math_SmoothScaleMaxF(&this->unk_18C.z, this->unk_198.z, this->unk_1A4.z, this->unk_1B0.z);
     }
 
-    func_800C04D8(globalCtx, this->camId, &this->unk_180, &this->unk_18C);
+    Gameplay_CameraSetAtEye(globalCtx, this->camId, &this->unk_180, &this->unk_18C);
 
     if ((this->unk_154 == func_8010BDBC(&globalCtx->msgCtx)) && (func_80106BC8(globalCtx) != 0)) {
         func_80106CCC(globalCtx);

Dragorn421/oot

src/overlays/actors/ovl_En_Bom_Bowl_Pit/z_en_bom_bowl_pit.c
@@ -95,7 +95,7 @@ void func_809C4E8C(EnBomBowlPit* this, GlobalContext* globalCtx) {
                 this->unk1D4.x = fabsf(this->unk180.x - this->unk1BC.x) * 0.02f;
                 this->unk1D4.y = fabsf(this->unk180.y - this->unk1BC.y) * 0.02f;
                 this->unk1D4.z = fabsf(this->unk180.z - this->unk1BC.z) * 0.02f;
-                func_800C04D8(globalCtx, this->unk152, &this->unk180, &this->unk18C);
+                Gameplay_CameraSetAtEye(globalCtx, this->unk152, &this->unk180, &this->unk18C);
                 this->actor.textId = 0xF;
                 func_8010B680(globalCtx, this->actor.textId, NULL);
                 this->unk154 = 5;
@@ -119,7 +119,7 @@ void func_809C5184(EnBomBowlPit* this, GlobalContext* globalCtx) {
         Math_SmoothScaleMaxF(&this->unk18C.y, this->unk198.y, this->unk1A4.y, this->unk1B0.y);
         Math_SmoothScaleMaxF(&this->unk18C.z, this->unk198.z, this->unk1A4.z, this->unk1B0.z);
     }
-    func_800C04D8(globalCtx, this->unk152, &this->unk180, &this->unk18C);
+    Gameplay_CameraSetAtEye(globalCtx, this->unk152, &this->unk180, &this->unk18C);
     if ((this->unk154 == func_8010BDBC(&globalCtx->msgCtx)) && (func_80106BC8(globalCtx) != 0)) {
         func_80106CCC(globalCtx);
     }