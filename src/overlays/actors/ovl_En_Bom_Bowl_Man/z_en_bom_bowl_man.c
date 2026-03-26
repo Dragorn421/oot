HackerN64/HackerOoT

Some cleanup and docs for z_skelanime (#601)
* docs and enums

* bgcheck merge

Co-authored-by: petrie911 <pmontag@DESKTOP-LG8A167.localdomain>

src/overlays/actors/ovl_En_Bom_Bowl_Man/z_en_bom_bowl_man.c
@@ -92,7 +92,7 @@ void EnBomBowlMan_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 
 void EnBomBowMan_SetupWaitAsleep(EnBomBowlMan* this, GlobalContext* globalCtx) {
     this->frameCount = (f32)Animation_GetLastFrame(&D_06000710);
-    Animation_Change(&this->skelAnime, &D_06000710, 1.0f, 0.0f, this->frameCount, 0, -10.0f);
+    Animation_Change(&this->skelAnime, &D_06000710, 1.0f, 0.0f, this->frameCount, ANIMMODE_LOOP, -10.0f);
     this->actor.textId = 0xC0;
     this->dialogState = 5;
     this->actionFunc = EnBomBowMan_WaitAsleep;
@@ -125,7 +125,7 @@ void EnBomBowMan_TalkAsleep(EnBomBowlMan* this, GlobalContext* globalCtx) {
 
 void EnBomBowMan_WakeUp(EnBomBowlMan* this, GlobalContext* globalCtx) {
     this->frameCount = (f32)Animation_GetLastFrame(&D_06000080);
-    Animation_Change(&this->skelAnime, &D_06000080, 1.0f, 0.0f, this->frameCount, 2, -10.0f);
+    Animation_Change(&this->skelAnime, &D_06000080, 1.0f, 0.0f, this->frameCount, ANIMMODE_ONCE, -10.0f);
     this->eyeMode = CHU_GIRL_EYES_OPEN_SLOWLY;
     this->actionFunc = EnBomBowMan_BlinkAwake;
 }
@@ -161,7 +161,7 @@ void EnBomBowMan_CheckBeatenDC(EnBomBowlMan* this, GlobalContext* globalCtx) {
     if ((func_8010BDBC(&globalCtx->msgCtx) == this->dialogState) && (func_80106BC8(globalCtx) != 0)) {
         func_80106CCC(globalCtx);
         this->frameCount = (f32)Animation_GetLastFrame(&D_060072AC);
-        Animation_Change(&this->skelAnime, &D_060072AC, 1.0f, 0.0f, this->frameCount, 0, -10.0f);
+        Animation_Change(&this->skelAnime, &D_060072AC, 1.0f, 0.0f, this->frameCount, ANIMMODE_LOOP, -10.0f);
         this->eyeMode = CHU_GIRL_EYES_AWAKE;
         this->blinkTimer = (s16)Rand_ZeroFloat(60.0f) + 20;
         // Check for beaten Dodongo's Cavern

src/overlays/actors/ovl_En_Firefly/z_en_firefly.c
@@ -164,7 +164,7 @@ void EnFirefly_SetupWait(EnFirefly* this) {
 void EnFirefly_SetupFall(EnFirefly* this) {
     this->timer = 40;
     this->actor.velocity.y = 0.0f;
-    Animation_Change(&this->skelAnime, &D_0600017C, 0.5f, 0.0f, 0.0f, 1, -3.0f);
+    Animation_Change(&this->skelAnime, &D_0600017C, 0.5f, 0.0f, 0.0f, ANIMMODE_LOOP_INTERP, -3.0f);
     Audio_PlayActorSound2(&this->actor, NA_SE_EN_FFLY_DEAD);
     this->actor.flags |= 0x10;
     func_8003426C(&this->actor, 0x4000, 0xFF, 0, 40);

Dragorn421/oot

src/overlays/actors/ovl_En_Bom_Bowl_Man/z_en_bom_bowl_man.c
@@ -92,7 +92,7 @@ void EnBomBowlMan_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 
 void func_809C3820(EnBomBowlMan* this, GlobalContext* globalCtx) {
     this->unk254_curAnimFraceCount = Animation_GetLastFrame(&D_6000710);
-    Animation_Change(&this->unk14C, &D_6000710, 1.0f, 0.0f, this->unk254_curAnimFraceCount, 0U, -10.0f);
+    Animation_Change(&this->unk14C, &D_6000710, 1.0f, 0.0f, this->unk254_curAnimFraceCount, ANIMMODE_LOOP, -10.0f);
     this->actor.textId = 0xC0;
     this->unk22E = 5;
     this->actionFunc = func_809C38A8_WaitTalk;
@@ -124,7 +124,7 @@ void func_809C395C(EnBomBowlMan* this, GlobalContext* globalCtx) {
 
 void func_809C39D0(EnBomBowlMan* this, GlobalContext* globalCtx) {
     this->unk254_curAnimFraceCount = Animation_GetLastFrame(&D_6000080);
-    Animation_Change(&this->unk14C, &D_6000080, 1.0f, 0.0f, this->unk254_curAnimFraceCount, 2U, -10.0f);
+    Animation_Change(&this->unk14C, &D_6000080, 1.0f, 0.0f, this->unk254_curAnimFraceCount, ANIMMODE_ONCE, -10.0f);
     this->unk238 = 1;
     this->actionFunc = func_809C3A54;
 }
@@ -157,7 +157,7 @@ void func_809C3B50(EnBomBowlMan* this, GlobalContext* globalCtx) {
     if ((this->unk22E == func_8010BDBC(&globalCtx->msgCtx)) && (func_80106BC8(globalCtx) != 0)) {
         func_80106CCC(globalCtx);
         this->unk254_curAnimFraceCount = Animation_GetLastFrame(&D_60072AC);
-        Animation_Change(&this->unk14C, &D_60072AC, 1.0f, 0.0f, this->unk254_curAnimFraceCount, 0U, -10.0f);
+        Animation_Change(&this->unk14C, &D_60072AC, 1.0f, 0.0f, this->unk254_curAnimFraceCount, ANIMMODE_LOOP, -10.0f);
         this->unk238 = 3;
         this->unk236 = (s16)Rand_ZeroFloat(60.0f) + 0x14;
         if (!(gSaveContext.eventChkInf[2] & 0x20) && (gGameInfo->data[0x962] == 0)) {

src/overlays/actors/ovl_En_Firefly/z_en_firefly.c
@@ -157,7 +157,7 @@ void func_80A132F4_Setup(EnFirefly* this) {
 void func_80A133A0_Setup(EnFirefly* this) {
     this->unk1BA_timer = 0x28;
     this->actor.velocity.y = 0.0f;
-    Animation_Change(&this->skelAnime, &D_600017C, 0.5f, 0.0f, 0.0f, 1U, -3.0f);
+    Animation_Change(&this->skelAnime, &D_600017C, 0.5f, 0.0f, 0.0f, ANIMMODE_LOOP_INTERP, -3.0f);
     Audio_PlayActorSound2(&this->actor, NA_SE_EN_FFLY_DEAD);
     this->actor.flags |= 0x10;
     func_8003426C(&this->actor, 0x4000, 0xFF, 0, 0x28);