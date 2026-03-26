HackerN64/HackerOoT

Consistent naming for Math_ functions (#542)
* Darkmeiro decompilation

Bg_Gnd_Darkmeiro decompiled, matched, and documented.

* give this a shot

* fix conflict

* one more try

* could be useful

* whoops

* ZAP2 stuff

* ZAP why

* ZAP again

* maths

* Factoriali -> Factorial

* soon, soon

* renames

* rand

* docs

* merged

* formatting

* little more cleanup

* asm crept back in

* changes to MathF

* smooth criminal

* functions.h

src/overlays/actors/ovl_Bg_Jya_Haheniron/z_bg_jya_haheniron.c
@@ -79,12 +79,12 @@ void BgJyaHaheniron_SpawnFragments(GlobalContext* globalCtx, Vec3f* vec1, Vec3f*
     f32 rand1;
 
     for (angle = 0, i = 0; i < ARRAY_COUNT(sKakeraScales); i++) {
-        rand1 = Math_Rand_ZeroOne() * 10.0f;
-        vel.x = (Math_Sins(angle) * rand1) + vec2->x;
-        vel.y = (Math_Rand_ZeroOne() * 10.0f) + vec2->y;
-        vel.z = (Math_Coss(angle) * rand1) + vec2->z;
+        rand1 = Rand_ZeroOne() * 10.0f;
+        vel.x = (Math_SinS(angle) * rand1) + vec2->x;
+        vel.y = (Rand_ZeroOne() * 10.0f) + vec2->y;
+        vel.z = (Math_CosS(angle) * rand1) + vec2->z;
 
-        rand1 = Math_Rand_ZeroOne();
+        rand1 = Rand_ZeroOne();
         if (rand1 < 0.2f) {
             arg5 = 96;
         } else if (rand1 < 0.8f) {
@@ -109,7 +109,7 @@ void BgJyaHaheniron_Init(Actor* thisx, GlobalContext* globalCtx) {
     Actor_SetScale(&this->actor, D_80898794[this->actor.params]);
     if (thisx->params == 0) {
         BgJyaHaheniron_ColliderInit(this, globalCtx);
-        thisx->shape.rot.z = (Math_Rand_ZeroOne() * 65535.0f);
+        thisx->shape.rot.z = (Rand_ZeroOne() * 65535.0f);
         BgJyaHaheniron_SetupChairCrumble(this);
     } else if (thisx->params == 1) {
         BgJyaHaheniron_SetupPillarCrumble(this);
@@ -135,9 +135,9 @@ void BgJyaHaheniron_ChairCrumble(BgJyaHaheniron* this, GlobalContext* globalCtx)
     func_8002E4B4(globalCtx, &this->actor, 5.0f, 8.0f, 0.0f, 0x85);
     if ((this->actor.bgCheckFlags & 9) ||
         ((this->collider.base.atFlags & 2) && (this->collider.base.at) && (this->collider.base.at->type == 2))) {
-        vec.x = -Math_Rand_ZeroOne() * this->actor.velocity.x;
-        vec.y = -Math_Rand_ZeroOne() * this->actor.velocity.y;
-        vec.z = -Math_Rand_ZeroOne() * this->actor.velocity.z;
+        vec.x = -Rand_ZeroOne() * this->actor.velocity.x;
+        vec.y = -Rand_ZeroOne() * this->actor.velocity.y;
+        vec.z = -Rand_ZeroOne() * this->actor.velocity.z;
         BgJyaHaheniron_SpawnFragments(globalCtx, &this->actor.posRot, &vec);
         Actor_Kill(&this->actor);
     } else if (this->timer >= 61) {

src/overlays/actors/ovl_En_Bom_Bowl_Pit/z_en_bom_bowl_pit.c
@@ -120,12 +120,12 @@ void EnBomBowlPit_DetectHit(EnBomBowlPit* this, GlobalContext* globalCtx) {
 
 void EnBomBowlPit_CameraDollyIn(EnBomBowlPit* this, GlobalContext* globalCtx) {
     if (this->camId != 0) {
-        Math_SmoothScaleMaxF(&this->unk_180.x, this->unk_1BC.x, this->unk_1C8.x, this->unk_1D4.x);
-        Math_SmoothScaleMaxF(&this->unk_180.y, this->unk_1BC.y, this->unk_1C8.y, this->unk_1D4.y);
-        Math_SmoothScaleMaxF(&this->unk_180.z, this->unk_1BC.z, this->unk_1C8.z, this->unk_1D4.z);
-        Math_SmoothScaleMaxF(&this->unk_18C.x, this->unk_198.x, this->unk_1A4.x, this->unk_1B0.x);
-        Math_SmoothScaleMaxF(&this->unk_18C.y, this->unk_198.y, this->unk_1A4.y, this->unk_1B0.y);
-        Math_SmoothScaleMaxF(&this->unk_18C.z, this->unk_198.z, this->unk_1A4.z, this->unk_1B0.z);
+        Math_ApproachF(&this->unk_180.x, this->unk_1BC.x, this->unk_1C8.x, this->unk_1D4.x);
+        Math_ApproachF(&this->unk_180.y, this->unk_1BC.y, this->unk_1C8.y, this->unk_1D4.y);
+        Math_ApproachF(&this->unk_180.z, this->unk_1BC.z, this->unk_1C8.z, this->unk_1D4.z);
+        Math_ApproachF(&this->unk_18C.x, this->unk_198.x, this->unk_1A4.x, this->unk_1B0.x);
+        Math_ApproachF(&this->unk_18C.y, this->unk_198.y, this->unk_1A4.y, this->unk_1B0.y);
+        Math_ApproachF(&this->unk_18C.z, this->unk_198.z, this->unk_1A4.z, this->unk_1B0.z);
     }
 
     Gameplay_CameraSetAtEye(globalCtx, this->camId, &this->unk_180, &this->unk_18C);

src/overlays/actors/ovl_En_Kusa/z_en_kusa.c
@@ -125,7 +125,7 @@ void EnKusa_DropCollectible(EnKusa* this, GlobalContext* globalCtx) {
             Item_DropCollectibleRandom(globalCtx, NULL, &this->actor.posRot.pos, dropParams << 4);
             break;
         case 1:
-            if (Math_Rand_ZeroOne() < 0.5f) {
+            if (Rand_ZeroOne() < 0.5f) {
                 Item_DropCollectible(globalCtx, &this->actor.posRot.pos, ITEM00_SEEDS);
             } else {
                 Item_DropCollectible(globalCtx, &this->actor.posRot.pos, ITEM00_HEART);
@@ -143,7 +143,7 @@ void EnKusa_Fall(EnKusa* this) {
 }
 
 void func_80A9B174(Vec3f* vec, f32 arg1) {
-    arg1 += ((Math_Rand_ZeroOne() * 0.2f) - 0.1f) * arg1;
+    arg1 += ((Rand_ZeroOne() * 0.2f) - 0.1f) * arg1;
     vec->x -= vec->x * arg1;
     vec->y -= vec->y * arg1;
     vec->z -= vec->z * arg1;
@@ -170,11 +170,11 @@ void EnKusa_SpawnFragments(EnKusa* this, GlobalContext* globalCtx) {
         pos.y = this->actor.posRot.pos.y + (scale->y * this->actor.scale.y * 20.0f) + 10.0f;
         pos.z = this->actor.posRot.pos.z + (scale->z * this->actor.scale.z * 20.0f);
 
-        velocity.x = (Math_Rand_ZeroOne() - 0.5f) * 8.0f;
-        velocity.y = Math_Rand_ZeroOne() * 10.0f;
-        velocity.z = (Math_Rand_ZeroOne() - 0.5f) * 8.0f;
+        velocity.x = (Rand_ZeroOne() - 0.5f) * 8.0f;
+        velocity.y = Rand_ZeroOne() * 10.0f;
+        velocity.z = (Rand_ZeroOne() - 0.5f) * 8.0f;
 
-        index = (s32)(Math_Rand_ZeroOne() * 111.1f) & 7;
+        index = (s32)(Rand_ZeroOne() * 111.1f) & 7;
 
         EffectSsKakera_Spawn(globalCtx, &pos, &velocity, &pos, -100, 64, 40, 3, 0, D_80A9C26C[index], 0, 0, 80,
                              KAKERA_COLOR_NONE, OBJECT_GAMEPLAY_KEEP, D_040355E0);
@@ -183,11 +183,11 @@ void EnKusa_SpawnFragments(EnKusa* this, GlobalContext* globalCtx) {
         pos.y = this->actor.posRot.pos.y + (scale->y * this->actor.scale.y * 40.0f) + 10.0f;
         pos.z = this->actor.posRot.pos.z + (scale->z * this->actor.scale.z * 40.0f);
 
-        velocity.x = (Math_Rand_ZeroOne() - 0.5f) * 6.0f;
-        velocity.y = Math_Rand_ZeroOne() * 10.0f;
-        velocity.z = (Math_Rand_ZeroOne() - 0.5f) * 6.0f;
+        velocity.x = (Rand_ZeroOne() - 0.5f) * 6.0f;
+        velocity.y = Rand_ZeroOne() * 10.0f;
+        velocity.z = (Rand_ZeroOne() - 0.5f) * 6.0f;
 
-        index = (s32)(Math_Rand_ZeroOne() * 111.1f) % 7;
+        index = (s32)(Rand_ZeroOne() * 111.1f) % 7;
 
         EffectSsKakera_Spawn(globalCtx, &pos, &velocity, &pos, -100, 64, 40, 3, 0, D_80A9C26C[index], 0, 0, 80,
                              KAKERA_COLOR_NONE, OBJECT_GAMEPLAY_KEEP, D_040356A0);
@@ -198,9 +198,8 @@ void EnKusa_SpawnBugs(EnKusa* this, GlobalContext* globalCtx) {
     s32 i;
 
     for (i = 0; i < 3; i++) {
-        Actor* bug =
-            Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_EN_INSECT, this->actor.posRot.pos.x,
-                        this->actor.posRot.pos.y, this->actor.posRot.pos.z, 0, Math_Rand_ZeroOne() * 0xFFFF, 0, 1);
+        Actor* bug = Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_EN_INSECT, this->actor.posRot.pos.x,
+                                 this->actor.posRot.pos.y, this->actor.posRot.pos.z, 0, Rand_ZeroOne() * 0xFFFF, 0, 1);
 
         if (bug == NULL) {
             break;
@@ -229,7 +228,7 @@ void EnKusa_Init(Actor* thisx, GlobalContext* globalCtx) {
     func_80061ED4(&this->actor.colChkInfo, NULL, &sColChkInfoInit);
 
     if (this->actor.shape.rot.y == 0) {
-        s16 rand = Math_Rand_ZeroFloat(0x10000);
+        s16 rand = Rand_ZeroFloat(0x10000);
 
         this->actor.posRot.rot.y = rand;
         this->actor.initPosRot.rot.y = rand;
@@ -335,8 +334,8 @@ void EnKusa_LiftedUp(EnKusa* this, GlobalContext* globalCtx) {
     if (Actor_HasNoParent(&this->actor, globalCtx)) {
         this->actor.room = globalCtx->roomCtx.curRoom.num;
         func_80A9BBB0(this);
-        this->actor.velocity.x = this->actor.speedXZ * Math_Sins(this->actor.posRot.rot.y);
-        this->actor.velocity.z = this->actor.speedXZ * Math_Coss(this->actor.posRot.rot.y);
+        this->actor.velocity.x = this->actor.speedXZ * Math_SinS(this->actor.posRot.rot.y);
+        this->actor.velocity.z = this->actor.speedXZ * Math_CosS(this->actor.posRot.rot.y);
         this->actor.colChkInfo.mass = 240;
         this->actor.gravity = -0.1f;
         EnKusa_Fall(this);
@@ -350,7 +349,7 @@ void EnKusa_LiftedUp(EnKusa* this, GlobalContext* globalCtx) {
 void func_80A9BBB0(EnKusa* this) {
     EnKusa_SetupAction(this, func_80A9BC1C);
     D_80A9C1D0 = -0xBB8;
-    D_80A9C1D8 = ((Math_Rand_ZeroOne() - 0.5f) * 1600.0f);
+    D_80A9C1D8 = ((Rand_ZeroOne() - 0.5f) * 1600.0f);
     D_80A9C1D4 = 0;
     D_80A9C1DC = 0;
 }
@@ -393,8 +392,8 @@ void func_80A9BC1C(EnKusa* this, GlobalContext* globalCtx) {
             Audio_PlaySoundAtPosition(globalCtx, &this->actor.posRot.pos, 40, NA_SE_EV_DIVE_INTO_WATER_L);
         }
         EnKusa_Fall(this);
-        Math_ApproxS(&D_80A9C1D4, D_80A9C1D0, 0x1F4);
-        Math_ApproxS(&D_80A9C1DC, D_80A9C1D8, 0xAA);
+        Math_StepToS(&D_80A9C1D4, D_80A9C1D0, 0x1F4);
+        Math_StepToS(&D_80A9C1DC, D_80A9C1D8, 0xAA);
         this->actor.shape.rot.x += D_80A9C1D4;
         this->actor.shape.rot.y += D_80A9C1DC;
         func_80A9B174(&this->actor.velocity, 0.05f);
@@ -436,7 +435,7 @@ void func_80A9BF3C(EnKusa* this) {
 
 void func_80A9BFA8(EnKusa* this, GlobalContext* globalCtx) {
     if (this->timer > 120) {
-        if ((Math_ApproxF(&this->actor.posRot.pos.y, this->actor.initPosRot.pos.y, 0.6f)) && (this->timer >= 170)) {
+        if ((Math_StepToF(&this->actor.posRot.pos.y, this->actor.initPosRot.pos.y, 0.6f)) && (this->timer >= 170)) {
             func_80A9C00C(this);
         }
     }
@@ -452,8 +451,8 @@ void func_80A9C00C(EnKusa* this) {
 void func_80A9C068(EnKusa* this, GlobalContext* globalCtx) {
     s32 sp24;
 
-    sp24 = Math_ApproxF(&this->actor.scale.y, 0.4f, 0.014f) & 1;
-    sp24 &= Math_ApproxF(&this->actor.scale.x, 0.4f, 0.011f);
+    sp24 = Math_StepToF(&this->actor.scale.y, 0.4f, 0.014f) & 1;
+    sp24 &= Math_StepToF(&this->actor.scale.x, 0.4f, 0.011f);
     this->actor.scale.z = this->actor.scale.x;
 
     if (sp24) {

src/overlays/actors/ovl_Mir_Ray/z_mir_ray.c
@@ -124,12 +124,12 @@ void MirRay_MakeShieldLight(MirRay* this, GlobalContext* globalCtx) {
         lightPt.z = (dataEntry->unk_18 * reflectionPt.z) + this->sourcePt.z;
 
         // Fade up
-        Math_ApproxS(&this->lightPointRad, dataEntry->lgtPtMaxRad, 6);
+        Math_StepToS(&this->lightPointRad, dataEntry->lgtPtMaxRad, 6);
         Lights_PointNoGlowSetInfo(&this->lightInfo, lightPt.x, lightPt.y, lightPt.z, dataEntry->color.r,
                                   dataEntry->color.g, dataEntry->color.b, this->lightPointRad);
     } else {
         // Fade down
-        Math_ApproxS(&this->lightPointRad, 0, 6);
+        Math_StepToS(&this->lightPointRad, 0, 6);
         Lights_PointSetColorAndRadius(&this->lightInfo, dataEntry->color.r, dataEntry->color.g, dataEntry->color.b,
                                       this->lightPointRad);
     }

Dragorn421/oot

src/overlays/actors/ovl_Bg_Jya_Haheniron/z_bg_jya_haheniron.c
@@ -71,11 +71,11 @@ void func_80898114(GlobalContext* globalCtx, Vec3f* arg1, Vec3f* arg2) {
     s32 var_v0;
 
     for (var_s1 = 0; var_s1 < ARRAY_COUNT(D_80898774); var_s1++) {
-        temp_fv0 = Math_Rand_ZeroOne() * 10.0f;
-        spB4.x = (Math_Sins(var_s1 * 0x3333) * temp_fv0) + arg2->x;
-        spB4.y = (Math_Rand_ZeroOne() * 10.0f) + arg2->y;
-        spB4.z = (Math_Coss(var_s1 * 0x3333) * temp_fv0) + arg2->z;
-        temp_fv0 = Math_Rand_ZeroOne();
+        temp_fv0 = Rand_ZeroOne() * 10.0f;
+        spB4.x = (Math_SinS(var_s1 * 0x3333) * temp_fv0) + arg2->x;
+        spB4.y = (Rand_ZeroOne() * 10.0f) + arg2->y;
+        spB4.z = (Math_CosS(var_s1 * 0x3333) * temp_fv0) + arg2->z;
+        temp_fv0 = Rand_ZeroOne();
         if (temp_fv0 < 0.2f) {
             var_v0 = 0x60;
         } else if (temp_fv0 < 0.8f) {
@@ -99,7 +99,7 @@ void BgJyaHaheniron_Init(Actor* thisx, GlobalContext* globalCtx) {
     Actor_SetScale(&this->actor, D_80898794[this->actor.params]);
     if (this->actor.params == 0) {
         func_808980C0(this, globalCtx);
-        this->actor.shape.rot.z = (s16)(s32)(Math_Rand_ZeroOne() * 65535.0f);
+        this->actor.shape.rot.z = (s16)(s32)(Rand_ZeroOne() * 65535.0f);
         func_8089843C(this);
     } else if (this->actor.params == 1) {
         func_80898588(this);
@@ -127,9 +127,9 @@ void func_8089844C(BgJyaHaheniron* this, GlobalContext* globalCtx) {
     func_8002E4B4(globalCtx, &this->actor, 5.0f, 8.0f, 0.0f, 0x85);
     if ((this->actor.bgCheckFlags & 9) ||
         ((this->unk150.base.atFlags & 2) && (this->unk150.base.at != NULL) && (this->unk150.base.at->type == 2))) {
-        sp2C.x = -Math_Rand_ZeroOne() * this->actor.velocity.x;
-        sp2C.y = -Math_Rand_ZeroOne() * this->actor.velocity.y;
-        sp2C.z = -Math_Rand_ZeroOne() * this->actor.velocity.z;
+        sp2C.x = -Rand_ZeroOne() * this->actor.velocity.x;
+        sp2C.y = -Rand_ZeroOne() * this->actor.velocity.y;
+        sp2C.z = -Rand_ZeroOne() * this->actor.velocity.z;
         func_80898114(globalCtx, &this->actor.posRot.pos, &sp2C);
         Actor_Kill(&this->actor);
     } else if (this->unk1B0 >= 0x3D) {

src/overlays/actors/ovl_En_Bom_Bowl_Pit/z_en_bom_bowl_pit.c
@@ -112,12 +112,12 @@ void func_809C4E8C(EnBomBowlPit* this, GlobalContext* globalCtx) {
 
 void func_809C5184(EnBomBowlPit* this, GlobalContext* globalCtx) {
     if (this->unk152 != 0) {
-        Math_SmoothScaleMaxF(&this->unk180.x, this->unk1BC.x, this->unk1C8.x, this->unk1D4.x);
-        Math_SmoothScaleMaxF(&this->unk180.y, this->unk1BC.y, this->unk1C8.y, this->unk1D4.y);
-        Math_SmoothScaleMaxF(&this->unk180.z, this->unk1BC.z, this->unk1C8.z, this->unk1D4.z);
-        Math_SmoothScaleMaxF(&this->unk18C.x, this->unk198.x, this->unk1A4.x, this->unk1B0.x);
-        Math_SmoothScaleMaxF(&this->unk18C.y, this->unk198.y, this->unk1A4.y, this->unk1B0.y);
-        Math_SmoothScaleMaxF(&this->unk18C.z, this->unk198.z, this->unk1A4.z, this->unk1B0.z);
+        Math_ApproachF(&this->unk180.x, this->unk1BC.x, this->unk1C8.x, this->unk1D4.x);
+        Math_ApproachF(&this->unk180.y, this->unk1BC.y, this->unk1C8.y, this->unk1D4.y);
+        Math_ApproachF(&this->unk180.z, this->unk1BC.z, this->unk1C8.z, this->unk1D4.z);
+        Math_ApproachF(&this->unk18C.x, this->unk198.x, this->unk1A4.x, this->unk1B0.x);
+        Math_ApproachF(&this->unk18C.y, this->unk198.y, this->unk1A4.y, this->unk1B0.y);
+        Math_ApproachF(&this->unk18C.z, this->unk198.z, this->unk1A4.z, this->unk1B0.z);
     }
     Gameplay_CameraSetAtEye(globalCtx, this->unk152, &this->unk180, &this->unk18C);
     if ((this->unk154 == func_8010BDBC(&globalCtx->msgCtx)) && (func_80106BC8(globalCtx) != 0)) {

src/overlays/actors/ovl_En_Kusa/z_en_kusa.c
@@ -118,7 +118,7 @@ void func_80A9B07C(EnKusa* this, GlobalContext* globalCtx) {
             Item_DropCollectibleRandom(globalCtx, NULL, &this->actor.posRot.pos, var_v0 * 0x10);
             break;
         case 1:
-            if (Math_Rand_ZeroOne() < 0.5f) {
+            if (Rand_ZeroOne() < 0.5f) {
                 Item_DropCollectible(globalCtx, &this->actor.posRot.pos, 0x10);
             } else {
                 Item_DropCollectible(globalCtx, &this->actor.posRot.pos, 3);
@@ -135,7 +135,7 @@ void func_80A9B140(EnKusa* this) {
 }
 
 void func_80A9B174(Vec3f* arg0, f32 arg1) {
-    arg1 += ((Math_Rand_ZeroOne() * 0.2f) - 0.1f) * arg1;
+    arg1 += ((Rand_ZeroOne() * 0.2f) - 0.1f) * arg1;
     arg0->x -= arg0->x * arg1;
     arg0->y -= arg0->y * arg1;
     arg0->z -= arg0->z * arg1;
@@ -160,20 +160,20 @@ void func_80A9B21C(EnKusa* this, GlobalContext* globalCtx) {
         spB8.x = this->actor.posRot.pos.x + (var_s1->x * this->actor.scale.x * 20.0f);
         spB8.y = this->actor.posRot.pos.y + (var_s1->y * this->actor.scale.y * 20.0f) + 10.0f;
         spB8.z = this->actor.posRot.pos.z + (var_s1->z * this->actor.scale.z * 20.0f);
-        spC4.x = (Math_Rand_ZeroOne() - 0.5f) * 8.0f;
-        spC4.y = Math_Rand_ZeroOne() * 10.0f;
-        spC4.z = (Math_Rand_ZeroOne() - 0.5f) * 8.0f;
-        new_var = (s32)(Math_Rand_ZeroOne() * 111.1f) & 7;
+        spC4.x = (Rand_ZeroOne() - 0.5f) * 8.0f;
+        spC4.y = Rand_ZeroOne() * 10.0f;
+        spC4.z = (Rand_ZeroOne() - 0.5f) * 8.0f;
+        new_var = (s32)(Rand_ZeroOne() * 111.1f) & 7;
         EffectSsKakera_Spawn(globalCtx, &spB8, &spC4, &spB8, -100, 0x40, 0x28, 3, 0, D_80A9C26C[new_var], 0, 0, 80, -1,
                              1, D_40355E0);
 
         spB8.x = this->actor.posRot.pos.x + (var_s1->x * this->actor.scale.x * 40.0f);
         spB8.y = this->actor.posRot.pos.y + (var_s1->y * this->actor.scale.y * 40.0f) + 10.0f;
         spB8.z = this->actor.posRot.pos.z + (var_s1->z * this->actor.scale.z * 40.0f);
-        spC4.x = (Math_Rand_ZeroOne() - 0.5f) * 6.0f;
-        spC4.y = Math_Rand_ZeroOne() * 10.0f;
-        spC4.z = (Math_Rand_ZeroOne() - 0.5f) * 6.0f;
-        new_var = (s32)(Math_Rand_ZeroOne() * 111.1f) % 7;
+        spC4.x = (Rand_ZeroOne() - 0.5f) * 6.0f;
+        spC4.y = Rand_ZeroOne() * 10.0f;
+        spC4.z = (Rand_ZeroOne() - 0.5f) * 6.0f;
+        new_var = (s32)(Rand_ZeroOne() * 111.1f) % 7;
         EffectSsKakera_Spawn(globalCtx, &spB8, &spC4, &spB8, -100, 0x40, 0x28, 3, 0, D_80A9C26C[new_var], 0, 0, 80, -1,
                              1, D_40356A0);
     }
@@ -185,7 +185,7 @@ void func_80A9B574(EnKusa* this, GlobalContext* globalCtx) {
     for (var_s0 = 0; var_s0 < 3; var_s0++) {
         if (Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_EN_INSECT, this->actor.posRot.pos.x,
                         this->actor.posRot.pos.y, this->actor.posRot.pos.z, 0,
-                        (s16)(s32)(Math_Rand_ZeroOne() * 65535.0f), 0, 1) == NULL) {
+                        (s16)(s32)(Rand_ZeroOne() * 65535.0f), 0, 1) == NULL) {
             break;
         }
     }
@@ -210,7 +210,7 @@ void EnKusa_Init(Actor* thisx, GlobalContext* globalCtx) {
     func_80061ED4(&this->actor.colChkInfo, NULL, &D_80A9C234);
     if (this->actor.shape.rot.y == 0) {
         this->actor.shape.rot.y = this->actor.initPosRot.rot.y = this->actor.posRot.rot.y =
-            (s16)(s32)Math_Rand_ZeroFloat(65536.0f);
+            (s16)(s32)Rand_ZeroFloat(65536.0f);
     }
     if (func_80A9AFAC(this, globalCtx, 0.0f) == 0) {
         Actor_Kill(&this->actor);
@@ -301,8 +301,8 @@ void func_80A9BAD8(EnKusa* this, GlobalContext* globalCtx) {
     if (Actor_HasNoParent(&this->actor, globalCtx)) {
         this->actor.room = globalCtx->roomCtx.curRoom.num;
         func_80A9BBB0(this);
-        this->actor.velocity.x = Math_Sins(this->actor.posRot.rot.y) * this->actor.speedXZ;
-        this->actor.velocity.z = Math_Coss(this->actor.posRot.rot.y) * this->actor.speedXZ;
+        this->actor.velocity.x = Math_SinS(this->actor.posRot.rot.y) * this->actor.speedXZ;
+        this->actor.velocity.z = Math_CosS(this->actor.posRot.rot.y) * this->actor.speedXZ;
         this->actor.colChkInfo.mass = 240;
         this->actor.gravity = -0.1f;
         func_80A9B140(this);
@@ -316,7 +316,7 @@ void func_80A9BAD8(EnKusa* this, GlobalContext* globalCtx) {
 void func_80A9BBB0(EnKusa* this) {
     func_80A9AFA0(this, func_80A9BC1C);
     D_80A9C1D0 = -0xBB8;
-    D_80A9C1D8 = (s16)(s32)((Math_Rand_ZeroOne() - 0.5f) * 1600.0f);
+    D_80A9C1D8 = (s16)(s32)((Rand_ZeroOne() - 0.5f) * 1600.0f);
     D_80A9C1D4 = 0;
     D_80A9C1DC = 0;
 }
@@ -358,8 +358,8 @@ void func_80A9BC1C(EnKusa* this, GlobalContext* globalCtx) {
             Audio_PlaySoundAtPosition(globalCtx, &this->actor.posRot.pos, 40, NA_SE_EV_DIVE_INTO_WATER_L);
         }
         func_80A9B140(this);
-        Math_ApproxS(&D_80A9C1D4, D_80A9C1D0, 0x1F4);
-        Math_ApproxS(&D_80A9C1DC, D_80A9C1D8, 0xAA);
+        Math_StepToS(&D_80A9C1D4, D_80A9C1D0, 0x1F4);
+        Math_StepToS(&D_80A9C1DC, D_80A9C1D8, 0xAA);
         this->actor.shape.rot.x += D_80A9C1D4;
         this->actor.shape.rot.y += D_80A9C1DC;
         func_80A9B174(&this->actor.velocity, 0.05f);
@@ -400,7 +400,7 @@ void func_80A9BF3C(EnKusa* this) {
 }
 
 void func_80A9BFA8(EnKusa* this, GlobalContext* globalCtx) {
-    if ((this->unk19C >= 0x79) && Math_ApproxF(&this->actor.posRot.pos.y, this->actor.initPosRot.pos.y, 0.6f) &&
+    if ((this->unk19C >= 0x79) && Math_StepToF(&this->actor.posRot.pos.y, this->actor.initPosRot.pos.y, 0.6f) &&
         (this->unk19C >= 0xAA)) {
         func_80A9C00C(this);
     }
@@ -416,8 +416,8 @@ void func_80A9C00C(EnKusa* this) {
 void func_80A9C068(EnKusa* this, GlobalContext* globalCtx) {
     s32 temp_v1 = 1;
 
-    temp_v1 &= Math_ApproxF(&this->actor.scale.y, 0.4f, 0.014f);
-    temp_v1 &= Math_ApproxF(&this->actor.scale.x, 0.4f, 0.011f);
+    temp_v1 &= Math_StepToF(&this->actor.scale.y, 0.4f, 0.014f);
+    temp_v1 &= Math_StepToF(&this->actor.scale.x, 0.4f, 0.011f);
     this->actor.scale.z = this->actor.scale.x;
     if (temp_v1) {
         Actor_SetScale(&this->actor, 0.4f);

src/overlays/actors/ovl_Mir_Ray/z_mir_ray.c
@@ -249,12 +249,12 @@ void MirRay_UpdateLight(MirRay* this, GlobalContext* globalCtx) {
         pointLightPos.x = (temp_s1->unk18_pointLightPosRatio * sp44.x) + this->coneFrustumCenterTop.x;
         pointLightPos.y = (temp_s1->unk18_pointLightPosRatio * sp44.y) + this->coneFrustumCenterTop.y;
         pointLightPos.z = (temp_s1->unk18_pointLightPosRatio * sp44.z) + this->coneFrustumCenterTop.z;
-        Math_ApproxS(&this->pointLightRadius, temp_s1->unk16_pointLightRadius, 6);
+        Math_StepToS(&this->pointLightRadius, temp_s1->unk16_pointLightRadius, 6);
         Lights_PointNoGlowSetInfo(&this->lightInfo, pointLightPos.x, pointLightPos.y, pointLightPos.z,
                                   temp_s1->unk1C_lightColor.r, temp_s1->unk1C_lightColor.g, temp_s1->unk1C_lightColor.b,
                                   this->pointLightRadius);
     } else {
-        Math_ApproxS(&this->pointLightRadius, 0, 6);
+        Math_StepToS(&this->pointLightRadius, 0, 6);
         Lights_PointSetColorAndRadius(&this->lightInfo, temp_s1->unk1C_lightColor.r, temp_s1->unk1C_lightColor.g,
                                       temp_s1->unk1C_lightColor.b, this->pointLightRadius);
     }