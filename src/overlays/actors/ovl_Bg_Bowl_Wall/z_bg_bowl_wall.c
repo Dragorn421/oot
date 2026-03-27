HackerN64/HackerOoT

Actor Struct Changes (and a few related things) (#617)
* reformat header

* type -> category

* done for now i think

* some more stuff

* first -> head

* focus

* flag comment

* ground -> floor

* remove asm, name wrapper funcs

* name func, format

* review

* targetPriority, format

* git subrepo pull --force tools/ZAPD

subrepo:
  subdir:   "tools/ZAPD"
  merged:   "0305ec2c2"
upstream:
  origin:   "https://github.com/zeldaret/ZAPD.git"
  branch:   "master"
  commit:   "0305ec2c2"
git-subrepo:
  version:  "0.4.3"
  origin:   "https://github.com/ingydotnet/git-subrepo.git"
  commit:   "2f68596"

* comment

* review

* feet flags

* horse shadow

src/overlays/actors/ovl_Bg_Bowl_Wall/z_bg_bowl_wall.c
@@ -31,7 +31,7 @@ extern Gfx D_06001390[];
 
 const ActorInit Bg_Bowl_Wall_InitVars = {
     ACTOR_BG_BOWL_WALL,
-    ACTORTYPE_PROP,
+    ACTORCAT_PROP,
     FLAGS,
     OBJECT_BOWL,
     sizeof(BgBowlWall),
@@ -65,7 +65,7 @@ void BgBowlWall_Init(Actor* thisx, GlobalContext* globalCtx) {
     }
 
     this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, colHeader);
-    this->initPos = this->dyna.actor.posRot.pos;
+    this->initPos = this->dyna.actor.world.pos;
     osSyncPrintf("\n\n");
     osSyncPrintf(VT_FGCOL(GREEN) " ☆☆☆☆☆ ボーリングおじゃま壁発生 ☆☆☆☆☆ %d\n" VT_RST, this->dyna.actor.params);
     this->actionFunc = BgBowlWall_SpawnBullseyes;
@@ -87,12 +87,12 @@ void BgBowlWall_SpawnBullseyes(BgBowlWall* this, GlobalContext* globalCtx) {
     type = this->dyna.actor.params;
     if (type != 0) {
         type += (s16)Rand_ZeroFloat(2.99f);
-        this->dyna.actor.shape.rot.z = this->dyna.actor.posRot.rot.z = sTargetRot[type];
+        this->dyna.actor.shape.rot.z = this->dyna.actor.world.rot.z = sTargetRot[type];
         osSyncPrintf("\n\n");
     }
-    this->bullseyeCenter.x = sBullseyeOffset[type].x + this->dyna.actor.posRot.pos.x;
-    this->bullseyeCenter.y = sBullseyeOffset[type].y + this->dyna.actor.posRot.pos.y;
-    this->bullseyeCenter.z = sBullseyeOffset[type].z + this->dyna.actor.posRot.pos.z;
+    this->bullseyeCenter.x = sBullseyeOffset[type].x + this->dyna.actor.world.pos.x;
+    this->bullseyeCenter.y = sBullseyeOffset[type].y + this->dyna.actor.world.pos.y;
+    this->bullseyeCenter.z = sBullseyeOffset[type].z + this->dyna.actor.world.pos.z;
     if (1) {}
     bullseye = (EnWallTubo*)Actor_SpawnAsChild(&globalCtx->actorCtx, &this->dyna.actor, globalCtx, ACTOR_EN_WALL_TUBO,
                                                this->bullseyeCenter.x, this->bullseyeCenter.y, this->bullseyeCenter.z,
@@ -100,10 +100,10 @@ void BgBowlWall_SpawnBullseyes(BgBowlWall* this, GlobalContext* globalCtx) {
     if (bullseye != NULL) {
         bullseye->explosionCenter = this->bullseyeCenter;
         if (type != 0) {
-            bullseye->explosionCenter = this->bullseyeCenter = this->dyna.actor.posRot.pos;
+            bullseye->explosionCenter = this->bullseyeCenter = this->dyna.actor.world.pos;
         }
         if (this->chuGirl == NULL) {
-            lookForGirl = globalCtx->actorCtx.actorList[ACTORTYPE_NPC].first;
+            lookForGirl = globalCtx->actorCtx.actorLists[ACTORCAT_NPC].head;
             while (lookForGirl != NULL) {
                 if (lookForGirl->id != ACTOR_EN_BOM_BOWL_MAN) {
                     lookForGirl = lookForGirl->next;
@@ -136,13 +136,13 @@ void BgBowlWall_FallDoEffects(BgBowlWall* this, GlobalContext* globalCtx) {
 
     if (this->dyna.actor.params == 0) { // wall collapses backwards
         Math_SmoothStepToS(&this->dyna.actor.shape.rot.x, -0x3E80, 3, 500, 0);
-        this->dyna.actor.posRot.rot.x = this->dyna.actor.shape.rot.x;
+        this->dyna.actor.world.rot.x = this->dyna.actor.shape.rot.x;
         if (this->dyna.actor.shape.rot.x < -0x3C1E) {
             wallFallen = true;
         }
     } else { // wall slides downwards
-        Math_ApproachF(&this->dyna.actor.posRot.pos.y, this->initPos.y - 450.0f, 0.3f, 10.0f);
-        if (this->dyna.actor.posRot.pos.y < (this->initPos.y - 400.0f)) {
+        Math_ApproachF(&this->dyna.actor.world.pos.y, this->initPos.y - 450.0f, 0.3f, 10.0f);
+        if (this->dyna.actor.world.pos.y < (this->initPos.y - 400.0f)) {
             wallFallen = true;
         }
     }
@@ -171,11 +171,11 @@ void BgBowlWall_FinishFall(BgBowlWall* this, GlobalContext* globalCtx) {
         if (this->dyna.actor.params == 0) {
             Math_SmoothStepToS(&this->dyna.actor.shape.rot.x, -0x3E80, 1, 200, 0);
         } else {
-            Math_ApproachF(&this->dyna.actor.posRot.pos.y, this->initPos.y - 450.0f, 0.3f, 10.0f);
+            Math_ApproachF(&this->dyna.actor.world.pos.y, this->initPos.y - 450.0f, 0.3f, 10.0f);
         }
     } else if (this->timer == 1) {
-        this->dyna.actor.posRot.rot.x = this->dyna.actor.shape.rot.x = 0;
-        this->dyna.actor.posRot.pos.y = this->initPos.y - 450.0f;
+        this->dyna.actor.world.rot.x = this->dyna.actor.shape.rot.x = 0;
+        this->dyna.actor.world.pos.y = this->initPos.y - 450.0f;
         this->chuGirl->wallStatus[this->dyna.actor.params] = 2;
         this->actionFunc = BgBowlWall_Reset;
     }
@@ -183,9 +183,9 @@ void BgBowlWall_FinishFall(BgBowlWall* this, GlobalContext* globalCtx) {
 
 void BgBowlWall_Reset(BgBowlWall* this, GlobalContext* globalCtx) {
     if (this->chuGirl->wallStatus[this->dyna.actor.params] != 2) {
-        Math_ApproachF(&this->dyna.actor.posRot.pos.y, this->initPos.y, 0.3f, 50.0f);
-        if (fabsf(this->dyna.actor.posRot.pos.y - this->initPos.y) <= 10.0f) {
-            this->dyna.actor.posRot.pos.y = this->initPos.y;
+        Math_ApproachF(&this->dyna.actor.world.pos.y, this->initPos.y, 0.3f, 50.0f);
+        if (fabsf(this->dyna.actor.world.pos.y - this->initPos.y) <= 10.0f) {
+            this->dyna.actor.world.pos.y = this->initPos.y;
             this->isHit = false;
             this->actionFunc = BgBowlWall_SpawnBullseyes;
         }

src/overlays/actors/ovl_En_Dns/z_en_dns.c
@@ -46,7 +46,7 @@ void EnDns_Burrow(EnDns* this, GlobalContext* globalCtx);
 
 const ActorInit En_Dns_InitVars = {
     ACTOR_EN_DNS,
-    ACTORTYPE_BG,
+    ACTORCAT_BG,
     FLAGS,
     OBJECT_SHOPNUTS,
     sizeof(EnDns),
@@ -117,8 +117,8 @@ DnsItemEntry* D_809F0500[] = {
 
 static InitChainEntry sInitChain[] = {
     ICHAIN_S8(naviEnemyId, 78, ICHAIN_CONTINUE),
-    ICHAIN_U8(unk_1F, 2, ICHAIN_CONTINUE),
-    ICHAIN_F32(unk_4C, 30, ICHAIN_STOP),
+    ICHAIN_U8(targetMode, 2, ICHAIN_CONTINUE),
+    ICHAIN_F32(targetArrowOffset, 30, ICHAIN_STOP),
 };
 
 typedef struct {
@@ -157,7 +157,7 @@ void EnDns_Init(Actor* thisx, GlobalContext* globalCtx) {
     SkelAnime_InitFlex(globalCtx, &this->skelAnime, &D_060041A8, &D_060009A0, this->jointTable, this->morphTable, 18);
     Collider_InitCylinder(globalCtx, &this->collider);
     Collider_SetCylinderType1(globalCtx, &this->collider, &this->actor, &sCylinderInit);
-    ActorShape_Init(&this->actor.shape, 0.0f, &ActorShadow_DrawFunc_Circle, 35.0f);
+    ActorShape_Init(&this->actor.shape, 0.0f, &ActorShadow_DrawCircle, 35.0f);
     this->actor.textId = D_809F040C[this->actor.params];
     Actor_SetScale(&this->actor, 0.01f);
     this->actor.colChkInfo.mass = MASS_IMMOVABLE;
@@ -324,17 +324,17 @@ void EnDns_SetupWait(EnDns* this, GlobalContext* globalCtx) {
 }
 
 void EnDns_Wait(EnDns* this, GlobalContext* globalCtx) {
-    Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsLink, 3, 2000, 0);
-    this->actor.posRot.rot.y = this->actor.shape.rot.y;
+    Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsPlayer, 3, 2000, 0);
+    this->actor.world.rot.y = this->actor.shape.rot.y;
     if (func_8002F194(&this->actor, globalCtx)) {
         this->actionFunc = EnDns_Talk;
     } else {
-        if ((this->collider.base.ocFlags1 & OC1_HIT) || (this->actor.unk_10C != 0)) {
+        if ((this->collider.base.ocFlags1 & OC1_HIT) || this->actor.isTargeted) {
             this->actor.flags |= 0x10000;
         } else {
             this->actor.flags &= ~0x10000;
         }
-        if (this->actor.xzDistToLink < 130.0f) {
+        if (this->actor.xzDistToPlayer < 130.0f) {
             func_8002F2F4(&this->actor, globalCtx);
         }
     }
@@ -444,7 +444,7 @@ void EnDns_SetupBurrow(EnDns* this, GlobalContext* globalCtx) {
         Audio_PlayActorSound2(&this->actor, NA_SE_EN_AKINDONUTS_HIDE);
         this->actionFunc = EnDns_Burrow;
         this->standOnGround = 0;
-        this->yInitPos = this->actor.posRot.pos.y;
+        this->yInitPos = this->actor.world.pos.y;
     }
 }
 
@@ -453,20 +453,20 @@ void EnDns_Burrow(EnDns* this, GlobalContext* globalCtx) {
     Vec3f initPos;
     s32 i;
 
-    depth = this->yInitPos - this->actor.posRot.pos.y;
+    depth = this->yInitPos - this->actor.world.pos.y;
     if ((this->dustTimer & 3) == 0) {
-        initPos.x = this->actor.posRot.pos.x;
+        initPos.x = this->actor.world.pos.x;
         initPos.y = this->yInitPos;
-        initPos.z = this->actor.posRot.pos.z;
+        initPos.z = this->actor.world.pos.z;
         func_80028990(globalCtx, 20.0f, &initPos);
     }
     this->actor.shape.rot.y += 0x2000;
     // Drops only if you bought its item
     if (depth > 400.0f) {
         if (this->dropCollectible) {
-            initPos.x = this->actor.posRot.pos.x;
+            initPos.x = this->actor.world.pos.x;
             initPos.y = this->yInitPos;
-            initPos.z = this->actor.posRot.pos.z;
+            initPos.z = this->actor.world.pos.z;
             for (i = 0; i < 3; i++) {
                 Item_DropCollectible(globalCtx, &initPos, ITEM00_HEART);
             }
@@ -481,13 +481,13 @@ void EnDns_Update(Actor* thisx, GlobalContext* globalCtx) {
 
     this->dustTimer++;
     this->actor.textId = D_809F040C[this->actor.params];
-    Actor_SetHeight(&this->actor, 60.0f);
+    Actor_SetFocus(&this->actor, 60.0f);
     Actor_SetScale(&this->actor, 0.01f);
     SkelAnime_Update(&this->skelAnime);
     Actor_MoveForward(&this->actor);
     this->actionFunc(this, globalCtx);
     if (this->standOnGround) {
-        func_8002E4B4(globalCtx, &this->actor, 20.0f, 20.0f, 20.0f, 4);
+        Actor_UpdateBgCheckInfo(globalCtx, &this->actor, 20.0f, 20.0f, 20.0f, 4);
     }
     if (this->maintainCollider) {
         Collider_UpdateCylinder(&this->actor, &this->collider);

Dragorn421/oot

src/overlays/actors/ovl_Bg_Bowl_Wall/z_bg_bowl_wall.c
@@ -30,7 +30,7 @@ extern CollisionHeader D_6001B00;
 
 const ActorInit Bg_Bowl_Wall_InitVars = {
     ACTOR_BG_BOWL_WALL,
-    ACTORTYPE_PROP,
+    ACTORCAT_PROP,
     FLAGS,
     OBJECT_BOWL,
     sizeof(BgBowlWall),
@@ -63,7 +63,7 @@ void BgBowlWall_Init(Actor* thisx, GlobalContext* globalCtx) {
         CollisionHeader_GetVirtual(&D_6001B00, &sp28);
     }
     this->dyna.bgId = DynaPoly_SetBgActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, sp28);
-    this->unk168 = this->dyna.actor.posRot.pos;
+    this->unk168 = this->dyna.actor.world.pos;
     osSyncPrintf("\n\n");
     osSyncPrintf("\x1b[32m ☆☆☆☆☆ ボーリングおじゃま壁発生 ☆☆☆☆☆ %d\n\x1b[m", this->dyna.actor.params);
     this->actionFunc = func_8086F260;
@@ -86,12 +86,12 @@ void func_8086F260(BgBowlWall* this, GlobalContext* globalCtx) {
 
     if (params != 0) {
         params += (s16)Rand_ZeroFloat(2.99f);
-        this->dyna.actor.shape.rot.z = this->dyna.actor.posRot.rot.z = D_8086FA70[params];
+        this->dyna.actor.shape.rot.z = this->dyna.actor.world.rot.z = D_8086FA70[params];
         osSyncPrintf("\n\n");
     }
-    this->unk174.x = D_8086FA40[params].x + this->dyna.actor.posRot.pos.x;
-    this->unk174.y = D_8086FA40[params].y + this->dyna.actor.posRot.pos.y;
-    this->unk174.z = D_8086FA40[params].z + this->dyna.actor.posRot.pos.z;
+    this->unk174.x = D_8086FA40[params].x + this->dyna.actor.world.pos.x;
+    this->unk174.y = D_8086FA40[params].y + this->dyna.actor.world.pos.y;
+    this->unk174.z = D_8086FA40[params].z + this->dyna.actor.world.pos.z;
     if (0) {}
     temp_v0_2 = (EnWallTubo*)Actor_SpawnAsChild(&globalCtx->actorCtx, &this->dyna.actor, globalCtx, ACTOR_EN_WALL_TUBO,
                                                 this->unk174.x, this->unk174.y, this->unk174.z, 0, 0, 0,
@@ -99,10 +99,10 @@ void func_8086F260(BgBowlWall* this, GlobalContext* globalCtx) {
     if (temp_v0_2 != NULL) {
         temp_v0_2->unk154 = this->unk174;
         if (params != 0) {
-            temp_v0_2->unk154 = this->unk174 = this->dyna.actor.posRot.pos;
+            temp_v0_2->unk154 = this->unk174 = this->dyna.actor.world.pos;
         }
         if (this->unk184 == NULL) {
-            var_v0_2 = globalCtx->actorCtx.actorList[4].first;
+            var_v0_2 = globalCtx->actorCtx.actorLists[4].head;
             while (var_v0_2 != NULL) {
                 if (var_v0_2->id != ACTOR_EN_BOM_BOWL_MAN) {
                     var_v0_2 = var_v0_2->next;
@@ -133,13 +133,13 @@ void func_8086F464(BgBowlWall* this, GlobalContext* globalCtx) {
     var_s0 = false;
     if (this->dyna.actor.params == 0) {
         Math_SmoothStepToS(&this->dyna.actor.shape.rot.x, -0x3E80, 3, 0x1F4, 0);
-        this->dyna.actor.posRot.rot.x = this->dyna.actor.shape.rot.x;
+        this->dyna.actor.world.rot.x = this->dyna.actor.shape.rot.x;
         if (this->dyna.actor.shape.rot.x < -0x3C1E) {
             var_s0 = true;
         }
     } else {
-        Math_ApproachF(&this->dyna.actor.posRot.pos.y, this->unk168.y - 450.0f, 0.3f, 10.0f);
-        if (this->dyna.actor.posRot.pos.y < (this->unk168.y - 400.0f)) {
+        Math_ApproachF(&this->dyna.actor.world.pos.y, this->unk168.y - 450.0f, 0.3f, 10.0f);
+        if (this->dyna.actor.world.pos.y < (this->unk168.y - 400.0f)) {
             var_s0 = true;
         }
     }
@@ -167,11 +167,11 @@ void func_8086F718(BgBowlWall* this, GlobalContext* globalCtx) {
         if (this->dyna.actor.params == 0) {
             Math_SmoothStepToS(&this->dyna.actor.shape.rot.x, -0x3E80, 1, 0xC8, 0);
         } else {
-            Math_ApproachF(&this->dyna.actor.posRot.pos.y, this->unk168.y - 450.0f, 0.3f, 10.0f);
+            Math_ApproachF(&this->dyna.actor.world.pos.y, this->unk168.y - 450.0f, 0.3f, 10.0f);
         }
     } else if (this->unk182 == 1) {
-        this->dyna.actor.posRot.pos.y = this->unk168.y - 450.0f;
-        this->dyna.actor.posRot.rot.x = this->dyna.actor.shape.rot.x = 0;
+        this->dyna.actor.world.pos.y = this->unk168.y - 450.0f;
+        this->dyna.actor.world.rot.x = this->dyna.actor.shape.rot.x = 0;
         this->unk184->unk23E_arr[this->dyna.actor.params] = 2;
         this->actionFunc = func_8086F7F8;
     }
@@ -179,9 +179,9 @@ void func_8086F718(BgBowlWall* this, GlobalContext* globalCtx) {
 
 void func_8086F7F8(BgBowlWall* this, GlobalContext* globalCtx) {
     if (this->unk184->unk23E_arr[this->dyna.actor.params] != 2) {
-        Math_ApproachF(&this->dyna.actor.posRot.pos.y, this->unk168.y, 0.3f, 50.0f);
-        if (fabsf(this->dyna.actor.posRot.pos.y - this->unk168.y) <= 10.0f) {
-            this->dyna.actor.posRot.pos.y = this->unk168.y;
+        Math_ApproachF(&this->dyna.actor.world.pos.y, this->unk168.y, 0.3f, 50.0f);
+        if (fabsf(this->dyna.actor.world.pos.y - this->unk168.y) <= 10.0f) {
+            this->dyna.actor.world.pos.y = this->unk168.y;
             this->unk180 = 0;
             this->actionFunc = func_8086F260;
         }

src/overlays/actors/ovl_En_Dns/z_en_dns.c
@@ -64,7 +64,7 @@ extern AnimationHeader D_6004404;
 
 const ActorInit En_Dns_InitVars = {
     ACTOR_EN_DNS,
-    ACTORTYPE_BG,
+    ACTORCAT_BG,
     FLAGS,
     OBJECT_SHOPNUTS,
     sizeof(EnDns),
@@ -152,8 +152,8 @@ static EnDnsPurchaseInfo* sEnDnsPurchaseInfos[] = {
 };
 static InitChainEntry D_809F052C[3] = {
     ICHAIN_S8(naviEnemyId, 78, ICHAIN_CONTINUE),
-    ICHAIN_U8(unk_1F, 2, ICHAIN_CONTINUE),
-    ICHAIN_F32(unk_4C, 30, ICHAIN_STOP),
+    ICHAIN_U8(targetMode, 2, ICHAIN_CONTINUE),
+    ICHAIN_F32(targetArrowOffset, 30, ICHAIN_STOP),
 };
 static struct _struct_D_809F0538_0xC D_809F0538[3] = {
     { 0x06001108, 0, 0.0f },
@@ -177,7 +177,7 @@ void EnDns_Init(Actor* thisx, GlobalContext* globalCtx) {
     SkelAnime_InitFlex(globalCtx, &this->unk14C, &D_60041A8, &D_60009A0, this->unk190, this->unk1FC, 0x12);
     Collider_InitCylinder(globalCtx, &this->unk26C);
     Collider_SetCylinderType1(globalCtx, &this->unk26C, &this->actor, &D_809F03E0);
-    ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawFunc_Circle, 35.0f);
+    ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawCircle, 35.0f);
     this->actor.textId = D_809F040C[this->actor.params];
     Actor_SetScale(&this->actor, 0.01f);
     this->actor.colChkInfo.mass = 0xFF;
@@ -340,17 +340,17 @@ void func_809EFB84(EnDns* this, GlobalContext* globalCtx) {
 }
 
 void func_809EFBC8(EnDns* this, GlobalContext* globalCtx) {
-    Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsLink, 3, 0x7D0, 0);
-    this->actor.posRot.rot.y = this->actor.shape.rot.y;
+    Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsPlayer, 3, 0x7D0, 0);
+    this->actor.world.rot.y = this->actor.shape.rot.y;
     if (func_8002F194(&this->actor, globalCtx) != 0) {
         this->unk268 = func_809EFC9C;
     } else {
-        if ((this->unk26C.base.ocFlags1 & OC1_HIT) || (this->actor.unk_10C != 0)) {
+        if ((this->unk26C.base.ocFlags1 & OC1_HIT) || (this->actor.isTargeted != 0)) {
             this->actor.flags |= 0x10000;
         } else {
             this->actor.flags &= ~0x10000;
         }
-        if (this->actor.xzDistToLink < 130.0f) {
+        if (this->actor.xzDistToPlayer < 130.0f) {
             func_8002F2F4(&this->actor, globalCtx);
         }
     }
@@ -461,7 +461,7 @@ void func_809F0100(EnDns* this, GlobalContext* globalCtx) {
         Audio_PlayActorSound2(&this->actor, NA_SE_EN_AKINDONUTS_HIDE);
         this->unk268 = func_809F017C;
         this->unk2BC = 0;
-        this->unk2C4 = this->actor.posRot.pos.y;
+        this->unk2C4 = this->actor.world.pos.y;
     }
 }
 
@@ -470,19 +470,19 @@ void func_809F017C(EnDns* this, GlobalContext* globalCtx) {
     Vec3f sp38;
     s32 var_s0;
 
-    var_fv0 = this->unk2C4 - this->actor.posRot.pos.y;
+    var_fv0 = this->unk2C4 - this->actor.world.pos.y;
     if ((this->unk2B8 % 4) == 0) {
-        sp38.x = this->actor.posRot.pos.x;
+        sp38.x = this->actor.world.pos.x;
         sp38.y = this->unk2C4;
-        sp38.z = this->actor.posRot.pos.z;
+        sp38.z = this->actor.world.pos.z;
         func_80028990(globalCtx, 20.0f, &sp38);
     }
     this->actor.shape.rot.y += 0x2000;
     if (var_fv0 > 400.0f) {
         if (this->unk2BD != 0) {
-            sp38.x = this->actor.posRot.pos.x;
+            sp38.x = this->actor.world.pos.x;
             sp38.y = this->unk2C4;
-            sp38.z = this->actor.posRot.pos.z;
+            sp38.z = this->actor.world.pos.z;
             for (var_s0 = 0; var_s0 < 3; var_s0++) {
                 Item_DropCollectible(globalCtx, &sp38, 3);
             }
@@ -497,13 +497,13 @@ void EnDns_Update(Actor* thisx, GlobalContext* globalCtx) {
 
     this->unk2B8++;
     this->actor.textId = D_809F040C[this->actor.params];
-    Actor_SetHeight(&this->actor, 60.0f);
+    Actor_SetFocus(&this->actor, 60.0f);
     Actor_SetScale(&this->actor, 0.01f);
     SkelAnime_Update(&this->unk14C);
     Actor_MoveForward(&this->actor);
     this->unk268(this, globalCtx);
     if (this->unk2BC != 0) {
-        func_8002E4B4(globalCtx, &this->actor, 20.0f, 20.0f, 20.0f, 4);
+        Actor_UpdateBgCheckInfo(globalCtx, &this->actor, 20.0f, 20.0f, 20.0f, 4);
     }
     if (this->unk2BB != 0) {
         Collider_UpdateCylinder(&this->actor, &this->unk26C);