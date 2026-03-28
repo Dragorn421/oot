HackerN64/HackerOoT

Bombchu OK (#647)
* all but 1

* done

* asm

* rename collider data

* final changes and format

* matched, ty petrie

* asm

* review

src/overlays/actors/ovl_En_Bom_Bowl_Pit/z_en_bom_bowl_pit.c
@@ -70,7 +70,7 @@ void EnBomBowlPit_DetectHit(EnBomBowlPit* this, GlobalContext* globalCtx) {
             if (((fabsf(chuPosDiff.x) < 40.0f) || (BREG(2))) && ((fabsf(chuPosDiff.y) < 40.0f) || (BREG(2))) &&
                 ((fabsf(chuPosDiff.z) < 40.0f) || (BREG(2)))) {
                 func_8002DF54(globalCtx, NULL, 8);
-                chu->unk_150 = 1;
+                chu->timer = 1;
 
                 this->camId = Gameplay_CreateSubCamera(globalCtx);
                 Gameplay_ChangeCameraStatus(globalCtx, 0, 1);

Dragorn421/oot

src/overlays/actors/ovl_En_Bom_Bowl_Pit/z_en_bom_bowl_pit.c
@@ -72,7 +72,7 @@ void func_809C4E8C(EnBomBowlPit* this, GlobalContext* globalCtx) {
                 ((fabsf(diff.z) < 40.0f) || (gGameInfo->data[0x962] != 0))) {
                 sp24 = (EnBomChu*)var_v1;
                 func_8002DF54(globalCtx, NULL, 8U);
-                sp24->unk150 = 1;
+                sp24->timer = 1;
                 this->unk152 = Gameplay_CreateSubCamera(globalCtx);
                 Gameplay_ChangeCameraStatus(globalCtx, 0, 1);
                 Gameplay_ChangeCameraStatus(globalCtx, this->unk152, 7);