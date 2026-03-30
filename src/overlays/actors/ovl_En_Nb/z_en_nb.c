HackerN64/HackerOoT

EnGoma OK (#770)
* En_Goma OK

* Label Goma

* Remove asm

* Labeling

* Review changes

* Forgot a comment

* sizeof

* Review comments and fix warnings

* review

Co-authored-by: Fig02 <fig02srl@gmail.com>

src/overlays/actors/ovl_En_Nb/z_en_nb.c
@@ -1424,7 +1424,8 @@ void EnNb_Init(Actor* thisx, GlobalContext* globalCtx) {
 
     ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawCircle, 30.0f);
     EnNb_SetupCollider(thisx, globalCtx);
-    SkelAnime_InitFlex(globalCtx, &this->skelAnime, &gNabooruSkel, NULL, this->jointTable, this->morphTable, NB_LIMB_MAX);
+    SkelAnime_InitFlex(globalCtx, &this->skelAnime, &gNabooruSkel, NULL, this->jointTable, this->morphTable,
+                       NB_LIMB_MAX);
 
     switch (EnNb_GetType(this)) {
         case NB_TYPE_DEMO02:

Dragorn421/oot

src/overlays/actors/ovl_En_Nb/z_en_nb.c
@@ -1446,7 +1446,8 @@ void EnNb_Init(Actor* thisx, GlobalContext* globalCtx) {
 
     ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawCircle, 30.0f);
     EnNb_SetupCollider(thisx, globalCtx);
-    SkelAnime_InitFlex(globalCtx, &this->skelAnime, &object_nb_0181C8_Skel, NULL, this->jointTable, this->morphTable, 19);
+    SkelAnime_InitFlex(globalCtx, &this->skelAnime, &object_nb_0181C8_Skel, NULL, this->jointTable, this->morphTable,
+                       19);
 
     switch (EnNb_GetType(this)) {
         case NB_TYPE_DEMO02: