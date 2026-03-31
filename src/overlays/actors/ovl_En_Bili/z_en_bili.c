HackerN64/HackerOoT

Some actor.c docs (in preparation for en_test) (#820)
* renames

* format

* format functions.h

* clean

* arg name

* remove asm

src/overlays/actors/ovl_En_Bili/z_en_bili.c
@@ -551,12 +551,12 @@ void EnBili_UpdateDamage(EnBili* this, GlobalContext* globalCtx) {
 
     if ((this->actor.colChkInfo.health != 0) && (this->collider.base.acFlags & AC_HIT)) {
         this->collider.base.acFlags &= ~AC_HIT;
-        func_80035650(&this->actor, &this->collider.info, 1);
-
+        Actor_SetDropFlag(&this->actor, &this->collider.info, 1);
+        
         if ((this->actor.colChkInfo.damageEffect != 0) || (this->actor.colChkInfo.damage != 0)) {
             if (Actor_ApplyDamage(&this->actor) == 0) {
                 Audio_PlayActorSound2(&this->actor, NA_SE_EN_BIRI_DEAD);
-                func_80032C7C(globalCtx, &this->actor);
+                Enemy_StartFinishingBlow(globalCtx, &this->actor);
                 this->actor.flags &= ~1;
             }
 

src/overlays/actors/ovl_En_Tp/z_en_tp.c
@@ -587,7 +587,7 @@ void EnTp_UpdateDamage(EnTp* this, GlobalContext* globalCtx) {
         }
 
         this->collider.base.acFlags &= ~AC_HIT;
-        func_8003573C(&this->actor, &this->collider, 1);
+        Actor_SetDropFlagJntSph(&this->actor, &this->collider, 1);
         this->damageEffect = this->actor.colChkInfo.damageEffect;
 
         if (this->actor.colChkInfo.damageEffect != TAILPASARAN_DMGEFF_NONE) {

Dragorn421/oot

src/overlays/actors/ovl_En_Bili/z_en_bili.c
@@ -494,11 +494,11 @@ void func_809C09E0(EnBili* this, GlobalContext* globalCtx) {
 void func_809C0A70(EnBili* this, GlobalContext* globalCtx) {
     if ((this->actor.colChkInfo.health != 0) && (this->unk1D4.base.acFlags & AC_HIT)) {
         this->unk1D4.base.acFlags &= ~AC_HIT;
-        func_80035650(&this->actor, &this->unk1D4.info, 1);
+        Actor_SetDropFlag(&this->actor, &this->unk1D4.info, 1);
         if ((((this->actor.colChkInfo.damageEffect != 0)) || (this->actor.colChkInfo.damage != 0))) {
             if (Actor_ApplyDamage(&this->actor) == 0) {
                 Audio_PlayActorSound2(&this->actor, NA_SE_EN_BIRI_DEAD);
-                func_80032C7C(globalCtx, &this->actor);
+                Enemy_StartFinishingBlow(globalCtx, &this->actor);
                 this->actor.flags &= ~1;
             }
             if (this->actor.colChkInfo.damageEffect == 1) {

src/overlays/actors/ovl_En_Tp/z_en_tp.c
@@ -488,7 +488,7 @@ void func_80B221E8(EnTp* this, GlobalContext* globalCtx) {
             var_s2 = 1;
         }
         this->unk174.base.acFlags &= ~2;
-        func_8003573C(&this->actor, &this->unk174, 1);
+        Actor_SetDropFlagJntSph(&this->actor, &this->unk174, 1);
         this->unk158 = this->actor.colChkInfo.damageEffect;
         if (this->actor.colChkInfo.damageEffect != 0) {
             if (this->actor.colChkInfo.damageEffect == 1) {