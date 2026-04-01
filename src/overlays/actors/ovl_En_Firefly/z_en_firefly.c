HackerN64/HackerOoT

Rename MtxF members to use the common convention (#923)
* Rename MtxF members like xy -> now_21

* Revert comments I didn't mean to commit

* Rename c*/r* temps in `SkinMatrix_MtxFMtxFMult`

* Rename MtxF members like now_21 -> yx

src/overlays/actors/ovl_En_Firefly/z_en_firefly.c
@@ -756,9 +756,9 @@ void EnFirefly_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList
             if ((limbIndex == 15) || (limbIndex == 21)) {
                 if (this->actionFunc != EnFirefly_Die) {
                     Matrix_Get(&mtx);
-                    effPos.x = (Rand_ZeroOne() * 5.0f) + mtx.wx;
-                    effPos.y = (Rand_ZeroOne() * 5.0f) + mtx.wy;
-                    effPos.z = (Rand_ZeroOne() * 5.0f) + mtx.wz;
+                    effPos.x = (Rand_ZeroOne() * 5.0f) + mtx.xw;
+                    effPos.y = (Rand_ZeroOne() * 5.0f) + mtx.yw;
+                    effPos.z = (Rand_ZeroOne() * 5.0f) + mtx.zw;
                     effScaleStep = -40;
                     effLife = 3;
                 } else {

Dragorn421/oot

src/overlays/actors/ovl_En_Firefly/z_en_firefly.c
@@ -664,9 +664,9 @@ void EnFirefly_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList
         if (((this->unk1B8 == 1) || (this->unk1B8 == 2)) && ((limbIndex == 0xF) || (limbIndex == 0x15))) {
             if (this->actionFunc != func_80A13D68_Action) {
                 Matrix_Get(&sp48);
-                sp94.x = (Rand_ZeroOne() * 5.0f) + sp48.wx;
-                sp94.y = (Rand_ZeroOne() * 5.0f) + sp48.wy;
-                sp94.z = (Rand_ZeroOne() * 5.0f) + sp48.wz;
+                sp94.x = (Rand_ZeroOne() * 5.0f) + sp48.xw;
+                sp94.y = (Rand_ZeroOne() * 5.0f) + sp48.yw;
+                sp94.z = (Rand_ZeroOne() * 5.0f) + sp48.zw;
                 var_t0 = -0x28;
                 var_t1 = 3;
             } else {