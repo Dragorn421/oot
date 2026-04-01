HackerN64/HackerOoT

Decompile z_kankyo (#956)
* working

* start color switch

* progress

* progress

* progress on bgm func

* progress

* game over matched (except the rodata meme)

* start update

* progress

* lightning docs done

* progress

* progress

* progress

* progress

* progress

* can compile at least

* suns state, progress on kankyo_update

* some new names

* progress

* progress

* progress

* new functions

* cleanup

* more matches

* another match

* now functional

* format

* better match

* hugely improved update

* cleanup/review

* remove old changes

* review2

* review3

* missed one

* review4

* change asm filenames

* update doorwarp1

* review5

* Kankyo_ -> Environment_

* format

* merge master and format functions.h

Co-authored-by: Jacob Young <jacobly0@users.noreply.github.com>
Co-authored-by: fig <fig02srl@gmail.com>
Co-authored-by: petrie911 <pmontag@DESKTOP-LG8A167.localdomain>

src/overlays/actors/ovl_En_Fish/z_en_fish.c
@@ -665,7 +665,7 @@ void EnFish_UpdateCutscene(EnFish* this, GlobalContext* globalCtx) {
     endPos.y = csAction->endPos.y;
     endPos.z = csAction->endPos.z;
 
-    progress = func_8006F93C(csAction->endFrame, csAction->startFrame, globalCtx->csCtx.frames);
+    progress = Environment_LerpWeight(csAction->endFrame, csAction->startFrame, globalCtx->csCtx.frames);
 
     this->actor.world.pos.x = (endPos.x - startPos.x) * progress + startPos.x;
     this->actor.world.pos.y = (endPos.y - startPos.y) * progress + startPos.y + D_80A17014;

src/overlays/actors/ovl_En_Hata/z_en_hata.c
@@ -88,19 +88,18 @@ void EnHata_Update(Actor* thisx, GlobalContext* globalCtx2) {
     f32 sin;
 
     SkelAnime_Update(&this->skelAnime);
-
     // Rotate to hang down by default
     this->limbs[FLAGPOLE_LIMB_FLAG_1_BASE].y = this->limbs[FLAGPOLE_LIMB_FLAG_2_BASE].y = -0x4000;
-    windVec.x = globalCtx->envCtx.unk_A8;
-    windVec.y = globalCtx->envCtx.unk_AA;
-    windVec.z = globalCtx->envCtx.unk_AC;
+    windVec.x = globalCtx->envCtx.windDirection.x;
+    windVec.y = globalCtx->envCtx.windDirection.y;
+    windVec.z = globalCtx->envCtx.windDirection.z;
 
-    if (globalCtx->envCtx.unk_B0 > 255.0f) {
-        globalCtx->envCtx.unk_B0 = 255.0f;
+    if (globalCtx->envCtx.windSpeed > 255.0f) {
+        globalCtx->envCtx.windSpeed = 255.0f;
     }
 
-    if (globalCtx->envCtx.unk_B0 < 0.0f) {
-        globalCtx->envCtx.unk_B0 = 0.0f;
+    if (globalCtx->envCtx.windSpeed < 0.0f) {
+        globalCtx->envCtx.windSpeed = 0.0f;
     }
 
     if (Rand_ZeroOne() > 0.5f) {
@@ -112,13 +111,13 @@ void EnHata_Update(Actor* thisx, GlobalContext* globalCtx2) {
     // Mimic varying wind gusts
     sin = Math_SinS(this->unk_278) * 80.0f;
     pitch = -Math_Vec3f_Pitch(&zeroVec, &windVec);
-    pitch = ((s32)((15000 - pitch) * (1.0f - (globalCtx->envCtx.unk_B0 / (255.0f - sin))))) + pitch;
+    pitch = ((s32)((15000 - pitch) * (1.0f - (globalCtx->envCtx.windSpeed / (255.0f - sin))))) + pitch;
     Math_SmoothStepToS(&this->limbs[FLAGPOLE_LIMB_FLAG_1_HOIST_END_BASE].y, pitch, this->invScale, this->maxStep,
                        this->minStep);
     this->limbs[FLAGPOLE_LIMB_FLAG_2_HOIST_END_BASE].y = this->limbs[FLAGPOLE_LIMB_FLAG_1_HOIST_END_BASE].y;
     this->limbs[FLAGPOLE_LIMB_FLAG_1_HOIST_END_BASE].z = -Math_Vec3f_Yaw(&zeroVec, &windVec);
     this->limbs[FLAGPOLE_LIMB_FLAG_2_HOIST_END_BASE].z = this->limbs[FLAGPOLE_LIMB_FLAG_1_HOIST_END_BASE].z;
-    this->skelAnime.playSpeed = (Rand_ZeroFloat(1.25f) + 2.75f) * (globalCtx->envCtx.unk_B0 / 255.0f);
+    this->skelAnime.playSpeed = (Rand_ZeroFloat(1.25f) + 2.75f) * (globalCtx->envCtx.windSpeed / 255.0f);
 }
 
 s32 EnHata_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, void* thisx) {

Dragorn421/oot

src/overlays/actors/ovl_En_Fish/z_en_fish.c
@@ -626,7 +626,7 @@ void func_80A16A64(EnFish* this, GlobalContext* globalCtx) {
     sp34.x = sp4C->endPos.x;
     sp34.y = sp4C->endPos.y;
     sp34.z = sp4C->endPos.z;
-    temp_fv0 = func_8006F93C(sp4C->endFrame, sp4C->startFrame, globalCtx->csCtx.frames);
+    temp_fv0 = Environment_LerpWeight(sp4C->endFrame, sp4C->startFrame, globalCtx->csCtx.frames);
     this->actor.world.pos.x = ((sp34.x - sp40.x) * temp_fv0) + sp40.x;
     this->actor.world.pos.y = ((sp34.y - sp40.y) * temp_fv0) + sp40.y + D_80A17014;
     this->actor.world.pos.z = ((sp34.z - sp40.z) * temp_fv0) + sp40.z;

src/overlays/actors/ovl_En_Hata/z_en_hata.c
@@ -74,14 +74,14 @@ void EnHata_Update(Actor* thisx, GlobalContext* globalCtx2) {
 
     SkelAnime_Update(&this->skelAnime);
     this->limbs[3].y = this->limbs[12].y = -0x4000;
-    sp3C.x = globalCtx->envCtx.unk_A8;
-    sp3C.y = globalCtx->envCtx.unk_AA;
-    sp3C.z = globalCtx->envCtx.unk_AC;
-    if (globalCtx->envCtx.unk_B0 > 255.0f) {
-        globalCtx->envCtx.unk_B0 = 255.0f;
+    sp3C.x = globalCtx->envCtx.windDirection.x;
+    sp3C.y = globalCtx->envCtx.windDirection.y;
+    sp3C.z = globalCtx->envCtx.windDirection.z;
+    if (globalCtx->envCtx.windSpeed > 255.0f) {
+        globalCtx->envCtx.windSpeed = 255.0f;
     }
-    if (globalCtx->envCtx.unk_B0 < 0.0f) {
-        globalCtx->envCtx.unk_B0 = 0.0f;
+    if (globalCtx->envCtx.windSpeed < 0.0f) {
+        globalCtx->envCtx.windSpeed = 0.0f;
     }
     if (Rand_ZeroOne() > 0.5f) {
         this->unk_278 += 6000;
@@ -90,12 +90,12 @@ void EnHata_Update(Actor* thisx, GlobalContext* globalCtx2) {
     }
     sin = Math_SinS(this->unk_278) * 80.0f;
     pitch = -Math_Vec3f_Pitch(&sp48, &sp3C);
-    pitch = ((s32)((0x3A98 - pitch) * (1.0f - (globalCtx->envCtx.unk_B0 / (255.0f - sin))))) + pitch;
+    pitch = ((s32)((0x3A98 - pitch) * (1.0f - (globalCtx->envCtx.windSpeed / (255.0f - sin))))) + pitch;
     Math_SmoothStepToS(&this->limbs[4].y, pitch, this->invScale, this->maxStep, this->minStep);
     this->limbs[13].y = this->limbs[4].y;
     this->limbs[4].z = -Math_Vec3f_Yaw(&sp48, &sp3C);
     this->limbs[13].z = this->limbs[4].z;
-    this->skelAnime.playSpeed = (Rand_ZeroFloat(1.25f) + 2.75f) * (globalCtx->envCtx.unk_B0 / 255.0f);
+    this->skelAnime.playSpeed = (Rand_ZeroFloat(1.25f) + 2.75f) * (globalCtx->envCtx.windSpeed / 255.0f);
 }
 
 s32 EnHata_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, void* thisx) {