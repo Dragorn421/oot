HackerN64/HackerOoT

Document EnGoroiwa and BgJyaGoroiwa (#860)
* Document EnGoroiwa and BgJyaGoroiwa

* Run format.sh

* Add renamed functions to actorfixer script

* no more calling out "the devs" for bad code

* `EnGoroiwa#flags` -> `stateFlags`

* `Matrix_MtxFToRotS` -> `Matrix_MtxFToYXZRotS` (and not ZXY oops), `func_800D2264` -> `Matrix_MtxFToZYXRotS`

src/overlays/actors/ovl_Bg_Haka_Huta/z_bg_haka_huta.c
@@ -194,13 +194,13 @@ void func_8087D720(BgHakaHuta* this, GlobalContext* globalCtx) {
         D_8087D958.x = 30.0f;
     }
     Matrix_RotateY(this->dyna.actor.world.rot.y * (M_PI / 0x8000), 0);
-    func_800D23FC(this->counter * (191 * M_PI / 3750), &D_8087D964, 1);
+    Matrix_RotateAxis(this->counter * (191 * M_PI / 3750), &D_8087D964, 1);
     Matrix_MultVec3f(&D_8087D958, &vec);
     this->dyna.actor.world.pos.x = this->dyna.actor.home.pos.x + vec.x;
     this->dyna.actor.world.pos.y = this->dyna.actor.home.pos.y + vec.y;
     this->dyna.actor.world.pos.z = this->dyna.actor.home.pos.z + vec.z;
     Matrix_Get(&mtx);
-    func_800D20CC(&mtx, &this->dyna.actor.shape.rot, 0);
+    Matrix_MtxFToYXZRotS(&mtx, &this->dyna.actor.shape.rot, 0);
 }
 
 void BgHakaHuta_DoNothing(BgHakaHuta* this, GlobalContext* globalCtx) {

src/overlays/actors/ovl_Bg_Spot08_Iceblock/z_bg_spot08_iceblock.c
@@ -246,10 +246,11 @@ void BgSpot08Iceblock_Roll(BgSpot08Iceblock* this, GlobalContext* globalCtx) {
     }
 
     // Rotation by the angle between surfaceNormal and the vertical about rotationAxis
-    func_800D23FC(Math_FAcosF(Math3D_Cos(&sVerticalVector, &this->surfaceNormal)), &this->rotationAxis, MTXMODE_NEW);
+    Matrix_RotateAxis(Math_FAcosF(Math3D_Cos(&sVerticalVector, &this->surfaceNormal)), &this->rotationAxis,
+                      MTXMODE_NEW);
     Matrix_RotateY(this->dyna.actor.shape.rot.y * (M_PI / 0x8000), MTXMODE_APPLY);
     Matrix_Get(&mtx);
-    func_800D20CC(&mtx, &this->dyna.actor.shape.rot, MTXMODE_NEW);
+    Matrix_MtxFToYXZRotS(&mtx, &this->dyna.actor.shape.rot, 0);
 }
 
 void BgSpot08Iceblock_SpawnTwinFloe(BgSpot08Iceblock* this, GlobalContext* globalCtx) {

Dragorn421/oot

src/overlays/actors/ovl_Bg_Haka_Huta/z_bg_haka_huta.c
@@ -203,13 +203,13 @@ void func_8087D720(BgHakaHuta* this, GlobalContext* globalCtx) {
         D_8087D958.x = 30.0f;
     }
     Matrix_RotateY(this->dyna.actor.world.rot.y * 0.0000958738f, MTXMODE_NEW);
-    func_800D23FC(this->unk168 * 0.16001178f, &D_8087D964, MTXMODE_APPLY);
+    Matrix_RotateAxis(this->unk168 * 0.16001178f, &D_8087D964, MTXMODE_APPLY);
     Matrix_MultVec3f(&D_8087D958, &sp34);
     this->dyna.actor.world.pos.x = this->dyna.actor.home.pos.x + sp34.x;
     this->dyna.actor.world.pos.y = this->dyna.actor.home.pos.y + sp34.y;
     this->dyna.actor.world.pos.z = this->dyna.actor.home.pos.z + sp34.z;
     Matrix_Get(&sp40);
-    func_800D20CC(&sp40, &this->dyna.actor.shape.rot, 0);
+    Matrix_MtxFToYXZRotS(&sp40, &this->dyna.actor.shape.rot, 0);
 }
 
 void BgHakaHuta_DoNothing(BgHakaHuta* this, GlobalContext* globalCtx) {

src/overlays/actors/ovl_Bg_Spot08_Iceblock/z_bg_spot08_iceblock.c
@@ -217,10 +217,10 @@ void func_808B0CE0(BgSpot08Iceblock* this, GlobalContext* globalCtx) {
     } else {
         this->unk168 = D_808B16B0;
     }
-    func_800D23FC(Math_FAcosF(Math3D_Cos(&D_808B16B0, &this->unk168)), &this->unk180, MTXMODE_NEW);
+    Matrix_RotateAxis(Math_FAcosF(Math3D_Cos(&D_808B16B0, &this->unk168)), &this->unk180, MTXMODE_NEW);
     Matrix_RotateY(this->dyna.actor.shape.rot.y * 0.0000958738f, MTXMODE_APPLY);
     Matrix_Get(&sp44);
-    func_800D20CC(&sp44, &this->dyna.actor.shape.rot, 0);
+    Matrix_MtxFToYXZRotS(&sp44, &this->dyna.actor.shape.rot, 0);
 }
 
 void func_808B1054(BgSpot08Iceblock* this, GlobalContext* globalCtx) {