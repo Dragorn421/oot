HackerN64/HackerOoT

Various minor cleanups (#940)
* Fix includes using braces instead of quotes

* Remove some unused printf arguments

* Fix a condition in Door_Shutter

* Run the formatter

* Cleanup incorrect uses of DynaPolyMoveFlag as function arguments

* Cleanup combined Math_StepToF checks

src/overlays/actors/ovl_Bg_Spot12_Gate/z_bg_spot12_gate.c
@@ -44,8 +44,7 @@ static InitChainEntry sInitChain[] = {
     ICHAIN_F32(uncullZoneDownward, 1200, ICHAIN_STOP),
 };
 
-void BgSpot12Gate_InitDynaPoly(BgSpot12Gate* this, GlobalContext* globalCtx, CollisionHeader* collision,
-                               DynaPolyMoveFlag flags) {
+void BgSpot12Gate_InitDynaPoly(BgSpot12Gate* this, GlobalContext* globalCtx, CollisionHeader* collision, s32 flags) {
     s32 pad;
     CollisionHeader* colHeader = NULL;
     s32 pad2;

src/overlays/actors/ovl_En_Kusa/z_en_kusa.c
@@ -463,9 +463,9 @@ void func_80A9C00C(EnKusa* this) {
 }
 
 void func_80A9C068(EnKusa* this, GlobalContext* globalCtx) {
-    s32 sp24;
+    s32 sp24 = true;
 
-    sp24 = Math_StepToF(&this->actor.scale.y, 0.4f, 0.014f) & 1;
+    sp24 &= Math_StepToF(&this->actor.scale.y, 0.4f, 0.014f);
     sp24 &= Math_StepToF(&this->actor.scale.x, 0.4f, 0.011f);
     this->actor.scale.z = this->actor.scale.x;
 

Dragorn421/oot

src/overlays/actors/ovl_Bg_Spot12_Gate/z_bg_spot12_gate.c
@@ -44,7 +44,7 @@ static InitChainEntry sInitChain[] = {
     ICHAIN_F32(uncullZoneDownward, 1200, ICHAIN_STOP),
 };
 
-void func_808B2F90(BgSpot12Gate* this, GlobalContext* globalCtx, CollisionHeader* collision, DynaPolyMoveFlag flags) {
+void func_808B2F90(BgSpot12Gate* this, GlobalContext* globalCtx, CollisionHeader* collision, s32 flags) {
     Actor* thisx = &this->dyna.actor;
     CollisionHeader* colHeader = NULL;
     s32 pad[2];

src/overlays/actors/ovl_En_Kusa/z_en_kusa.c
@@ -413,7 +413,7 @@ void func_80A9C00C(EnKusa* this) {
 }
 
 void func_80A9C068(EnKusa* this, GlobalContext* globalCtx) {
-    s32 temp_v1 = 1;
+    s32 temp_v1 = true;
 
     temp_v1 &= Math_StepToF(&this->actor.scale.y, 0.4f, 0.014f);
     temp_v1 &= Math_StepToF(&this->actor.scale.x, 0.4f, 0.011f);