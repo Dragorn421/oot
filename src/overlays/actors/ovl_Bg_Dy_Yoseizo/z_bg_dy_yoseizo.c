HackerN64/HackerOoT

z_message_PAL, message_data_static and surrounding doc (#996)
* Initial progress on z_message_PAL, very messy

* Fix merge

* Some more progress

* Fix merge

* More z_message_PAL

* Small progress

* More small progress

* message_data_static files OK

* Prepare z_message_tables

* Matched another function, small updates

* Attempt to use asm-processor static-symbols branch

* Refactor text id declarations

* Begin large text codes parser function

* Fix merge

* Refactor done

* Build OK, add color and highscore names

* Remove encoded text headers and automatically encode during build

* Fix kanfont

* Various cleanups

* DISP macros

* Another match aside data

* Further progress

* Small improvements

* Deduplicate magic values for text control codes, small improvements

* Tiny progress

* Minor cleanups

* Clean up z_message_PAL comment

* Progress on large functions

* Further progress on large functions

* Changes to mkldscript to link .data in the .rodata section

* data OK

* Few improvements

* Use gDPLoadTextureBlock macros where appropriate

* rm z_message_tables, progress on large functions

* 2 more matches

* Improvements

* Small progress

* More progress on big function

* progress

* match func_80107980

* match Message_Update

* match func_8010BED8

* done

* Progress on remaining large functions

* Small progress on largest function

* Another match, extract text and move to assets, improve text build system

* Small nonmatchings improvements

* docs wip

* Largest function maybe equivalent

* Fix merge

* Document do_action values, largest function is almost instruction-matching

* Rename NAVI do_action to NONE, as that appears to be how that value is used in practice

* Fix merge

* one match

* Last function is instruction-matching

* Fix

* Improvements thanks to engineer124

* Stack matched thanks to petrie911, now just a/v/low 

src/overlays/actors/ovl_Bg_Dy_Yoseizo/z_bg_dy_yoseizo.c
@@ -182,7 +182,7 @@ void BgDyYoseizo_Bob(BgDyYoseizo* this, GlobalContext* globalCtx) {
 
 void BgDyYoseizo_CheckMagicAcquired(BgDyYoseizo* this, GlobalContext* globalCtx) {
     if (Flags_GetSwitch(globalCtx, 0x38)) {
-        globalCtx->msgCtx.unk_E3EE = 4;
+        globalCtx->msgCtx.ocarinaMode = OCARINA_MODE_04;
         if (globalCtx->sceneNum == SCENE_DAIYOUSEI_IZUMI) {
             if (!gSaveContext.magicAcquired && (this->fountainType != FAIRY_UPGRADE_MAGIC)) {
                 Actor_Kill(&this->actor);
@@ -204,7 +204,7 @@ void BgDyYoseizo_ChooseType(BgDyYoseizo* this, GlobalContext* globalCtx) {
 
     func_8002DF54(globalCtx, &this->actor, 1);
     // "Mode"
-    osSyncPrintf(VT_FGCOL(YELLOW) "☆☆☆☆☆ もうど ☆☆☆☆☆ %d\n" VT_RST, globalCtx->msgCtx.unk_E3EE);
+    osSyncPrintf(VT_FGCOL(YELLOW) "☆☆☆☆☆ もうど ☆☆☆☆☆ %d\n" VT_RST, globalCtx->msgCtx.ocarinaMode);
     givingReward = false;
 
     if (globalCtx->sceneNum != SCENE_DAIYOUSEI_IZUMI) {
@@ -376,8 +376,8 @@ void BgDyYoseizo_SetupGreetPlayer_NoReward(BgDyYoseizo* this, GlobalContext* glo
     }
 
     this->actor.textId = 0xDB;
-    this->dialogState = 5;
-    func_8010B680(globalCtx, this->actor.textId, NULL);
+    this->dialogState = TEXT_STATE_EVENT;
+    Message_StartTextbox(globalCtx, this->actor.textId, NULL);
     BgDyYoseizo_SpawnParticles(this, globalCtx, 0);
     this->actionFunc = BgDyYoseizo_GreetPlayer_NoReward;
 }
@@ -392,8 +392,8 @@ void BgDyYoseizo_GreetPlayer_NoReward(BgDyYoseizo* this, GlobalContext* globalCt
 
     SkelAnime_Update(&this->skelAnime);
 
-    if ((this->dialogState == func_8010BDBC(&globalCtx->msgCtx)) && (func_80106BC8(globalCtx) != 0)) {
-        func_80106CCC(globalCtx);
+    if ((this->dialogState == Message_GetState(&globalCtx->msgCtx)) && Message_ShouldAdvance(globalCtx)) {
+        Message_CloseTextbox(globalCtx);
         Interface_ChangeAlpha(5);
         this->actionFunc = BgDyYoseizo_SetupHealPlayer_NoReward;
     }
@@ -483,8 +483,8 @@ void BgDyYoseizo_HealPlayer_NoReward(BgDyYoseizo* this, GlobalContext* globalCtx
 
     if (this->healingTimer == 1) {
         this->actor.textId = 0xDA;
-        this->dialogState = 5;
-        func_8010B720(globalCtx, this->actor.textId);
+        this->dialogState = TEXT_STATE_EVENT;
+        Message_ContinueTextbox(globalCtx, this->actor.textId);
         this->actionFunc = BgDyYoseizo_SayFarewell_NoReward;
         return;
     }
@@ -500,8 +500,8 @@ void BgDyYoseizo_SayFarewell_NoReward(BgDyYoseizo* this, GlobalContext* globalCt
 
     SkelAnime_Update(&this->skelAnime);
 
-    if ((this->dialogState == func_8010BDBC(&globalCtx->msgCtx)) && (func_80106BC8(globalCtx) != 0)) {
-        func_80106CCC(globalCtx);
+    if ((this->dialogState == Message_GetState(&globalCtx->msgCtx)) && Message_ShouldAdvance(globalCtx)) {
+        Message_CloseTextbox(globalCtx);
         this->mouthState = 0;
         this->actionFunc = BgDyYoseizo_SetupSpinShrink;
         func_8005B1A4(GET_ACTIVE_CAM(globalCtx));

src/overlays/actors/ovl_Obj_Timeblock/z_obj_timeblock.c
@@ -171,7 +171,7 @@ s32 ObjTimeblock_WaitForOcarina(ObjTimeblock* this, GlobalContext* globalCtx) {
 
     if (ObjTimeblock_PlayerIsInRange(this, globalCtx)) {
         if (player->stateFlags2 & 0x1000000) {
-            func_8010BD58(globalCtx, 1);
+            func_8010BD58(globalCtx, OCARINA_ACTION_FREE_PLAY);
             this->songObserverFunc = ObjTimeblock_WaitForSong;
         } else {
             player->stateFlags2 |= 0x800000;
@@ -181,10 +181,10 @@ s32 ObjTimeblock_WaitForOcarina(ObjTimeblock* this, GlobalContext* globalCtx) {
 }
 
 s32 ObjTimeblock_WaitForSong(ObjTimeblock* this, GlobalContext* globalCtx) {
-    if (globalCtx->msgCtx.unk_E3EE == 4) {
+    if (globalCtx->msgCtx.ocarinaMode == OCARINA_MODE_04) {
         this->songObserverFunc = ObjTimeblock_WaitForOcarina;
     }
-    if (globalCtx->msgCtx.unk_E3EC == 10) {
+    if (globalCtx->msgCtx.lastPlayedSong == OCARINA_SONG_TIME) {
         if (this->unk_172 == 254) {
             this->songEndTimer = 110;
         } else {
@@ -229,7 +229,7 @@ void ObjTimeblock_Normal(ObjTimeblock* this, GlobalContext* globalCtx) {
         }
     }
 
-    this->unk_172 = globalCtx->msgCtx.unk_E3EC;
+    this->unk_172 = globalCtx->msgCtx.lastPlayedSong;
     if (this->demoEffectFirstPartTimer > 0) {
         this->demoEffectFirstPartTimer--;
         if (this->demoEffectFirstPartTimer == 0) {
@@ -254,7 +254,7 @@ void ObjTimeblock_Normal(ObjTimeblock* this, GlobalContext* globalCtx) {
 
 void func_80BA06AC(ObjTimeblock* this, GlobalContext* globalCtx) {
     s32 switchFlag = this->dyna.actor.params & 0x3F;
-    this->unk_172 = globalCtx->msgCtx.unk_E3EC;
+    this->unk_172 = globalCtx->msgCtx.lastPlayedSong;
 
     if (this->demoEffectFirstPartTimer > 0 && --this->demoEffectFirstPartTimer == 0) {
         this->unk_174 = (Flags_GetSwitch(globalCtx, switchFlag)) ? true : false;

Dragorn421/oot

src/overlays/actors/ovl_Bg_Dy_Yoseizo/z_bg_dy_yoseizo.c
@@ -169,7 +169,7 @@ void func_80872C58(BgDyYoseizo* this, GlobalContext* globalCtx) {
 
 void func_80872D20(BgDyYoseizo* this, GlobalContext* globalCtx) {
     if (Flags_GetSwitch(globalCtx, 0x38)) {
-        globalCtx->msgCtx.unk_E3EE = 4;
+        globalCtx->msgCtx.ocarinaMode = OCARINA_MODE_04;
         if (globalCtx->sceneNum == SCENE_DAIYOUSEI_IZUMI) {
             if (!gSaveContext.magicAcquired && (this->unk2EC != 0)) {
                 Actor_Kill(&this->actor);
@@ -190,7 +190,7 @@ void func_80872DE4(BgDyYoseizo* this, GlobalContext* globalCtx) {
     s32 var_v1;
 
     func_8002DF54(globalCtx, &this->actor, 1U);
-    osSyncPrintf("\x1b[33m☆☆☆☆☆ もうど ☆☆☆☆☆ %d\n\x1b[m", globalCtx->msgCtx.unk_E3EE);
+    osSyncPrintf("\x1b[33m☆☆☆☆☆ もうど ☆☆☆☆☆ %d\n\x1b[m", globalCtx->msgCtx.ocarinaMode);
     var_v1 = 0;
     if (globalCtx->sceneNum != SCENE_DAIYOUSEI_IZUMI) {
         switch (this->unk2EC) {
@@ -353,8 +353,8 @@ void func_8087358C(BgDyYoseizo* this, GlobalContext* globalCtx) {
                          -10.0f);
     }
     this->actor.textId = 0xDB;
-    this->unk2EE = 5;
-    func_8010B680(globalCtx, this->actor.textId, NULL);
+    this->unk2EE = TEXT_STATE_EVENT;
+    Message_StartTextbox(globalCtx, this->actor.textId, NULL);
     func_80872960(this, globalCtx, 0);
     this->actionFunc = func_808736A4;
 }
@@ -366,8 +366,8 @@ void func_808736A4(BgDyYoseizo* this, GlobalContext* globalCtx) {
         this->unk324 = 0.0f;
     }
     SkelAnime_Update(&this->skelAnime);
-    if ((this->unk2EE == func_8010BDBC(&globalCtx->msgCtx)) && (func_80106BC8(globalCtx) != 0)) {
-        func_80106CCC(globalCtx);
+    if ((this->unk2EE == Message_GetState(&globalCtx->msgCtx)) && Message_ShouldAdvance(globalCtx)) {
+        Message_CloseTextbox(globalCtx);
         Interface_ChangeAlpha(5U);
         this->actionFunc = func_80873780;
     }
@@ -452,8 +452,8 @@ void func_80873868(BgDyYoseizo* this, GlobalContext* globalCtx) {
     }
     if (this->unk302 == 1) {
         this->actor.textId = 0xDA;
-        this->unk2EE = 5;
-        func_8010B720(globalCtx, this->actor.textId);
+        this->unk2EE = TEXT_STATE_EVENT;
+        Message_ContinueTextbox(globalCtx, this->actor.textId);
         this->actionFunc = func_80873B3C;
     } else {
         func_80872C58(this, globalCtx);
@@ -466,8 +466,8 @@ void func_80873B3C(BgDyYoseizo* this, GlobalContext* globalCtx) {
         this->unk324 = 0.0f;
     }
     SkelAnime_Update(&this->skelAnime);
-    if ((this->unk2EE == func_8010BDBC(&globalCtx->msgCtx)) && (func_80106BC8(globalCtx) != 0)) {
-        func_80106CCC(globalCtx);
+    if ((this->unk2EE == Message_GetState(&globalCtx->msgCtx)) && Message_ShouldAdvance(globalCtx)) {
+        Message_CloseTextbox(globalCtx);
         this->unk2F6 = 0;
         this->actionFunc = func_80873C14;
         func_8005B1A4(globalCtx->cameraPtrs[globalCtx->activeCamera]);

src/overlays/actors/ovl_Obj_Timeblock/z_obj_timeblock.c
@@ -169,7 +169,7 @@ s32 func_80BA040C(ObjTimeblock* this, GlobalContext* globalCtx) {
     temp_v1 = GET_PLAYER(globalCtx);
     if (func_80BA032C(this, globalCtx) != 0) {
         if (temp_v1->stateFlags2 & 0x01000000) {
-            func_8010BD58(globalCtx, 1U);
+            func_8010BD58(globalCtx, OCARINA_ACTION_FREE_PLAY);
             this->unk168 = func_80BA0480;
         } else {
             temp_v1->stateFlags2 |= 0x800000;
@@ -179,10 +179,10 @@ s32 func_80BA040C(ObjTimeblock* this, GlobalContext* globalCtx) {
 }
 
 s32 func_80BA0480(ObjTimeblock* this, GlobalContext* globalCtx) {
-    if (globalCtx->msgCtx.unk_E3EE == 4) {
+    if (globalCtx->msgCtx.ocarinaMode == OCARINA_MODE_04) {
         this->unk168 = func_80BA040C;
     }
-    if (globalCtx->msgCtx.unk_E3EC == 0xA) {
+    if (globalCtx->msgCtx.lastPlayedSong == OCARINA_SONG_TIME) {
         if (this->unk172 == 0xFE) {
             this->unk16E = 0x6E;
         } else {
@@ -221,7 +221,7 @@ void func_80BA0524(ObjTimeblock* this, GlobalContext* globalCtx) {
             func_80BA00CC(globalCtx, this->dyna.actor.params & 0x3F);
         }
     }
-    this->unk172 = globalCtx->msgCtx.unk_E3EC;
+    this->unk172 = globalCtx->msgCtx.lastPlayedSong;
     if (this->unk170 > 0) {
         this->unk170 -= 1;
         if (this->unk170 == 0) {
@@ -254,7 +254,7 @@ void func_80BA06AC(ObjTimeblock* this, GlobalContext* globalCtx) {
 
     sp24 = this->dyna.actor.params & 0x3F;
     temp_v0 = this->unk170;
-    this->unk172 = globalCtx->msgCtx.unk_E3EC;
+    this->unk172 = globalCtx->msgCtx.lastPlayedSong;
     if (temp_v0 > 0) {
         this->unk170 = temp_v0 - 1;
         if (this->unk170 == 0) {