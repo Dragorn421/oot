HackerN64/HackerOoT

Angle cleanup - `BINANG_TO_RAD`, `BINANG_TO_RAD_ALT` (#1158)
* Run formatter

* Touch up angle macros (parentheses and hex constants)

* Add `BINANG_TO_RAD_ALT`

* Swap `BINANG_TO_RAD` and `BINANG_TO_RAD_ALT`

* Run formatter

src/overlays/actors/ovl_En_Anubice/z_en_anubice.c
@@ -337,8 +337,8 @@ void EnAnubice_Die(EnAnubice* this, GlobalContext* globalCtx) {
     rotX = curFrame * -3000.0f;
     rotX = CLAMP_MIN(rotX, -11000.0f);
 
-    Matrix_RotateY(BINANG_TO_RAD(this->actor.shape.rot.y), MTXMODE_NEW);
-    Matrix_RotateX(BINANG_TO_RAD(rotX), MTXMODE_APPLY);
+    Matrix_RotateY(BINANG_TO_RAD_ALT(this->actor.shape.rot.y), MTXMODE_NEW);
+    Matrix_RotateX(BINANG_TO_RAD_ALT(rotX), MTXMODE_APPLY);
     baseFireEffectPos.y = Rand_CenteredFloat(10.0f) + 30.0f;
     Matrix_MultVec3f(&baseFireEffectPos, &rotatedFireEffectPos);
     rotatedFireEffectPos.x += this->actor.world.pos.x + Rand_CenteredFloat(40.0f);
@@ -404,7 +404,7 @@ void EnAnubice_Update(Actor* thisx, GlobalContext* globalCtx) {
                 rotatedKnockbackVelocity.y = 0.0f;
                 rotatedKnockbackVelocity.z = 0.0f;
 
-                Matrix_RotateY(BINANG_TO_RAD(this->actor.shape.rot.y), MTXMODE_NEW);
+                Matrix_RotateY(BINANG_TO_RAD_ALT(this->actor.shape.rot.y), MTXMODE_NEW);
                 Matrix_MultVec3f(&baseKnockbackVelocity, &rotatedKnockbackVelocity);
 
                 this->actor.velocity.x = rotatedKnockbackVelocity.x;

Dragorn421/oot

src/overlays/actors/ovl_En_Anubice/z_en_anubice.c
@@ -327,8 +327,8 @@ void EnAnubice_Die(EnAnubice* this, GlobalContext* globalCtx) {
     phi_f2 = curFrame * -3000.0f;
     phi_f2 = CLAMP_MIN(phi_f2, -11000.0f);
 
-    Matrix_RotateY(BINANG_TO_RAD(this->actor.shape.rot.y), MTXMODE_NEW);
-    Matrix_RotateX(BINANG_TO_RAD(phi_f2), MTXMODE_APPLY);
+    Matrix_RotateY(BINANG_TO_RAD_ALT(this->actor.shape.rot.y), MTXMODE_NEW);
+    Matrix_RotateX(BINANG_TO_RAD_ALT(phi_f2), MTXMODE_APPLY);
     sp4C.y = Rand_CenteredFloat(10.0f) + 30.0f;
     Matrix_MultVec3f(&sp4C, &fireEffectPos);
     fireEffectPos.x += this->actor.world.pos.x + Rand_CenteredFloat(40.0f);
@@ -394,7 +394,7 @@ void EnAnubice_Update(Actor* thisx, GlobalContext* globalCtx) {
                 sp3C.y = 0.0f;
                 sp3C.z = 0.0f;
 
-                Matrix_RotateY(BINANG_TO_RAD(this->actor.shape.rot.y), MTXMODE_NEW);
+                Matrix_RotateY(BINANG_TO_RAD_ALT(this->actor.shape.rot.y), MTXMODE_NEW);
                 Matrix_MultVec3f(&sp48, &sp3C);
 
                 this->actor.velocity.x = sp3C.x;