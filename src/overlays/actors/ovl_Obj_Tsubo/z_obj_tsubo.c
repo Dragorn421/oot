HackerN64/HackerOoT

Fix misc 10 (#1208)
* Cleanup around `Item_DropCollectible`

* Cleanup around `Math3D_Vec3fDistSq`, `Math3D_Dist2DSq`

* Material/Model naming for one dlist pair

* Minor comments fixup

* Explicit `!= NULL` check

* Signed decimal for an array of coordinates

* Fixup comments some more

src/overlays/actors/ovl_Obj_Tsubo/z_obj_tsubo.c
@@ -85,7 +85,7 @@ static InitChainEntry sInitChain[] = {
 void ObjTsubo_SpawnCollectible(ObjTsubo* this, GlobalContext* globalCtx) {
     s16 dropParams = this->actor.params & 0x1F;
 
-    if ((dropParams >= ITEM00_RUPEE_GREEN) && (dropParams <= ITEM00_BOMBS_SPECIAL)) {
+    if ((dropParams >= 0) && (dropParams < ITEM00_MAX)) {
         Item_DropCollectible(globalCtx, &this->actor.world.pos,
                              (dropParams | (((this->actor.params >> 9) & 0x3F) << 8)));
     }

Dragorn421/oot

src/overlays/actors/ovl_Obj_Tsubo/z_obj_tsubo.c
@@ -80,7 +80,7 @@ void func_80BA0D60(ObjTsubo* this, GlobalContext* globalCtx) {
     s16 temp_v0;
 
     temp_v0 = this->actor.params & 0x1F;
-    if ((temp_v0 >= 0) && (temp_v0 < 0x1A)) {
+    if ((temp_v0 >= 0) && (temp_v0 < ITEM00_MAX)) {
         Item_DropCollectible(globalCtx, &this->actor.world.pos,
                              (s16)(temp_v0 | (((this->actor.params >> 9) & 0x3F) << 8)));
     }