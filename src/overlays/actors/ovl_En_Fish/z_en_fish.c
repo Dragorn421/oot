HackerN64/HackerOoT

Defines for flags passed to `Actor_UpdateBgCheckInfo` (#1127)
* Tool-assisted flags for `Actor_UpdateBgCheckInfo`

* Manual flags separation

* Run formatter

* Light doc on the flags

* `UPDBGCHECKINFOFLAG_` -> `UPDBGCHECKINFO_FLAG_` (what an improvement!)

* Run formatter

* Run formatter

src/overlays/actors/ovl_En_Fish/z_en_fish.c
@@ -363,7 +363,7 @@ void EnFish_Dropped_SetupFall(EnFish* this) {
     this->actor.minVelocityY = -10.0f;
     this->actor.shape.yOffset = 0.0f;
     EnFish_SetOutOfWaterAnimation(this);
-    this->unk_250 = 5;
+    this->unk_250 = UPDBGCHECKINFO_FLAG_0 | UPDBGCHECKINFO_FLAG_2;
     this->actionFunc = EnFish_Dropped_Fall;
     this->timer = 300;
 }
@@ -424,7 +424,7 @@ void EnFish_Dropped_SetupFlopOnGround(EnFish* this) {
     this->actor.shape.yOffset = 300.0f;
     EnFish_SetOutOfWaterAnimation(this);
     this->actionFunc = EnFish_Dropped_FlopOnGround;
-    this->unk_250 = 5;
+    this->unk_250 = UPDBGCHECKINFO_FLAG_0 | UPDBGCHECKINFO_FLAG_2;
 
     if (playSound && (this->actor.draw != NULL)) {
         Audio_PlayActorSound2(&this->actor, NA_SE_EV_FISH_LEAP);
@@ -480,7 +480,7 @@ void EnFish_Dropped_SetupSwimAway(EnFish* this) {
     this->actor.shape.yOffset = 0.0f;
     EnFish_SetInWaterAnimation(this);
     this->actionFunc = EnFish_Dropped_SwimAway;
-    this->unk_250 = 5;
+    this->unk_250 = UPDBGCHECKINFO_FLAG_0 | UPDBGCHECKINFO_FLAG_2;
 }
 
 void EnFish_Dropped_SwimAway(EnFish* this, GlobalContext* globalCtx) {

Dragorn421/oot

src/overlays/actors/ovl_En_Fish/z_en_fish.c
@@ -361,7 +361,7 @@ void func_80A15F24(EnFish* this) {
     this->actor.minVelocityY = -10.0f;
     this->actor.shape.yOffset = 0.0f;
     func_80A15310(this);
-    this->unk250 = 5;
+    this->unk250 = UPDBGCHECKINFO_FLAG_0 | UPDBGCHECKINFO_FLAG_2;
     this->unk244 = func_80A15F84;
     this->unk248 = 0x12C;
 }
@@ -413,7 +413,7 @@ void func_80A160BC(EnFish* this) {
     this->actor.shape.yOffset = 300.0f;
     func_80A15310(this);
     this->unk244 = func_80A16200;
-    this->unk250 = 5;
+    this->unk250 = UPDBGCHECKINFO_FLAG_0 | UPDBGCHECKINFO_FLAG_2;
     if ((var_v0 != 0) && (this->actor.draw != NULL)) {
         Audio_PlayActorSound2(&this->actor, NA_SE_EV_FISH_LEAP);
     }
@@ -462,7 +462,7 @@ void func_80A163DC(EnFish* this) {
     this->actor.shape.yOffset = 0.0f;
     func_80A152AC(this);
     this->unk244 = func_80A16450;
-    this->unk250 = 5;
+    this->unk250 = UPDBGCHECKINFO_FLAG_0 | UPDBGCHECKINFO_FLAG_2;
 }
 
 void func_80A16450(EnFish* this, GlobalContext* globalCtx) {