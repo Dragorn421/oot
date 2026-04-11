HackerN64/HackerOoT

Doc misc 4 (#1177)
* Doc/cleanup misc

* Name `prevFrame`/`beforeCurFrame`/`afterPrevFrame` and related variables

* Actually this pad variable I renamed can be removed

src/overlays/actors/ovl_En_Wf/z_en_wf.c
@@ -490,13 +490,13 @@ void EnWf_SetupRunAtPlayer(EnWf* this, GlobalContext* globalCtx) {
 }
 
 void EnWf_RunAtPlayer(EnWf* this, GlobalContext* globalCtx) {
-    s32 animPrevFrame;
-    s32 sp58;
+    s32 prevFrame;
+    s32 beforeCurFrame;
     s32 pad;
     f32 baseRange = 0.0f;
     s16 playerFacingAngleDiff;
     Player* player = GET_PLAYER(globalCtx);
-    s32 playSpeed;
+    s32 absPlaySpeed;
 
     if (!EnWf_DodgeRanged(globalCtx, this)) {
         Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsPlayer, 1, 0x2EE, 0);
@@ -528,10 +528,10 @@ void EnWf_RunAtPlayer(EnWf* this, GlobalContext* globalCtx) {
             }
         }
 
-        animPrevFrame = this->skelAnime.curFrame;
+        prevFrame = (s32)this->skelAnime.curFrame;
         SkelAnime_Update(&this->skelAnime);
-        sp58 = this->skelAnime.curFrame - ABS(this->skelAnime.playSpeed);
-        playSpeed = (f32)ABS(this->skelAnime.playSpeed);
+        beforeCurFrame = (s32)(this->skelAnime.curFrame - ABS(this->skelAnime.playSpeed));
+        absPlaySpeed = (s32)(f32)ABS(this->skelAnime.playSpeed);
 
         if (!Actor_IsFacingPlayer(&this->actor, 0x11C7)) {
             if (Rand_ZeroOne() > 0.5f) {
@@ -556,7 +556,8 @@ void EnWf_RunAtPlayer(EnWf* this, GlobalContext* globalCtx) {
             if ((globalCtx->gameplayFrames & 95) == 0) {
                 Audio_PlayActorSound2(&this->actor, NA_SE_EN_WOLFOS_CRY);
             }
-            if ((animPrevFrame != (s32)this->skelAnime.curFrame) && (sp58 <= 0) && ((playSpeed + animPrevFrame) > 0)) {
+            if ((prevFrame != (s32)this->skelAnime.curFrame) && (beforeCurFrame <= 0) &&
+                ((absPlaySpeed + prevFrame) > 0)) {
                 Audio_PlayActorSound2(&this->actor, NA_SE_EN_WOLFOS_WALK);
                 Actor_SpawnFloorDustRing(globalCtx, &this->actor, &this->actor.world.pos, 20.0f, 3, 3.0f, 50, 50, true);
             }
@@ -632,9 +633,9 @@ void EnWf_RunAroundPlayer(EnWf* this, GlobalContext* globalCtx) {
     s16 angle2;
     s32 pad;
     f32 baseRange = 0.0f;
-    s32 animPrevFrame;
-    s32 animFrameSpeedDiff;
-    s32 animSpeed;
+    s32 prevFrame;
+    s32 beforeCurFrame;
+    s32 absPlaySpeed;
     Player* player = GET_PLAYER(globalCtx);
 
     Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsPlayer + this->runAngle, 1, 4000, 1);
@@ -679,15 +680,14 @@ void EnWf_RunAroundPlayer(EnWf* this, GlobalContext* globalCtx) {
         } else {
             this->skelAnime.playSpeed = this->runSpeed * 0.175f;
         }
-
         this->skelAnime.playSpeed = CLAMP(this->skelAnime.playSpeed, -3.0f, 3.0f);
-        animPrevFrame = this->skelAnime.curFrame;
-        SkelAnime_Update(&this->skelAnime);
-        animFrameSpeedDiff = this->skelAnime.curFrame - ABS(this->skelAnime.playSpeed);
-        animSpeed = (f32)ABS(this->skelAnime.playSpeed);
 
-        if ((animPrevFrame != (s32)this->skelAnime.curFrame) && (animFrameSpeedDiff <= 0) &&
-            (animSpeed + animPrevFrame > 0)) {
+        prevFrame = (s32)this->skelAnime.curFrame;
+        SkelAnime_Update(&this->skelAnime);
+        beforeCurFrame = (s32)(this->skelAnime.curFrame - ABS(this->skelAnime.playSpeed));
+        absPlaySpeed = (s32)(f32)ABS(this->skelAnime.playSpeed);
+
+        if ((prevFrame != (s32)this->skelAnime.curFrame) && (beforeCurFrame <= 0) && (absPlaySpeed + prevFrame > 0)) {
             Audio_PlayActorSound2(&this->actor, NA_SE_EN_WOLFOS_WALK);
             Actor_SpawnFloorDustRing(globalCtx, &this->actor, &this->actor.world.pos, 20.0f, 3, 3.0f, 50, 50, true);
         }
@@ -1091,9 +1091,9 @@ void EnWf_SetupSidestep(EnWf* this, GlobalContext* globalCtx) {
 void EnWf_Sidestep(EnWf* this, GlobalContext* globalCtx) {
     s16 angleDiff1;
     Player* player = GET_PLAYER(globalCtx);
-    s32 animPrevFrame;
-    s32 animFrameSpeedDiff;
-    s32 animSpeed;
+    s32 prevFrame;
+    s32 beforeCurFrame;
+    s32 absPlaySpeed;
     f32 baseRange = 0.0f;
 
     Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsPlayer + this->runAngle, 1, 3000, 1);
@@ -1136,13 +1136,12 @@ void EnWf_Sidestep(EnWf* this, GlobalContext* globalCtx) {
     } else {
         this->skelAnime.playSpeed = this->runSpeed * 0.175f;
     }
-
     this->skelAnime.playSpeed = CLAMP(this->skelAnime.playSpeed, -3.0f, 3.0f);
 
-    animPrevFrame = this->skelAnime.curFrame;
+    prevFrame = (s32)this->skelAnime.curFrame;
     SkelAnime_Update(&this->skelAnime);
-    animFrameSpeedDiff = this->skelAnime.curFrame - ABS(this->skelAnime.playSpeed);
-    animSpeed = (f32)ABS(this->skelAnime.playSpeed);
+    beforeCurFrame = (s32)(this->skelAnime.curFrame - ABS(this->skelAnime.playSpeed));
+    absPlaySpeed = (s32)(f32)ABS(this->skelAnime.playSpeed);
 
     if (!EnWf_ChangeAction(globalCtx, this, false)) {
         this->actionTimer--;
@@ -1169,8 +1168,7 @@ void EnWf_Sidestep(EnWf* this, GlobalContext* globalCtx) {
             }
         }
 
-        if ((animPrevFrame != (s32)this->skelAnime.curFrame) && (animFrameSpeedDiff <= 0) &&
-            ((animSpeed + animPrevFrame) > 0)) {
+        if ((prevFrame != (s32)this->skelAnime.curFrame) && (beforeCurFrame <= 0) && ((absPlaySpeed + prevFrame) > 0)) {
             Audio_PlayActorSound2(&this->actor, NA_SE_EN_WOLFOS_WALK);
             Actor_SpawnFloorDustRing(globalCtx, &this->actor, &this->actor.world.pos, 20.0f, 3, 3.0f, 50, 50, true);
         }

Dragorn421/oot

src/overlays/actors/ovl_En_Wf/z_en_wf.c
@@ -455,8 +455,8 @@ void func_80B347FC(EnWf* this, GlobalContext* globalCtx) {
 }
 
 void func_80B3487C(EnWf* this, GlobalContext* globalCtx) {
-    s32 sp5C_real;
-    s32 sp58_real;
+    s32 prevFrame;
+    s32 beforeCurFrame;
     s32 pad;
     f32 sp50_real;
     s32 pad2;
@@ -464,7 +464,7 @@ void func_80B3487C(EnWf* this, GlobalContext* globalCtx) {
     s32 pad3;
     s16 temp_v1_real;
     s16 var_v0_real;
-    f32 sp3C_probreal;
+    f32 absPlaySpeed;
     s32 pad4;
 
     sp50_real = 0.0f;
@@ -495,11 +495,11 @@ void func_80B3487C(EnWf* this, GlobalContext* globalCtx) {
                 return;
             }
         }
-        sp5C_real = (s32)this->unk188.curFrame;
+        prevFrame = (s32)this->unk188.curFrame;
         SkelAnime_Update(&this->unk188);
-        sp3C_probreal = ABS(this->unk188.playSpeed);
-        sp58_real = (s32)(this->unk188.curFrame - sp3C_probreal);
-        sp3C_probreal = ABS(this->unk188.playSpeed);
+        absPlaySpeed = ABS(this->unk188.playSpeed);
+        beforeCurFrame = (s32)(this->unk188.curFrame - absPlaySpeed);
+        absPlaySpeed = ABS(this->unk188.playSpeed);
         if (Actor_IsFacingPlayer(&this->actor, 0x11C7) == 0) {
             if (Rand_ZeroOne() > 0.5f) {
                 func_80B34F28(this);
@@ -523,8 +523,8 @@ void func_80B3487C(EnWf* this, GlobalContext* globalCtx) {
             if ((globalCtx->gameplayFrames & 0x5F) == 0) {
                 Audio_PlayActorSound2(&this->actor, NA_SE_EN_WOLFOS_CRY);
             }
-            if ((sp5C_real != (s32)this->unk188.curFrame) && (sp58_real <= 0) &&
-                (((s32)sp3C_probreal + sp5C_real) > 0)) {
+            if ((prevFrame != (s32)this->unk188.curFrame) && (beforeCurFrame <= 0) &&
+                (((s32)absPlaySpeed + prevFrame) > 0)) {
                 Audio_PlayActorSound2(&this->actor, NA_SE_EN_WOLFOS_WALK);
                 Actor_SpawnFloorDustRing(globalCtx, &this->actor, &this->actor.world.pos, 20.0f, 3, 3.0f, 50, 50, true);
             }
@@ -599,9 +599,9 @@ void func_80B35024(EnWf* this, GlobalContext* globalCtx) {
     s16 sp56_real;
     s32 new_var;
     f32 sp4C_real;
-    s32 temp_ft2_sp48_real;
-    s32 v;
-    s32 v2;
+    s32 prevFrame;
+    s32 beforeCurFrame;
+    s32 absPlaySpeed;
     Player* player;
     s16 var_v0_2_real;
 
@@ -642,11 +642,11 @@ void func_80B35024(EnWf* this, GlobalContext* globalCtx) {
             this->unk188.playSpeed = this->unk2EC * 0.175f;
         }
         this->unk188.playSpeed = CLAMP(this->unk188.playSpeed, -3.0f, 3.0f);
-        temp_ft2_sp48_real = (s32)this->unk188.curFrame;
+        prevFrame = (s32)this->unk188.curFrame;
         SkelAnime_Update(&this->unk188);
-        v = (s32)(this->unk188.curFrame - ABS(this->unk188.playSpeed));
-        v2 = new_var = (s32)ABS(this->unk188.playSpeed);
-        if ((temp_ft2_sp48_real != (s32)this->unk188.curFrame) && (v <= 0) && ((v2 + temp_ft2_sp48_real) > 0)) {
+        beforeCurFrame = (s32)(this->unk188.curFrame - ABS(this->unk188.playSpeed));
+        absPlaySpeed = new_var = (s32)ABS(this->unk188.playSpeed);
+        if ((prevFrame != (s32)this->unk188.curFrame) && (beforeCurFrame <= 0) && ((absPlaySpeed + prevFrame) > 0)) {
             Audio_PlayActorSound2(&this->actor, NA_SE_EN_WOLFOS_WALK);
             Actor_SpawnFloorDustRing(globalCtx, &this->actor, &this->actor.world.pos, 20.0f, 3, 3.0f, 50, 50, true);
         }
@@ -1028,11 +1028,11 @@ void func_80B365A8(EnWf* this, GlobalContext* globalCtx) {
 void func_80B36740(EnWf* this, GlobalContext* globalCtx) {
     Player* player2;
     Player* player;
-    s32 sp64_real;
-    s32 sp60_real;
+    s32 prevFrame;
+    s32 beforeCurFrame;
     s32 new_var;
     f32 sp58_real;
-    s32 var_fa0_4_sp44_real;
+    s32 absPlaySpeed;
     s16 var_v0_probreal;
     s16 var_v0_2_probreal;
     s16 temp_a2_sp4E;
@@ -1072,10 +1072,10 @@ void func_80B36740(EnWf* this, GlobalContext* globalCtx) {
         this->unk188.playSpeed = this->unk2EC * 0.175f;
     }
     this->unk188.playSpeed = CLAMP(this->unk188.playSpeed, -3.0f, 3.0f);
-    sp64_real = (s32)this->unk188.curFrame;
+    prevFrame = (s32)this->unk188.curFrame;
     SkelAnime_Update(&this->unk188);
-    sp60_real = (s32)(this->unk188.curFrame - ABS(this->unk188.playSpeed));
-    var_fa0_4_sp44_real = new_var = ABS(this->unk188.playSpeed);
+    beforeCurFrame = (s32)(this->unk188.curFrame - ABS(this->unk188.playSpeed));
+    absPlaySpeed = new_var = ABS(this->unk188.playSpeed);
     if (func_80B33FB0(globalCtx, this, 0) == 0) {
         this->unk2E8 -= 1;
         if (this->unk2E8 == 0) {
@@ -1098,8 +1098,8 @@ void func_80B36740(EnWf* this, GlobalContext* globalCtx) {
                 }
             }
         }
-        if ((sp64_real != (s32)this->unk188.curFrame) && (sp60_real <= 0) &&
-            (((s32)var_fa0_4_sp44_real + sp64_real) > 0)) {
+        if ((prevFrame != (s32)this->unk188.curFrame) && (beforeCurFrame <= 0) &&
+            (((s32)absPlaySpeed + prevFrame) > 0)) {
             Audio_PlayActorSound2(&this->actor, NA_SE_EN_WOLFOS_WALK);
             Actor_SpawnFloorDustRing(globalCtx, &this->actor, &this->actor.world.pos, 20.0f, 3, 3.0f, 50, 50, true);
         }