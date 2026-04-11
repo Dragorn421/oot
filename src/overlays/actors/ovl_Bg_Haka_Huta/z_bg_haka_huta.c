HackerN64/HackerOoT

Document code_8006BA00 (Audio Sound Sources) (#997)
* Document SoundSources

* PR Suggestions

* Duration timers to dec

* `PlaySfxByPosAndId` -> `PlaySoundByPosition`

* Improve names

* `PlaySfxAtStationaryPosition` -> `PlaySfxAtFixedWorldPos` and `originWorldPos` -> `worldPos`

* format

src/overlays/actors/ovl_Bg_Haka_Huta/z_bg_haka_huta.c
@@ -100,7 +100,7 @@ void BgHakaHuta_PlaySound(BgHakaHuta* this, GlobalContext* globalCtx, u16 sfx) {
                                                 : this->dyna.actor.world.pos.z - 120.0f;
     pos.x = this->dyna.actor.world.pos.x;
     pos.y = this->dyna.actor.world.pos.y;
-    Audio_PlaySoundAtPosition(globalCtx, &pos, 30, sfx);
+    SoundSource_PlaySfxAtFixedWorldPos(globalCtx, &pos, 30, sfx);
 }
 
 void BgHakaHuta_SpawnEnemies(BgHakaHuta* this, GlobalContext* globalCtx) {

Dragorn421/oot

src/overlays/actors/ovl_Bg_Haka_Huta/z_bg_haka_huta.c
@@ -99,7 +99,7 @@ void BgHakaHuta_PlaySound(BgHakaHuta* this, GlobalContext* globalCtx, u16 arg2) 
     }
     sp24.x = this->dyna.actor.world.pos.x;
     sp24.y = this->dyna.actor.world.pos.y;
-    Audio_PlaySoundAtPosition(globalCtx, &sp24, 30, arg2);
+    SoundSource_PlaySfxAtFixedWorldPos(globalCtx, &sp24, 30, arg2);
 }
 
 void func_8087D2F0(BgHakaHuta* this, GlobalContext* globalCtx) {