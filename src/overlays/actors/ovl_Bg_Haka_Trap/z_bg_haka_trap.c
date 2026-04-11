HackerN64/HackerOoT

Document parts of audio (#1000)
* Minor labelling

* progress

* play percentage -> delay

* duration -> gatetime

* more

* more

* more

* seqIdx -> playerIdx

* more

* more

* more

* more

* format

* fix comment

* filters

* more

* media

* confusion

* Sync load is actually slow load

* AudioHeap prefix

* more

* more

* reformat

* more

* more

* AudioLoad

* more

* more

* seq banks

* more consistent

* more

* name last function in audio_load

* More audio_synthesis

* clean up audio tables

* minor

* slow/fast load ramAddr

* format

* remove unused

* Remove union

* remove padding

* audio bank -> sound font

* seqLayer -> layer

* stuff

* seqChannel -> channel

* ChannelLayer -> Layer

* remove define, add bug comment

* format

* more

* cache enum

* more

* AudioSeq function prefix

* naming

* bankIdx -> bankId

* more

* format

* review

* more

* fixes

* avoid hardcoded sfxid's

* SE -> Sfx

Co-authored-by: zelda2774 <zelda2774@invalid>

src/overlays/actors/ovl_Bg_Haka_Trap/z_bg_haka_trap.c
@@ -200,7 +200,7 @@ void BgHakaTrap_Destroy(Actor* thisx, GlobalContext* globalCtx) {
         Collider_DestroyCylinder(globalCtx, &this->colliderCylinder);
     }
 
-    func_800F89E8(&this->unk_16C);
+    Audio_StopSfxByPos(&this->unk_16C);
 }
 
 void func_8087FFC0(BgHakaTrap* this, GlobalContext* globalCtx) {

Dragorn421/oot

src/overlays/actors/ovl_Bg_Haka_Trap/z_bg_haka_trap.c
@@ -154,7 +154,7 @@ void BgHakaTrap_Destroy(Actor* thisx, GlobalContext* globalCtx) {
         }
         Collider_DestroyCylinder(globalCtx, &this->unk178);
     }
-    func_800F89E8(&this->unk16C);
+    Audio_StopSfxByPos(&this->unk16C);
 }
 
 void func_8087FFC0(BgHakaTrap* this, GlobalContext* globalCtx) {