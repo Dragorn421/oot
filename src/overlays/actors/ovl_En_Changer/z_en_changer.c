HackerN64/HackerOoT

Defines for `SaveContext` flags (`eventChkInf`, `itemGetInf`, `infTable`, `eventInf`) (#1094)
* script-assisted `itemGetInf` flags

* comment near non-trivial `itemGetInf` usage

* Run formatter

* Try something with the itemgetinf flags that can't use the packed value

* Add defines for `eventChkInf` (direct access)

* Add defines for `eventChkInf` (access through `Flags_GetEventChkInf`)

* Add defines for `eventChkInf` (access through `Flags_SetEventChkInf`)

* Add defines for `infTable` (direct access)

* Add defines for `infTable` (used by `Flags_GetInfTable`)

* Add defines for `infTable` (used by `Flags_SetInfTable`)

* Add defines for `eventInf`

* parenthesis cleanup near `GET_EVENTCHKINF` usage

* parenthesis cleanup near `GET_ITEMGETINF` usage

* parenthesis cleanup near `GET_INFTABLE` usage

* fixup one `eventInf` usage

* parenthesis cleanup near `GET_EVENTINF` usage

* parenthesis cleanup near `Flags_GetEventChkInf` usage

* slight `z64save.h` formatting improvement

* Improve itemGetInf flags in z_bg_dy_yoseizo

* Questionable improvement in z_en_ge2

* Questionable improvement in z_en_daiku

* Questionable improvement in z_en_mu

* Run formatter

* Parentheses around `gSaveContext` macros arguments

* Move individual flags define to the end of z64save.h, and improve comments separating the groups

src/overlays/actors/ovl_En_Changer/z_en_changer.c
@@ -101,7 +101,7 @@ void EnChanger_Init(Actor* thisx, GlobalContext* globalCtx2) {
     minigameRoomNum *= 2;
     // Spawn Heart Piece in chest (or Purple Rupee if won Heart Piece)
     if (globalCtx->roomCtx.curRoom.num >= 6) {
-        rewardChestParams = ((gSaveContext.itemGetInf[1] & 0x800) ? (0x4EA0) : (0x4EC0));
+        rewardChestParams = GET_ITEMGETINF(ITEMGETINF_1B) ? 0x4EA0 : 0x4EC0;
         rewardChestParams = sTreasureFlags[5] | rewardChestParams;
         this->finalChest = (EnBox*)Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_EN_BOX,
                                                       20.0f, 20.0f, -2500.0f, 0, 0x7FFF, 0, rewardChestParams);
@@ -111,8 +111,8 @@ void EnChanger_Init(Actor* thisx, GlobalContext* globalCtx2) {
                 Actor_Kill(&this->actor);
                 return;
             } else {
-                rewardParams = ((gSaveContext.itemGetInf[1] & 0x800) ? (ITEM_ETC_RUPEE_PURPLE_CHEST_GAME)
-                                                                     : (ITEM_ETC_HEART_PIECE_CHEST_GAME)) &
+                rewardParams = (GET_ITEMGETINF(ITEMGETINF_1B) ? ITEM_ETC_RUPEE_PURPLE_CHEST_GAME
+                                                              : ITEM_ETC_HEART_PIECE_CHEST_GAME) &
                                0xFF;
                 Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_ITEM_ETCETERA, 20.0f, 20.0f, -2500.0f, 0, 0, 0,
                             ((sTreasureFlags[5] & 0x1F) << 8) + rewardParams);
@@ -276,8 +276,8 @@ void EnChanger_OpenChests(EnChanger* this, GlobalContext* globalCtx) {
 
 void EnChanger_SetHeartPieceFlag(EnChanger* this, GlobalContext* globalCtx) {
     if (this->finalChest->unk_1F4 != 0) {
-        if (!(gSaveContext.itemGetInf[1] & 0x800)) {
-            gSaveContext.itemGetInf[1] |= 0x800;
+        if (!GET_ITEMGETINF(ITEMGETINF_1B)) {
+            SET_ITEMGETINF(ITEMGETINF_1B);
         }
         Actor_Kill(&this->actor);
     }

Dragorn421/oot

src/overlays/actors/ovl_En_Changer/z_en_changer.c
@@ -63,7 +63,7 @@ void EnChanger_Init(Actor* thisx, GlobalContext* globalCtx2) {
     osSyncPrintf("\n\n");
     temp_v1_curRoomIndex = temp_v1_curRoomIndex * 2;
     if (globalCtx->roomCtx.curRoom.num >= 6) {
-        sp6A_finalChestParams = (gSaveContext.itemGetInf[1] & 0x800) ? 0x4EA0 : 0x4EC0;
+        sp6A_finalChestParams = GET_ITEMGETINF(ITEMGETINF_1B) ? 0x4EA0 : 0x4EC0;
         sp6A_finalChestParams = sTreasureFlags[5] | sp6A_finalChestParams;
         this->unk158 = (EnBox*)Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_EN_BOX, 20.0f,
                                                    20.0f, -2500.0f, 0, 0x7FFF, 0, sp6A_finalChestParams);
@@ -204,8 +204,8 @@ void func_809D2D70(EnChanger* this, GlobalContext* globalCtx) {
 
 void func_809D2F74(EnChanger* this, GlobalContext* globalCtx) {
     if (this->unk158->unk_1F4 != 0) {
-        if (!(gSaveContext.itemGetInf[1] & 0x800)) {
-            gSaveContext.itemGetInf[1] |= 0x800;
+        if (!GET_ITEMGETINF(ITEMGETINF_1B)) {
+            SET_ITEMGETINF(ITEMGETINF_1B);
         }
         Actor_Kill(&this->actor);
     }