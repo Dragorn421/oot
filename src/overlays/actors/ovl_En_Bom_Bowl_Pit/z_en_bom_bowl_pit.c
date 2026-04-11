HackerN64/HackerOoT

Names for some view things (#1191)
* Names for some view things

* Review: ORTHOGRAPHIC → ORTHO

* Review: dirty → dirtyFlags

* Review: Doc comment style

* Review: Define constants in terms of each other

* Review: Rename the ApplyToOverlay functions

* Update include/z64.h

Co-authored-by: fig02 <fig02srl@gmail.com>

* Review: Better View_LookAtInternal doc

* arg1 → mask

* View_LookAtInternal → View_LookAtUnsafe

* Review: View_SanityCheckEyePosition → View_ErrorCheckEyePosition

* sViewNotInitialized → sLogOnNextViewInit

* Remove VIEW_FORCE_ALL

* near/far → zNear/zFar

Co-authored-by: engineer124 <47598039+engineer124@users.noreply.github.com>

* lookAt → at

Co-authored-by: engineer124 <47598039+engineer124@users.noreply.github.com>

* near/far → zNear/zFar

* View_ApplyPerspective/Ortho: Remove from header instead of commenting

* lookAt → at

* Add fwd decls

* Fix external view->at accesses

* Missed these somehow

* lookAt → at

* Omit "Apply" in comments

* dirtyFlags → flags

* View_ApplyScissor → View_ApplyShrinkWindow

* Update src/code/z_view.c

Co-authored-by: Dragorn421 <Dragorn421@users.noreply.github.com>

Co-authored-by: fig02 <fig02srl@gmail.com>
Co-authored-by: engineer124 <47598039+engineer124@users.noreply.github.com>
Co-authored-by: Dragorn421 <Dragorn421@users.noreply.github.com>

src/overlays/actors/ovl_En_Bom_Bowl_Pit/z_en_bom_bowl_pit.c
@@ -77,9 +77,9 @@ void EnBomBowlPit_DetectHit(EnBomBowlPit* this, GlobalContext* globalCtx) {
                 this->unk_1C8.x = this->unk_1C8.y = this->unk_1C8.z = 0.1f;
                 this->unk_1A4.x = this->unk_1A4.y = this->unk_1A4.z = 0.1f;
 
-                this->unk_180.x = this->unk_168.x = globalCtx->view.lookAt.x;
-                this->unk_180.y = this->unk_168.y = globalCtx->view.lookAt.y;
-                this->unk_180.z = this->unk_168.z = globalCtx->view.lookAt.z;
+                this->unk_180.x = this->unk_168.x = globalCtx->view.at.x;
+                this->unk_180.y = this->unk_168.y = globalCtx->view.at.y;
+                this->unk_180.z = this->unk_168.z = globalCtx->view.at.z;
 
                 this->unk_18C.x = this->unk_174.x = globalCtx->view.eye.x;
                 this->unk_18C.y = this->unk_174.y = globalCtx->view.eye.y;

Dragorn421/oot

src/overlays/actors/ovl_En_Bom_Bowl_Pit/z_en_bom_bowl_pit.c
@@ -77,9 +77,9 @@ void func_809C4E8C(EnBomBowlPit* this, GlobalContext* globalCtx) {
                 Gameplay_ChangeCameraStatus(globalCtx, this->unk152, CAM_STAT_ACTIVE);
                 this->unk1C8.x = this->unk1C8.y = this->unk1C8.z = 0.1f;
                 this->unk1A4.x = this->unk1A4.y = this->unk1A4.z = 0.1f;
-                this->unk180.x = this->unk168.x = globalCtx->view.lookAt.x;
-                this->unk180.y = this->unk168.y = globalCtx->view.lookAt.y;
-                this->unk180.z = this->unk168.z = globalCtx->view.lookAt.z;
+                this->unk180.x = this->unk168.x = globalCtx->view.at.x;
+                this->unk180.y = this->unk168.y = globalCtx->view.at.y;
+                this->unk180.z = this->unk168.z = globalCtx->view.at.z;
                 this->unk18C.x = this->unk174.x = globalCtx->view.eye.x;
                 this->unk18C.y = this->unk174.y = globalCtx->view.eye.y;
                 this->unk18C.z = this->unk174.z = globalCtx->view.eye.z;