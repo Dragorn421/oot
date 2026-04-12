HackerN64/HackerOoT

Doc Actor Rotation Functions (last 4 funcs in actor.c) (#1249)
* Cleanup functions

* Rename functions

* functions comments

* fix apostrophes

* Renames TurnTo -> Track, other review comments

src/overlays/actors/ovl_Bg_Dy_Yoseizo/z_bg_dy_yoseizo.c
@@ -859,7 +859,7 @@ void BgDyYoseizo_Update(Actor* thisx, PlayState* play2) {
     this->heightOffset = this->scale * 7500.0f;
     Actor_SetFocus(&this->actor, this->heightOffset);
     this->actor.focus.pos.y = this->heightOffset;
-    func_80038290(play, &this->actor, &this->headRot, &this->torsoRot, this->actor.focus.pos);
+    Actor_TrackPlayer(play, &this->actor, &this->headRot, &this->torsoRot, this->actor.focus.pos);
     BgDyYoseizo_UpdateEffects(this, play);
     Actor_SetScale(&this->actor, this->scale);
 }

src/overlays/actors/ovl_En_Bom_Bowl_Man/z_en_bom_bowl_man.c
@@ -497,7 +497,7 @@ void EnBomBowlMan_Update(Actor* thisx, PlayState* play) {
                 }
             }
 
-            func_80038290(play, &this->actor, &this->unk_218, &this->unk_224, this->actor.focus.pos);
+            Actor_TrackPlayer(play, &this->actor, &this->unk_218, &this->unk_224, this->actor.focus.pos);
             break;
     }
 

src/overlays/actors/ovl_En_Ge1/z_en_ge1.c
@@ -693,7 +693,7 @@ void EnGe1_TurnToFacePlayer(EnGe1* this, PlayState* play) {
     if (ABS(angleDiff) <= 0x4000) {
         Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsPlayer, 6, 4000, 100);
         this->actor.world.rot.y = this->actor.shape.rot.y;
-        func_80038290(play, &this->actor, &this->headRot, &this->unk_2A2, this->actor.focus.pos);
+        Actor_TrackPlayer(play, &this->actor, &this->headRot, &this->unk_2A2, this->actor.focus.pos);
     } else {
         if (angleDiff < 0) {
             Math_SmoothStepToS(&this->headRot.y, -0x2000, 6, 6200, 0x100);
@@ -710,7 +710,7 @@ void EnGe1_LookAtPlayer(EnGe1* this, PlayState* play) {
     s16 angleDiff = this->actor.yawTowardsPlayer - this->actor.shape.rot.y;
 
     if ((ABS(angleDiff) <= 0x4300) && (this->actor.xzDistToPlayer < 100.0f)) {
-        func_80038290(play, &this->actor, &this->headRot, &this->unk_2A2, this->actor.focus.pos);
+        Actor_TrackPlayer(play, &this->actor, &this->headRot, &this->unk_2A2, this->actor.focus.pos);
     } else {
         Math_SmoothStepToS(&this->headRot.x, 0, 6, 6200, 100);
         Math_SmoothStepToS(&this->headRot.y, 0, 6, 6200, 100);

Dragorn421/oot

src/overlays/actors/ovl_Bg_Dy_Yoseizo/z_bg_dy_yoseizo.c
@@ -802,7 +802,7 @@ void BgDyYoseizo_Update(Actor* thisx, PlayState* play2) {
     this->unk328 = this->unk308 * 7500.0f;
     Actor_SetFocus(&this->actor, this->unk328);
     this->actor.focus.pos.y = this->unk328;
-    func_80038290(play, &this->actor, &this->unk334, &this->unk33A, this->actor.focus.pos);
+    Actor_TrackPlayer(play, &this->actor, &this->unk334, &this->unk33A, this->actor.focus.pos);
     BgDyYoseizo_UpdateEffects(this, play);
     Actor_SetScale(&this->actor, this->unk308);
 }

src/overlays/actors/ovl_En_Bom_Bowl_Man/z_en_bom_bowl_man.c
@@ -450,7 +450,7 @@ void EnBomBowlMan_Update(Actor* thisx, PlayState* play) {
                     this->unk236 = (s16)Rand_ZeroFloat(60.0f) + 0x14;
                 }
             }
-            func_80038290(play, &this->actor, &this->unk218, &this->unk224, this->actor.focus.pos);
+            Actor_TrackPlayer(play, &this->actor, &this->unk218, &this->unk224, this->actor.focus.pos);
             break;
     }
     if (this->unk22A == 0) {

src/overlays/actors/ovl_En_Ge1/z_en_ge1.c
@@ -629,7 +629,7 @@ void func_80A32078(EnGe1* this, PlayState* play) {
     if (ABS(temp_v0) <= 0x4000) {
         Math_SmoothStepToS(&this->actor.shape.rot.y, this->actor.yawTowardsPlayer, 6, 0xFA0, 0x64);
         this->actor.world.rot.y = this->actor.shape.rot.y;
-        func_80038290(play, &this->actor, &this->unk29C, &this->unk2A2, this->actor.focus.pos);
+        Actor_TrackPlayer(play, &this->actor, &this->unk29C, &this->unk2A2, this->actor.focus.pos);
     } else {
         if (temp_v0 < 0) {
             Math_SmoothStepToS(&this->unk29C.y, -0x2000, 6, 0x1838, 0x100);
@@ -646,7 +646,7 @@ void func_80A32190(EnGe1* this, PlayState* play) {
 
     temp_v0 = this->actor.yawTowardsPlayer - this->actor.shape.rot.y;
     if ((ABS(temp_v0) <= 0x4300) && (this->actor.xzDistToPlayer < 100.0f)) {
-        func_80038290(play, &this->actor, &this->unk29C, &this->unk2A2, this->actor.focus.pos);
+        Actor_TrackPlayer(play, &this->actor, &this->unk29C, &this->unk2A2, this->actor.focus.pos);
     } else {
         Math_SmoothStepToS(&this->unk29C.x, 0, 6, 0x1838, 0x64);
         Math_SmoothStepToS(&this->unk29C.y, 0, 6, 0x1838, 0x64);