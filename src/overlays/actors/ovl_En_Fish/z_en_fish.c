HackerN64/HackerOoT

Fix RaycastFloor flags (#1328)
* fix raycast floor flags

* format.sh

* Name flags

* Rename RaycastFloor functions, clean up caller code, document z_bg_spot15_rrbox

* change comment to prevent format wrap

* change to "if BGCHECK_GROUND_CHECK_ON is set"

* roman suggestions

src/overlays/actors/ovl_En_Fish/z_en_fish.c
@@ -669,7 +669,7 @@ void EnFish_UpdateCutscene(EnFish* this, PlayState* play) {
     this->actor.world.pos.z = (endPos.z - startPos.z) * progress + startPos.z;
 
     this->actor.floorHeight =
-        BgCheck_EntityRaycastFloor4(&play->colCtx, &this->actor.floorPoly, &bgId, &this->actor, &this->actor.world.pos);
+        BgCheck_EntityRaycastDown4(&play->colCtx, &this->actor.floorPoly, &bgId, &this->actor, &this->actor.world.pos);
 }
 
 // Update functions and Draw

src/overlays/actors/ovl_En_Kusa/z_en_kusa.c
@@ -99,7 +99,7 @@ void EnKusa_SetupAction(EnKusa* this, EnKusaActionFunc actionFunc) {
 
 s32 EnKusa_SnapToFloor(EnKusa* this, PlayState* play, f32 yOffset) {
     s32 pad;
-    CollisionPoly* poly;
+    CollisionPoly* groundPoly;
     Vec3f pos;
     s32 bgId;
     f32 floorY;
@@ -108,7 +108,7 @@ s32 EnKusa_SnapToFloor(EnKusa* this, PlayState* play, f32 yOffset) {
     pos.y = this->actor.world.pos.y + 30.0f;
     pos.z = this->actor.world.pos.z;
 
-    floorY = BgCheck_EntityRaycastFloor4(&play->colCtx, &poly, &bgId, &this->actor, &pos);
+    floorY = BgCheck_EntityRaycastDown4(&play->colCtx, &groundPoly, &bgId, &this->actor, &pos);
 
     if (floorY > BGCHECK_Y_MIN) {
         this->actor.world.pos.y = floorY + yOffset;

Dragorn421/oot

src/overlays/actors/ovl_En_Fish/z_en_fish.c
@@ -629,7 +629,7 @@ void func_80A16A64(EnFish* this, PlayState* play) {
     this->actor.world.pos.y = ((sp34.y - sp40.y) * temp_fv0) + sp40.y + D_80A17014;
     this->actor.world.pos.z = ((sp34.z - sp40.z) * temp_fv0) + sp40.z;
     this->actor.floorHeight =
-        BgCheck_EntityRaycastFloor4(&play->colCtx, &this->actor.floorPoly, &sp2C, &this->actor, &this->actor.world.pos);
+        BgCheck_EntityRaycastDown4(&play->colCtx, &this->actor.floorPoly, &sp2C, &this->actor, &this->actor.world.pos);
 }
 
 void func_80A16C68(EnFish* this, PlayState* play) {

src/overlays/actors/ovl_En_Kusa/z_en_kusa.c
@@ -88,7 +88,7 @@ s32 func_80A9AFAC(EnKusa* this, PlayState* play, f32 arg2) {
     sp2C.x = this->actor.world.pos.x;
     sp2C.y = this->actor.world.pos.y + 30.0f;
     sp2C.z = this->actor.world.pos.z;
-    temp_fv0 = BgCheck_EntityRaycastFloor4(&play->colCtx, &sp38, &sp28, &this->actor, &sp2C);
+    temp_fv0 = BgCheck_EntityRaycastDown4(&play->colCtx, &sp38, &sp28, &this->actor, &sp2C);
     if (temp_fv0 > BGCHECK_Y_MIN) {
         this->actor.world.pos.y = temp_fv0 + arg2;
         Math_Vec3f_Copy(&this->actor.home.pos, &this->actor.world.pos);