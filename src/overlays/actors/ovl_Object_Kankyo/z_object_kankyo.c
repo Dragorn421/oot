HackerN64/HackerOoT

Format script improvements and use strict name checks (#1275)
* Prioritize using clang-tidy 6 and improve format.sh

* Use clang-tidy-6.0 when available since it's better at applying 'readability-inconsistent-declaration-parameter-name'
* Add -m32 and -Wno-everything to compiler options to avoid unwanted errors and warnings
* Remove -fsyntax-only since it serves no purpose in the context of clang-tidy

* Apply clang-tidy fixes for argument names

* Run format.sh again

* Use 'Strict' option instead of relying on clang-tidy 6

* Run format script

* Add --fix-notes to clang-tidy options for version 13+

src/overlays/actors/ovl_Object_Kankyo/z_object_kankyo.c
@@ -27,11 +27,11 @@ void ObjectKankyo_SunGraveSpark(ObjectKankyo* this, PlayState* play);
 void ObjectKankyo_WaitForBeamObject(ObjectKankyo* this, PlayState* play);
 void ObjectKankyo_Beams(ObjectKankyo* this, PlayState* play);
 
-void ObjectKankyo_DrawFairies(Actor* this, PlayState* play);
-void ObjectKankyo_DrawSnow(Actor* this, PlayState* play);
-void ObjectKankyo_DrawLightning(Actor* this, PlayState* play);
-void ObjectKankyo_DrawSunGraveSpark(Actor* this, PlayState* play);
-void ObjectKankyo_DrawBeams(Actor* this, PlayState* play);
+void ObjectKankyo_DrawFairies(Actor* thisx, PlayState* play2);
+void ObjectKankyo_DrawSnow(Actor* thisx, PlayState* play2);
+void ObjectKankyo_DrawLightning(Actor* thisx, PlayState* play);
+void ObjectKankyo_DrawSunGraveSpark(Actor* thisx, PlayState* play2);
+void ObjectKankyo_DrawBeams(Actor* thisx, PlayState* play2);
 
 static void* sEffLightningTextures[] = {
     gEffLightning1Tex, gEffLightning2Tex, gEffLightning3Tex, gEffLightning4Tex,

Dragorn421/oot

src/overlays/actors/ovl_Object_Kankyo/z_object_kankyo.c
@@ -27,11 +27,11 @@ void ObjectKankyo_SunGraveSpark(ObjectKankyo* this, PlayState* play);
 void ObjectKankyo_WaitForBeamObject(ObjectKankyo* this, PlayState* play);
 void ObjectKankyo_Beams(ObjectKankyo* this, PlayState* play);
 
-void ObjectKankyo_DrawFairies(ObjectKankyo* this, PlayState* play);
-void ObjectKankyo_DrawSnow(ObjectKankyo* this, PlayState* play);
+void ObjectKankyo_DrawFairies(ObjectKankyo* this2, PlayState* play2);
+void ObjectKankyo_DrawSnow(ObjectKankyo* this2, PlayState* play2);
 void ObjectKankyo_DrawLightning(ObjectKankyo* this, PlayState* play);
-void ObjectKankyo_DrawSunGraveSpark(ObjectKankyo* this, PlayState* play);
-void ObjectKankyo_DrawBeams(ObjectKankyo* this, PlayState* play);
+void ObjectKankyo_DrawSunGraveSpark(ObjectKankyo* this2, PlayState* play2);
+void ObjectKankyo_DrawBeams(ObjectKankyo* this2, PlayState* play2);
 
 static void* sEffLightningTextures[] = {
     gEffLightning1Tex, gEffLightning2Tex, gEffLightning3Tex, gEffLightning4Tex,