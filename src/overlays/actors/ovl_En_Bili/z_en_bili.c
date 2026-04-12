HackerN64/HackerOoT

Use `DMG_` defines in code (#1245)
* Use `DMG_` defines for `dmgFlags` when single bits are used

* Pass on several (in code, no colliders)

* Conditional cleanup

* Remove wip comment, just use the flags 4head

src/overlays/actors/ovl_En_Bili/z_en_bili.c
@@ -586,7 +586,7 @@ void EnBili_UpdateDamage(EnBili* this, PlayState* play) {
                 EnBili_SetupBurnt(this);
             }
 
-            if (this->collider.info.acHitInfo->toucher.dmgFlags & 0x1F820) { // DMG_ARROW
+            if (this->collider.info.acHitInfo->toucher.dmgFlags & DMG_ARROW) {
                 this->actor.flags |= ACTOR_FLAG_4;
             }
         }

src/overlays/actors/ovl_En_Ge2/z_en_ge2.c
@@ -554,7 +554,8 @@ void EnGe2_Update(Actor* thisx, PlayState* play) {
     if ((this->stateFlags & GE2_STATE_KO) || (this->stateFlags & GE2_STATE_CAPTURING)) {
         this->actionFunc(this, play);
     } else if (this->collider.base.acFlags & AC_HIT) {
-        if ((this->collider.info.acHitInfo != NULL) && (this->collider.info.acHitInfo->toucher.dmgFlags & 0x80)) {
+        if ((this->collider.info.acHitInfo != NULL) &&
+            (this->collider.info.acHitInfo->toucher.dmgFlags & DMG_HOOKSHOT)) {
             Actor_SetColorFilter(&this->actor, 0, 120, 0, 400);
             this->actor.update = EnGe2_UpdateStunned;
             return;
@@ -603,8 +604,8 @@ void EnGe2_UpdateStunned(Actor* thisx, PlayState* play2) {
     CollisionCheck_SetOC(play, &play->colChkCtx, &this->collider.base);
     Actor_UpdateBgCheckInfo(play, &this->actor, 40.0f, 25.0f, 40.0f, UPDBGCHECKINFO_FLAG_0 | UPDBGCHECKINFO_FLAG_2);
 
-    if ((this->collider.base.acFlags & AC_HIT) &&
-        ((this->collider.info.acHitInfo == NULL) || !(this->collider.info.acHitInfo->toucher.dmgFlags & 0x80))) {
+    if ((this->collider.base.acFlags & AC_HIT) && ((this->collider.info.acHitInfo == NULL) ||
+                                                   !(this->collider.info.acHitInfo->toucher.dmgFlags & DMG_HOOKSHOT))) {
         this->actor.colorFilterTimer = 0;
         EnGe2_ChangeAction(this, GE2_ACTION_KNOCKEDOUT);
         this->timer = 100;

Dragorn421/oot

src/overlays/actors/ovl_En_Bili/z_en_bili.c
@@ -514,7 +514,7 @@ void func_809C0A70(EnBili* this, PlayState* play) {
             } else {
                 func_809BFC48(this);
             }
-            if (this->unk1D4.info.acHitInfo->toucher.dmgFlags & 0x1F820) {
+            if (this->unk1D4.info.acHitInfo->toucher.dmgFlags & DMG_ARROW) {
                 this->actor.flags |= ACTOR_FLAG_4;
             }
         }

src/overlays/actors/ovl_En_Ge2/z_en_ge2.c
@@ -506,7 +506,8 @@ void EnGe2_Update(Actor* thisx, PlayState* play) {
         this->unk308(this, play);
     } else {
         if (this->unk14C.base.acFlags & AC_HIT) {
-            if ((this->unk14C.info.acHitInfo != NULL) && (this->unk14C.info.acHitInfo->toucher.dmgFlags & 0x80)) {
+            if ((this->unk14C.info.acHitInfo != NULL) &&
+                (this->unk14C.info.acHitInfo->toucher.dmgFlags & DMG_HOOKSHOT)) {
                 Actor_SetColorFilter(&this->actor, 0, 0x78, 0, 0x190);
                 this->actor.update = func_80A3402C;
                 return;
@@ -546,7 +547,7 @@ void func_80A3402C(Actor* thisx, PlayState* play2) {
     CollisionCheck_SetOC(play, &play->colChkCtx, &this->unk14C.base);
     Actor_UpdateBgCheckInfo(play, &this->actor, 40.0f, 25.0f, 40.0f, UPDBGCHECKINFO_FLAG_0 | UPDBGCHECKINFO_FLAG_2);
     if ((this->unk14C.base.acFlags & AC_HIT) &&
-        (((this->unk14C.info.acHitInfo == NULL)) || !(this->unk14C.info.acHitInfo->toucher.dmgFlags & 0x80))) {
+        (((this->unk14C.info.acHitInfo == NULL)) || !(this->unk14C.info.acHitInfo->toucher.dmgFlags & DMG_HOOKSHOT))) {
         this->actor.colorFilterTimer = 0;
         func_80A32BD0(this, 3);
         this->unk305 = 0x64;