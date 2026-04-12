HackerN64/HackerOoT

Fix misc 11 (#1209)
* Pass boolean for `freezeFlag` to `Actor_SetDropFlag`

* Use `else if`s in `func_80835F44`

* Cleanup `transitionCtx` usage of instance data

* `TransitionContext.setEnvColor` -> `TransitionContext.setUnkColor`

* Fixup one comment

* linebreaks (#1)

* Run formatter

Co-authored-by: fig02 <fig02srl@gmail.com>

src/overlays/actors/ovl_En_Bili/z_en_bili.c
@@ -549,7 +549,7 @@ void EnBili_UpdateDamage(EnBili* this, GlobalContext* globalCtx) {
 
     if ((this->actor.colChkInfo.health != 0) && (this->collider.base.acFlags & AC_HIT)) {
         this->collider.base.acFlags &= ~AC_HIT;
-        Actor_SetDropFlag(&this->actor, &this->collider.info, 1);
+        Actor_SetDropFlag(&this->actor, &this->collider.info, true);
 
         if ((this->actor.colChkInfo.damageEffect != 0) || (this->actor.colChkInfo.damage != 0)) {
             if (Actor_ApplyDamage(&this->actor) == 0) {

src/overlays/actors/ovl_En_Skb/z_en_skb.c
@@ -449,7 +449,7 @@ void func_80AFD968(EnSkb* this, GlobalContext* globalCtx) {
             this->collider.base.acFlags &= ~2;
             if (this->actor.colChkInfo.damageEffect != 6) {
                 this->unk_282 = this->actor.colChkInfo.damageEffect;
-                Actor_SetDropFlag(&this->actor, &this->collider.elements[1].info, 1);
+                Actor_SetDropFlag(&this->actor, &this->collider.elements[1].info, true);
                 this->unk_281 = 0;
                 if (this->actor.colChkInfo.damageEffect == 1) {
                     if (this->unk_280 != 6) {

src/overlays/actors/ovl_En_Tp/z_en_tp.c
@@ -578,7 +578,7 @@ void EnTp_UpdateDamage(EnTp* this, GlobalContext* globalCtx) {
         }
 
         this->collider.base.acFlags &= ~AC_HIT;
-        Actor_SetDropFlagJntSph(&this->actor, &this->collider, 1);
+        Actor_SetDropFlagJntSph(&this->actor, &this->collider, true);
         this->damageEffect = this->actor.colChkInfo.damageEffect;
 
         if (this->actor.colChkInfo.damageEffect != TAILPASARAN_DMGEFF_NONE) {

src/overlays/actors/ovl_En_Wf/z_en_wf.c
@@ -1266,7 +1266,7 @@ void EnWf_UpdateDamage(EnWf* this, GlobalContext* globalCtx) {
 
             if (this->actor.colChkInfo.damageEffect != ENWF_DMGEFF_ICE_MAGIC) {
                 this->damageEffect = this->actor.colChkInfo.damageEffect;
-                Actor_SetDropFlag(&this->actor, &this->colliderCylinderBody.info, 1);
+                Actor_SetDropFlag(&this->actor, &this->colliderCylinderBody.info, true);
                 this->slashStatus = 0;
 
                 if ((this->actor.colChkInfo.damageEffect == ENWF_DMGEFF_STUN) ||

Dragorn421/oot

src/overlays/actors/ovl_En_Bili/z_en_bili.c
@@ -483,7 +483,7 @@ void func_809C09E0(EnBili* this, GlobalContext* globalCtx) {
 void func_809C0A70(EnBili* this, GlobalContext* globalCtx) {
     if ((this->actor.colChkInfo.health != 0) && (this->unk1D4.base.acFlags & AC_HIT)) {
         this->unk1D4.base.acFlags &= ~AC_HIT;
-        Actor_SetDropFlag(&this->actor, &this->unk1D4.info, 1);
+        Actor_SetDropFlag(&this->actor, &this->unk1D4.info, true);
         if ((((this->actor.colChkInfo.damageEffect != 0)) || (this->actor.colChkInfo.damage != 0))) {
             if (Actor_ApplyDamage(&this->actor) == 0) {
                 Audio_PlayActorSound2(&this->actor, NA_SE_EN_BIRI_DEAD);

src/overlays/actors/ovl_En_Skb/z_en_skb.c
@@ -407,7 +407,7 @@ void func_80AFD968(EnSkb* this, GlobalContext* globalCtx) {
         this->unk2A4.base.acFlags &= ~AC_HIT;
         if (((this->actor.colChkInfo.damageEffect != 6))) {
             this->unk282 = this->actor.colChkInfo.damageEffect;
-            Actor_SetDropFlag(&this->actor, &this->unk2A4.elements[1].info, 1);
+            Actor_SetDropFlag(&this->actor, &this->unk2A4.elements[1].info, true);
             this->unk281 = 0;
             if (this->actor.colChkInfo.damageEffect == 1) {
                 if (this->unk280 != 6) {

src/overlays/actors/ovl_En_Tp/z_en_tp.c
@@ -486,7 +486,7 @@ void func_80B221E8(EnTp* this, GlobalContext* globalCtx) {
             var_s2 = 1;
         }
         this->unk174.base.acFlags &= ~2;
-        Actor_SetDropFlagJntSph(&this->actor, &this->unk174, 1);
+        Actor_SetDropFlagJntSph(&this->actor, &this->unk174, true);
         this->unk158 = this->actor.colChkInfo.damageEffect;
         if (this->actor.colChkInfo.damageEffect != 0) {
             if (this->actor.colChkInfo.damageEffect == 1) {

src/overlays/actors/ovl_En_Wf/z_en_wf.c
@@ -1185,7 +1185,7 @@ void func_80B37038(EnWf* this, GlobalContext* globalCtx) {
         this->unk470.base.acFlags &= ~AC_HIT;
         if (this->actor.colChkInfo.damageEffect != 6) {
             this->unk2E6 = this->actor.colChkInfo.damageEffect;
-            Actor_SetDropFlag(&this->actor, &this->unk424.info, 1);
+            Actor_SetDropFlag(&this->actor, &this->unk424.info, true);
             this->unk2F8 = 0;
             if ((this->actor.colChkInfo.damageEffect == 1) || (this->actor.colChkInfo.damageEffect == 0xF)) {
                 if (this->unk2D4 != 0xF) {