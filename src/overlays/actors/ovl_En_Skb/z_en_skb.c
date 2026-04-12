HackerN64/HackerOoT

Fix misc 16 (#1371)
* Replace tabs in exceptasm.s source

* Replace tabs with \t in strings

* Fix grammar in various comments

* Fix "initalize" typos in code

* (fix typo) `__osMallocIsInitalized` -> `__osMallocIsInitialized`

* Some Link -> Player

* Use `EXCH_ITEM_` more

* "bgm music" -> "background music"

* Fix typos in `place_title_cards.xml`

* `SkelAnime_Update` returns a boolean

* Run formatter

src/overlays/actors/ovl_En_Skb/z_en_skb.c
@@ -231,7 +231,7 @@ void EnSkb_RiseFromGround(EnSkb* this, PlayState* play) {
     if ((play->gameplayFrames & 1) != 0) {
         EnSkb_SpawnDebris(play, this, &this->actor.world.pos);
     }
-    if ((SkelAnime_Update(&this->skelAnime) != 0) && (0.0f == this->actor.shape.yOffset)) {
+    if (SkelAnime_Update(&this->skelAnime) && (0.0f == this->actor.shape.yOffset)) {
         EnSkb_DecideNextAction(this);
     }
 }
@@ -253,7 +253,7 @@ void EnSkb_Despawn(EnSkb* this, PlayState* play) {
         EnSkb_SpawnDebris(play, this, &this->actor.world.pos);
     }
     Math_SmoothStepToF(&this->actor.shape.shadowScale, 0.0f, 1.0f, 2.5f, 0.0f);
-    if (SkelAnime_Update(&this->skelAnime) != 0) {
+    if (SkelAnime_Update(&this->skelAnime)) {
         Actor_Kill(&this->actor);
     }
 }
@@ -328,7 +328,7 @@ void EnSkb_Attack(EnSkb* this, PlayState* play) {
     if (this->collider.base.atFlags & AT_BOUNCED) {
         this->collider.base.atFlags &= ~(AT_HIT | AT_BOUNCED);
         EnSkb_SetupRecoil(this);
-    } else if (SkelAnime_Update(&this->skelAnime) != 0) {
+    } else if (SkelAnime_Update(&this->skelAnime)) {
         EnSkb_DecideNextAction(this);
     }
 }
@@ -343,7 +343,7 @@ void EnSkb_SetupRecoil(EnSkb* this) {
 }
 
 void EnSkb_Recoil(EnSkb* this, PlayState* play) {
-    if (SkelAnime_Update(&this->skelAnime) != 0) {
+    if (SkelAnime_Update(&this->skelAnime)) {
         EnSkb_DecideNextAction(this);
     }
 }

Dragorn421/oot

src/overlays/actors/ovl_En_Skb/z_en_skb.c
@@ -225,7 +225,7 @@ void EnSkb_Despawn(EnSkb* this, PlayState* play) {
         func_80AFC9A8(play, this, &this->actor.world.pos);
     }
     Math_SmoothStepToF(&this->actor.shape.shadowScale, 0.0f, 1.0f, 2.5f, 0.0f);
-    if (SkelAnime_Update(&this->unk14C) != 0) {
+    if (SkelAnime_Update(&this->unk14C)) {
         Actor_Kill(&this->actor);
     }
 }