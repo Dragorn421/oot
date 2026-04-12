HackerN64/HackerOoT

Fix misc 13 (#1244)
* Squared variables fixup

* `sAnimSoundFrames` fixup

* `ocElemFlags` fixup

* `toucherFlags` fixup

* `bumperFlags` fixup

* `atFlags` fixup

* `acFlags` fixup

* `ocFlags1` fixup

* `ocFlags2` fixup

* `&=`

* `TOUCH_SFX_NONE` -> `_MASK` in `CollisionCheck_HitSolid`

src/overlays/actors/ovl_Bg_Haka_Trap/z_bg_haka_trap.c
@@ -166,7 +166,7 @@ void BgHakaTrap_Init(Actor* thisx, PlayState* play) {
                 this->colliderCylinder.dim.radius = 18;
                 this->colliderCylinder.dim.height = 115;
 
-                this->colliderCylinder.info.toucherFlags = this->colliderCylinder.info.toucherFlags;
+                this->colliderCylinder.info.toucherFlags &= ~TOUCH_SFX_NORMAL;
                 this->colliderCylinder.info.toucherFlags |= TOUCH_SFX_WOOD;
 
                 this->actionFunc = func_808801B8;

Dragorn421/oot

src/overlays/actors/ovl_Bg_Haka_Trap/z_bg_haka_trap.c
@@ -127,7 +127,7 @@ void BgHakaTrap_Init(Actor* thisx, PlayState* play) {
                 Collider_SetTris(play, &this->unk1C4, &this->dyna.actor, &D_80880FF8, this->unk1E4);
                 this->unk178.dim.radius = 0x12;
                 this->unk178.dim.height = 0x73;
-                this->unk178.info.toucherFlags |= 0;
+                this->unk178.info.toucherFlags &= ~TOUCH_SFX_NORMAL;
                 this->unk178.info.toucherFlags |= 0x10;
                 this->actionFunc = &func_808801B8;
             }