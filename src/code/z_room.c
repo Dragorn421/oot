HackerN64/HackerOoT

Some doc on env light settings (#1307)
* remove `LightSettings` struct, typedef for zapd

* more decimal

* fog far -> z far

* `LIGHTCTX_FOGNEAR_MAX`, `LIGHTCTX_ZFAR_MAX`

* name sp88,sp8C in `Environment_Update`

* `EnvLightSettings.fogNear` -> `blendRateAndFogNear` and macros

* A different struct for `EnvironmentContext.lightSettings`

* Uniform zapd compat typedefs todos

* `LIGHTCTX_` -> `ENV_`

* Comment on `blendRateAndFogNear` + "fogFar"

* Move fogFar~1000 comment to zFar

* comment rewrite attempt

* move relevant macros down

Co-authored-by: fig02 <fig02srl@gmail.com>

src/code/z_room.c
@@ -153,7 +153,7 @@ void Room_DrawCullable(PlayState* play, Room* room, u32 flags) {
             entryBoundsNearZ = projectedPos.z - roomShapeCullableEntry->boundsSphereRadius;
 
             // If the entry bounding sphere isn't fully beyond the rendered depth range
-            if (entryBoundsNearZ < play->lightCtx.fogFar) {
+            if (entryBoundsNearZ < play->lightCtx.zFar) {
 
                 // This entry will be rendered
                 insert->entry = roomShapeCullableEntry;

Dragorn421/oot

src/code/z_room.c
@@ -153,7 +153,7 @@ void Room_DrawCullable(PlayState* play, Room* room, u32 flags) {
             entryStartZ = projectedPos.z - roomShapeCullableEntry->boundsSphereRadius;
 
             // If the entry isn't fully beyond the rendered depth range
-            if (entryStartZ < play->lightCtx.fogFar) {
+            if (entryStartZ < play->lightCtx.zFar) {
 
                 // This entry will be rendered
                 insert->entry = roomShapeCullableEntry;